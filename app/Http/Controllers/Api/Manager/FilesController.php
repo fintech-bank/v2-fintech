ce758de94ba56734799fe1faa2241b127b909d1b
Mockelyn Maxime <trainznation@gmail.com>
2022-10-15 17:58:09 +0200

    FE151020221758
    # Admin
    - Gestionnaire de documents

--- app/Http/Controllers/Api/Manager/FilesController.php
+++ app/Http/Controllers/Api/Manager/FilesController.php
@@ -4,8 +4,46 @@ namespace App\Http\Controllers\Api\Manager;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class FilesController extends Controller
 {
-    //
+    public function lists(Request $request)
+    {
+        $folder = $request->query->get('folder');
+        $files = Storage::disk()->files($folder);
+        return collect($files)
+            ->filter(fn(string $file) => !str_starts_with($file, '.'))
+            ->values()
+            ->map([$this, 'toArray']);
+    }
+
+    public function store(UploadFileRequest $request)
+    {
+        $file = $request->file('file');
+        $folder = $request->post('folder');
+        $filename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $path = $file->storeAs($folder, $filename . '_' . $file->hashName());
+        return $this->toArray($path);
+    }
+
+    public function delete(string $file)
+    {
+        Storage::disk()->delete($file);
+        return response('', Response::HTTP_NO_CONTENT);
+    }
+
+    public function toArray(string $file): array
+    {
+        $info = pathinfo($file);
+        $disk = Storage::disk();
+        return [
+            'id' => $file,
+            'name' => $info['basename'],
+            'url' => $disk->url($file),
+            'size' => $disk->size($file),
+            'folder' => $info['dirname'] === '.' ? null : $info['dirname'],
+            'thumbnail' => $disk->url($file),
+        ];
+    }
 }
--- resources/views/admin/scripts/account/documents/index.blade.php
+++ resources/views/admin/scripts/account/documents/index.blade.php
@@ -1,41 +1,14 @@
+<script type="module" defer>
+    import {FileManager} from '/assets/plugins/custom/filemanager/filemanager.js'
+
+    FileManager.register();
+
+
+</script>
+
 <script type="text/javascript">
-    let tables = {
-        tableManager: document.querySelector('#tableManager')
-    }
+    let tables = {}
     let elements = {}
     let modals = {}
     let forms = {}
-
-    let getFolders = (parent = null) => {
-        $.ajax({
-            url: '/api/manager/folders',
-            data: {'user_id': '{{ $user->id }}'},
-            success: data => {
-                console.log(data)
-                let table = tables.tableManager.querySelector('tbody');
-                table.innerHTML = ''
-                data.forEach(folder => {
-                    table.innerHTML += `
-                    <tr>
-                        <td>
-                            <div class="d-flex align-items-center">
-                                <i class="fa-solid fa-folder text-primary me-4"></i>
-                                <a href="" class="text-gray-800 text-hover-primary">${folder.name}</a>
-                            </div>
-                        </td>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                            <button class="btn btn-icon btn-sm btn-light btn-active-danger me-2"><i class="fa-solid fa-trash"></i> </button>
-                        </td>
-                    </tr>
-                    `
-                })
-
-                $(tables.tableManager).DataTable()
-            }
-        })
-    }
-
-    getFolders()
 </script>
