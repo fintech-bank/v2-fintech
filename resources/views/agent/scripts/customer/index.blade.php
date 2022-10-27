bca5e17888a142ad3bd9d98dd7e6b3c6527824bf
mmockelyn <trainznation@gmail.com>
2022-10-28 01:50:22 +0200

    Update index.blade.php

--- resources/views/agent/scripts/customer/index.blade.php
+++ resources/views/agent/scripts/customer/index.blade.php
@@ -6,7 +6,18 @@
     let modals = {}
     let forms = {}
     let dataTable = {
-        datatableCustomer: $(tables.tableCustomer).DataTable()
+        datatableCustomer: $(tables.tableCustomer).DataTable({
+            info: !1,
+            order: [],
+            columnDefs: [{
+                orderable: !1,
+                targets: 0
+            },
+                {
+                    orderable: !1,
+                    targets: 6
+                }]
+        })
     }
     let block = {}
 </script>
