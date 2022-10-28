2f6436d8f3aeee4d7333e78854be9691017b708b
mmockelyn <trainznation@gmail.com>
2022-10-28 02:46:53 +0200

    Update index.blade.php

--- resources/views/agent/customer/index.blade.php
+++ resources/views/agent/customer/index.blade.php
@@ -56,13 +56,13 @@
 								<path d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z" fill="currentColor" />
 							</svg>
 						</span>
-                        Filter
+                        Filtrer
                     </button>
                     <!--begin::Menu 1-->
                     <div class="menu menu-sub menu-sub-dropdown w-300px w-md-325px" data-kt-menu="true" id="kt-toolbar-filter">
                         <!--begin::Header-->
                         <div class="px-7 py-5">
-                            <div class="fs-4 text-dark fw-bold">Filter Options</div>
+                            <div class="fs-4 text-dark fw-bold">Option de filtre</div>
                         </div>
                         <!--end::Header-->
                         <!--begin::Separator-->
@@ -142,7 +142,7 @@
                     <!--end::Menu 1-->
                     <!--end::Filter-->
                     <!--begin::Add customer-->
-                    <a class="btn btn-primary"><i class="fa-solid fa-plus-circle me-2"></i> Nouveau client</a>
+                    <a class="btn btn-primary"><i class="fa-regular fa-plus fa-lg me-2"></i> Nouveau client</a>
                     <!--end::Add customer-->
                 </div>
                 <!--end::Toolbar-->
