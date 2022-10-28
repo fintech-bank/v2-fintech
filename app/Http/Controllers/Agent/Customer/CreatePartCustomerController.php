6c147cb8e43bfcf689cd1db990b6b4a999ae52e1
mmockelyn <trainznation@gmail.com>
2022-10-28 04:49:08 +0200

    Update CreatePartCustomerController.php

--- app/Http/Controllers/Agent/Customer/CreatePartCustomerController.php
+++ app/Http/Controllers/Agent/Customer/CreatePartCustomerController.php
@@ -14,7 +14,7 @@ class CreatePartCustomerController extends Controller
 
     public function pro(Request $request)
     {
-        session()->put('info', $request->all());
+        session()->put('info', $request->except('_token'));
 
         return view('agent.customer.create.part.pro');
     }
