48d7d500c578f598827d454236f61e065f3d4731
mmockelyn <trainznation@gmail.com>
2022-10-28 04:19:43 +0200

    Update create.blade.php

--- resources/views/agent/scripts/customer/create.blade.php
+++ resources/views/agent/scripts/customer/create.blade.php
@@ -8,5 +8,23 @@
     let dataTable = {}
     let block = {}
 
+    let countryBirthOptions = (item) => {
+        if ( !item.id ) {
+            return item.text;
+        }
+        let span = document.createElement('span');
+        let imgUrl = item.element.getAttribute('data-kt-select2-country');
+        let template = '';
+        template += '<img src="' + imgUrl + '" class="rounded-circle w-20px h-20px me-2" alt="image" />';
+        template += item.text;
+        span.innerHTML = template;
+        return $(span);
+    }
+
     if(elements.field_datebirth) { $(elements.field_datebirth).flatpickr({"locale": "fr"}) }
+
+    $("#countrybirth").select2({
+        templateSelection: countryBirthOptions,
+        templateResult: countryBirthOptions
+    })
 </script>
