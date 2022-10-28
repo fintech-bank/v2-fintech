34ca980623512a1d36f0ba4d16f5d8ce3f399180
mmockelyn <trainznation@gmail.com>
2022-10-28 19:47:01 +0200

    feat(Agent Client Création Client): Edition de la partie client

--- resources/views/agent/customer/create/part/options.blade.php
+++ resources/views/agent/customer/create/part/options.blade.php
@@ -239,7 +239,7 @@
                         </a>
                         <a href="" class="card shadow-lg me-5" data-bs-toggle="modal" data-bs-target="#subscribeDailyInsurance">
                             <div class="card-body">
-                                <div class="d-flex flex-row">
+                                <div class="d-flex flex-row align-items-center">
                                     <div class="symbol symbol-50px me-5">
                                         <div class="symbol-label fs-2 fw-semibold text-success"><i class="fa-solid fa-umbrella fs-2"></i> </div>
                                     </div>
