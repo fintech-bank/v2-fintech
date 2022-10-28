cb5053a3ef8c02c082f31ad5cd3469107ed854f6
mmockelyn <trainznation@gmail.com>
2022-10-28 20:27:57 +0200

    feat(Agent Client Création Client): Edition de la partie client

--- app/Http/Controllers/Api/Customer/CustomerController.php
+++ app/Http/Controllers/Api/Customer/CustomerController.php
@@ -14,6 +14,7 @@ class CustomerController extends Controller
             'dailyInsurance' => $this->subscribeDailyInsurance(),
             'dab' => $this->subscribeDab(),
             'overdraft' => $this->subscribeOverdraft($request->get('overdraft_amount')),
+            'card_code' => $this->subscribeCardCode(),
             default => null,
         };
     }
