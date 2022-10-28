1d33cf772daa0b4a1f0d546276eb4b2a9187936d
mmockelyn <trainznation@gmail.com>
2022-10-28 04:20:47 +0200

    Update create.blade.php

--- resources/views/agent/scripts/customer/create.blade.php
+++ resources/views/agent/scripts/customer/create.blade.php
@@ -21,6 +21,57 @@
         return $(span);
     }
 
+    let countryOptions = (item) => {
+        if ( !item.id ) {
+            return item.text;
+        }
+        let span = document.createElement('span');
+        let imgUrl = item.element.getAttribute('data-kt-select2-country');
+        let template = '';
+        template += '<img src="' + imgUrl + '" class="rounded-circle w-20px h-20px me-2" alt="image" />';
+        template += item.text;
+        span.innerHTML = template;
+        return $(span);
+    }
+
+    let citiesFromCountry = (select) => {
+        console.log(select.value)
+        let contentCities = document.querySelector('#divCities')
+        $.ajax({
+            url: '/api/geo/cities',
+            method: 'post',
+            data: {"country": select.value},
+            success: data => {
+                console.log(data)
+                contentCities.innerHTML = data
+                $("#citybirth").select2()
+            }
+        })
+    }
+    let citiesFromPostal = (select) => {
+        let contentCities = document.querySelector('#divCity')
+        let block = new KTBlockUI(contentCities, {
+            message: '<div class="blockui-message"><span class="spinner-border text-primary"></span> Chargement...</div>',
+        })
+        block.block();
+        $.ajax({
+            url: '/api/geo/cities/'+select.value,
+            success: data => {
+                block.release()
+                contentCities.innerHTML = data
+                $("#city").select2()
+            }
+        })
+    }
+    document.querySelectorAll('[name="postal"]').forEach(input => {
+        input.addEventListener('keyup', e => {
+            console.log(e.target.value.length)
+            if(e.target.value.length === 5) {
+                citiesFromPostal(e.target)
+            }
+        })
+    })
+
     if(elements.field_datebirth) { $(elements.field_datebirth).flatpickr({"locale": "fr"}) }
 
     $("#countrybirth").select2({
