17d97e72e7569814b8fe708084c80e5c25ceebca
mmockelyn <trainznation@gmail.com>
2022-10-31 17:05:25 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Agent/Customer/CustomerController.php
+++ app/Http/Controllers/Agent/Customer/CustomerController.php
@@ -25,14 +25,17 @@ class CustomerController extends Controller
     {
         $session = (object) session()->all();
         $help = new CustomerHelper();
-        dd($session);
 
         if($request->has('refresh')) {
             $customer = Customer::find($request->get('customer_id'));
         } else {
             $create = $help->createCustomer($session);
             $customer = $create->customers;
-            session()->forget('');
+            session()->forget('perso');
+            session()->forget('rent');
+            session()->forget('package');
+            session()->forget('card');
+            session()->forget('subscribe');
         }
 
         return view('agent.customer.create.finish', compact('customer'));
