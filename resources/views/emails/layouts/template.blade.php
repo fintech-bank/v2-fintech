ec0a6824d7bf1fad55fa9c75c872d3a313f81d01
mmockelyn <trainznation@gmail.com>
2022-10-31 15:24:24 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/emails/layouts/template.blade.php
+++ resources/views/emails/layouts/template.blade.php
@@ -383,8 +383,8 @@
                                                         <tr>
                                                             <td align="center" style="padding:0;Margin:0">
                                                                 <p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;margin-bottom:11px;color:#333333;font-size:14px">
-                                                                    {{ $customer->info->full_name }} <br>
-                                                                    {{ $customer->info->mobile }} <br>
+                                                                    {{ $customer->info->full_name }} -
+                                                                    {{ $customer->info->mobile }} -
                                                                     {{ \App\Helper\CustomerInfoHelper::getAddress($customer->info) }}
                                                                 </p>
                                                             </td>
