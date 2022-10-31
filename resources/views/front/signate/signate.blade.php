fc9ad44d01c81ac1496c94a6c0c52d7ef800a918
mmockelyn <trainznation@gmail.com>
2022-10-31 20:23:20 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/front/signate/signate.blade.php
+++ resources/views/front/signate/signate.blade.php
@@ -64,7 +64,7 @@
             pageNum = 1,
             pageRendering = false,
             pageNumPending = null,
-            scale = 2.5,
+            scale = 2,
             canvas = document.getElementById('pdfcontent'),
             ctx = canvas.getContext('2d');
 
