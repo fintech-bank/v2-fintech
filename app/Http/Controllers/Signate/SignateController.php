ddce643c9461b93fe2bb5453757599e071e40803
mmockelyn <trainznation@gmail.com>
2022-10-31 18:52:17 +0100

    feat(Agent Client Création Client) : Edition de la partie client

---
+++ app/Http/Controllers/Signate/SignateController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Signate;
+
+use App\Http\Controllers\Controller;
+use App\Models\Customer\CustomerDocument;
+use Illuminate\Http\Request;
+
+class SignateController extends Controller
+{
+    public function signate($token)
+    {
+        $token_d = base64_decode($token);
+        $document = CustomerDocument::find($token_d);
+        dd($document);
+    }
+}
