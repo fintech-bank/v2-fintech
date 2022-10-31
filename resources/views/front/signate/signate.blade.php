fe5deb3651c047b6dc2d8f4df3313cc4ac9b9bd9
mmockelyn <trainznation@gmail.com>
2022-10-31 20:34:18 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/front/signate/signate.blade.php
+++ resources/views/front/signate/signate.blade.php
@@ -27,7 +27,7 @@
             </div>
         </div>
         <div class="card-body">
-            <div class="d-flex flex-center scroll h-600px">
+            <div class="d-flex flex-center scroll h-750px">
                 <canvas id="pdfcontent"></canvas>
             </div>
         </div>
