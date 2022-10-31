2beee70dca30fb1ba1ec0bcc8c5742eae7483df7
mmockelyn <trainznation@gmail.com>
2022-10-31 15:13:51 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- routes/mailbook.php
+++ routes/mailbook.php
@@ -4,8 +4,4 @@ use App\Mail\MailbookMail;
 use Xammie\Mailbook\Facades\Mailbook;
 
 Mailbook::add(MailbookMail::class);
-Mailbook::add(function () {
-    $customer = \App\Models\Customer\Customer::find(1);
-    $link = '/test';
-    return new \App\Notifications\Customer\SendVerificationLinkNotification($customer, $link);
-});
+
