e4250c8ee57c8f947ab164ba8c254246c8ea80c3
mmockelyn <trainznation@gmail.com>
2022-11-01 23:15:54 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,23 +36,12 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
-    private Guard $auth;
-    private Registrar $registrar;
-    private AuthyService $authy;
 
-    /**
-     * Create a new controller instance.
-     * @param Guard $auth
-     * @param Registrar $registrar
-     * @param AuthyService $authy
-     */
-    public function __construct(Guard $auth, Registrar $registrar, AuthyService $authy)
+
+    public function __construct()
     {
 
         $this->middleware('guest')->except('logout');
-        $this->auth = $auth;
-        $this->registrar = $registrar;
-        $this->authy = $authy;
     }
 
     /**
@@ -65,68 +54,6 @@ class LoginController extends Controller
         return view('auths.login');
     }
 
-    /**
-     * Handle a login request to the application.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function login(Request $request)
-    {
-        $credentials = $request->only('email', 'password');
-
-        if (\Auth::validate($credentials)) {
-            $user = User::where('email', '=', $request->input('email'))->firstOrFail();
-
-            Session::put('password_validated', true);
-            Session::put('user_id', $user->id);
-
-            if ($this->authy->verifyUserStatus($user->authy_id)->registered) {
-                $uuid = $this->authy->sendOneTouch($user->authy_id, 'Request to Login to Twilio demo app');
-                $user->update([
-                    'authy_one_touch_uuid' => $uuid
-                ]);
-                Session::put('one_touch_uuid', $uuid);
-
-                return response()->json(['status' => "ok"]);
-            } else {
-                return response()->json(['status' => 'verify']);
-            }
-        } else {
-            return response()->json([
-                "status" => "failed",
-                "message" => "La combinaison e-mail et mot de passe que vous avez saisie est incorrecte."
-            ]);
-        }
-    }
-
-    public function getTwoFactor()
-    {
-        $message = Session::get('message');
-
-        return view('auths.auth.verify', ['message' => $message]);
-    }
-
-    public function postTwoFactor(Request $request)
-    {
-        if (!Session::get('password_validated') || !Session::get('id')) {
-            return redirect('/auth/login');
-        }
-
-        if (isset($_POST['token'])) {
-            $user = User::find(Session::get('id'));
-            if ($this->authy->verifyToken($user->authy_id, $request->input('token'))) {
-                Auth::login($user);
-                return redirect()->intended('/redirect');
-            } else {
-                return redirect('/auth/verify')->withErrors([
-                    'token' => 'The token you entered is incorrect',
-                ]);
-            }
-        }
-    }
 
     /**
      * The user has been authenticated.
