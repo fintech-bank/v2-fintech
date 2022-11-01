6f515ef3da1b3e54e182af1c0848ec0e65d3464b
mmockelyn <trainznation@gmail.com>
2022-11-02 00:09:34 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/show.blade.php
+++ resources/views/agent/customer/show.blade.php
@@ -39,7 +39,7 @@
                     <!--begin: Pic-->
                     <div class="me-7 mb-4">
                         <div class="symbol symbol-100px symbol-lg-160px symbol-fixed position-relative">
-                            <img src="/metronic8/demo1/assets/media/avatars/300-1.jpg" alt="image">
+                            {!! $customer->user->avatar_symbol !!}
                             <div class="position-absolute translate-middle bottom-0 start-100 mb-6 bg-success rounded-circle border border-4 border-body h-20px w-20px"></div>
                         </div>
                     </div>
