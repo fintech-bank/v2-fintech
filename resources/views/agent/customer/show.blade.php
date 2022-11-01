35d6edc8c3924ce237e71ba529760cc40928d90c
mmockelyn <trainznation@gmail.com>
2022-11-02 00:20:04 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/show.blade.php
+++ resources/views/agent/customer/show.blade.php
@@ -65,7 +65,7 @@
                             <div class="d-flex flex-wrap fw-semibold fs-6 mb-4 pe-2">
                                 <a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
                                     <i class="fa-solid fa-boxes-packing me-1"></i>
-                                    <span>Developer</span>
+                                    <span>{{ $customer->package->name }}</span>
                                 </a>
                                 <a href="#" class="d-flex align-items-center text-gray-400 text-hover-primary me-5 mb-2">
                                     <!--begin::Svg Icon | path: icons/duotune/general/gen018.svg-->
