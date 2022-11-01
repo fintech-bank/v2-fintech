93ab13bb34ebdadcc8342506cd5f1be1f51a13be
mmockelyn <trainznation@gmail.com>
2022-11-02 00:10:17 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/show.blade.php
+++ resources/views/agent/scripts/customer/show.blade.php
@@ -4,12 +4,5 @@
     let modals = {}
     let forms = {}
     let dataTable = {}
-    let block = {
-        blockApp: new KTBlockUI(document.querySelector("#app"), {
-            message: `<div class="blockui-message"><span class="spinner-border text-primary"></span> Chargement en cours...</div>`,
-            overlayClass: "bg-gray-600",
-        })
-    }
-
-    block.blockApp.block()
+    let block = {}
 </script>
