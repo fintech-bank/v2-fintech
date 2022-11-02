eff2ca873bc6cefd8ff68155865fba41d9d6668d
mmockelyn <trainznation@gmail.com>
2022-11-02 01:45:00 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- routes/agent.php
+++ routes/agent.php
@@ -53,6 +53,7 @@ Route::prefix('agence')->middleware(['auth', 'agent'])->group(function () {
     Route::prefix('customer')->group(function () {
         Route::get('/', [\App\Http\Controllers\Agent\Customer\CustomerController::class, 'index'])->name('agent.customer.index');
         Route::get('{id}', [\App\Http\Controllers\Agent\Customer\CustomerController::class, 'show'])->name('agent.customer.show');
+        Route::put('{id}', [\App\Http\Controllers\Agent\Customer\CustomerController::class, 'update'])->name('agent.customer.update');
         Route::prefix('create')->group(function() {
             Route::get('start', [\App\Http\Controllers\Agent\Customer\CustomerController::class, 'start'])->name('agent.customer.create.start');
             Route::post('subscribe', [\App\Http\Controllers\Api\Customer\CustomerController::class, 'subscribe']);
