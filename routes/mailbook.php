37c9301bca1db340e236c278f94f1f77dcf71298
mmockelyn <trainznation@gmail.com>
2022-11-02 23:37:20 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- routes/mailbook.php
+++ routes/mailbook.php
@@ -11,7 +11,7 @@ Mailbook::add(function () {
     return new \App\Mail\Customer\SendVerificationLinkMail($customer, $link);
 });
 
-Mailbook::add(function () {
+Mailbook::add(class: function () {
     $customer = \App\Models\Customer\Customer::find(1);
 
     return new \App\Mail\Customer\UpdateStatusAccountMail($customer, $customer->status_open_account);
