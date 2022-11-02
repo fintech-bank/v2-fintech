d557fa69cc8abc639cbee04f9a2aa7f8f64b9077
mmockelyn <trainznation@gmail.com>
2022-11-02 22:08:51 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/show.blade.php
+++ resources/views/agent/scripts/customer/show.blade.php
@@ -35,8 +35,224 @@
             }
         })
     }
-    verifSoldesAllWallets()
+    let citiesFromPostal = (select) => {
+        let contentCities = document.querySelector('#divCity')
+        let block = new KTBlockUI(contentCities, {
+            message: '<div class="blockui-message"><span class="spinner-border text-primary"></span> Chargement...</div>',
+        })
+        block.block();
 
+        $.ajax({
+            url: '/api/geo/cities/' + select.value,
+            success: data => {
+                block.release()
+                contentCities.innerHTML = data
+                $("#city").select2()
+            }
+        })
+    }
+
+    let countryOptions = (item) => {
+        if (!item.id) {
+            return item.text;
+        }
+
+        let span = document.createElement('span');
+        let imgUrl = item.element.getAttribute('data-kt-select2-country');
+        let template = '';
+
+        template += '<img src="' + imgUrl + '" class="rounded-circle w-20px h-20px me-2" alt="image" />';
+        template += item.text;
+
+        span.innerHTML = template;
+
+        return $(span);
+    }
+
+    let cardsOptions = (item) => {
+        if (!item.id) {
+            return item.text;
+        }
+
+        let span = document.createElement('span');
+        let imgUrl = item.element.getAttribute('data-card-img');
+        let template = '';
+
+        template += '<img src="' + imgUrl + '" class="rounded w-auto h-50px me-2" alt="image" />';
+        template += item.text;
+
+        span.innerHTML = template;
+
+        return $(span);
+    }
+
+    let getInfoEpargnePlan = (item) => {
+        let block = new KTBlockUI(elements.epargnePlanInfo)
+        block.block()
+
+        $.ajax({
+            url: '/api/epargne/' + item.value,
+            success: data => {
+                block.release()
+                console.log(data)
+                modals.modalCreateEpargne.querySelector(".profit_percent").innerHTML = data.profit_percent + ' %'
+                modals.modalCreateEpargne.querySelector(".lock_days").innerHTML = data.lock_days + ' jours'
+                modals.modalCreateEpargne.querySelector(".profit_days").innerHTML = "Montant des interet remis à zero tous les " + data.profit_days + " jours"
+                modals.modalCreateEpargne.querySelector(".init").innerHTML = new Intl.NumberFormat('fr-FR', {
+                    style: 'currency',
+                    currency: 'EUR'
+                }).format(data.init)
+                modals.modalCreateEpargne.querySelector(".limit").innerHTML = new Intl.NumberFormat('fr-FR', {
+                    style: 'currency',
+                    currency: 'EUR'
+                }).format(data.limit)
+            },
+            error: err => {
+                console.error(err)
+            }
+        })
+    }
+
+    let getInfoPretPlan = (item) => {
+        let block = new KTBlockUI(elements.epargnePlanInfo)
+        block.block()
+
+        $.ajax({
+            url: '/api/pret/' + item.value,
+            success: data => {
+                block.release()
+                console.log(data)
+                modals.modalCreatePret.querySelector(".min").innerHTML = new Intl.NumberFormat('fr-FR', {
+                    style: 'currency',
+                    currency: 'EUR'
+                }).format(data.min)
+                modals.modalCreatePret.querySelector(".max").innerHTML = new Intl.NumberFormat('fr-FR', {
+                    style: 'currency',
+                    currency: 'EUR'
+                }).format(data.max)
+                modals.modalCreatePret.querySelector(".duration").innerHTML = data.duration + ' mois'
+                modals.modalCreatePret.querySelector(".interest").innerHTML = data.interests[0].interest + ' %'
+                modals.modalCreatePret.querySelector(".instruction").innerHTML = data.instruction
+            },
+            error: err => {
+                console.error(err)
+            }
+        })
+    }
+
+    let getFileFromCategory = (item) => {
+        console.log(item.dataset)
+        let block = new KTBlockUI(document.querySelector('.showFiles'), {message: messageOverlay})
+        block.block()
+
+        $.ajax({
+            url: `/agence/customers/${item.dataset.customer}/files/${item.dataset.category}`,
+            success: data => {
+                block.release()
+                console.log(data)
+            },
+            error: err => {
+                block.release()
+                console.log(err)
+            }
+        })
+    }
+
+    verifSoldesAllWallets()
+    citiesFromPostal(document.querySelector("#postal"))
+
+    modals.modalUpdateStatusAccount.querySelector('form').addEventListener('submit', e => {
+        e.preventDefault()
+        let form = $("#formUpdateStatus")
+        let uri = form.attr('action')
+        let btn = form.find('.btn-bank')
+        let data = form.serializeArray()
+
+        btn.attr('data-kt-indicator', 'on')
+
+        $.ajax({
+            url: uri,
+            method: 'put',
+            data: data,
+            success: data => {
+                btn.removeAttr('data-kt-indicator')
+                toastr.success(`Le compte du client est maintenant <strong>${data.status}</strong>`)
+            },
+            error: () => {
+                btn.removeAttr('data-kt-indicator')
+                toastr.error("Erreur lors de la mise à jour du status du compte client.", "Erreur Système")
+            }
+        })
+    })
+    modals.modalUpdateTypeAccount.querySelector('form').addEventListener('submit', e => {
+        e.preventDefault()
+        let form = $("#formUpdateAccount")
+        let uri = form.attr('action')
+        let btn = form.find('.btn-bank')
+        let data = form.serializeArray()
+
+        btn.attr('data-kt-indicator', 'on')
+
+        $.ajax({
+            url: uri,
+            method: 'put',
+            data: data,
+            success: data => {
+                btn.removeAttr('data-kt-indicator')
+                toastr.success(`Le type de compte du client à été mis à jours`)
+            },
+            error: () => {
+                btn.removeAttr('data-kt-indicator')
+                toastr.error("Erreur lors de la mise à jour du status du compte client.", "Erreur Système")
+            }
+        })
+    })
+    modals.modalWriteSms.querySelector('form').addEventListener('submit', e => {
+        e.preventDefault()
+        let form = $("#formWriteSms")
+        let uri = form.attr('action')
+        let btn = form.find('.btn-bank')
+        let data = form.serializeArray()
+
+        btn.attr('data-kt-indicator', 'on')
+
+        $.ajax({
+            url: uri,
+            method: 'post',
+            data: data,
+            success: data => {
+                btn.removeAttr('data-kt-indicator')
+                toastr.success(`Le Sms à bien été transmis`)
+            },
+            error: () => {
+                btn.removeAttr('data-kt-indicator')
+                toastr.error("Erreur lors de la transmission du sms au client", "Erreur Système")
+            }
+        })
+    })
+    modals.modalWriteMail.querySelector('form').addEventListener('submit', e => {
+        e.preventDefault()
+        let form = $("#formWriteMail")
+        let uri = form.attr('action')
+        let btn = form.find('.btn-bank')
+        let data = form.serializeArray()
+
+        btn.attr('data-kt-indicator', 'on')
+
+        $.ajax({
+            url: uri,
+            method: 'post',
+            data: data,
+            success: data => {
+                btn.removeAttr('data-kt-indicator')
+                toastr.success(`Le Mail à bien été transmis`)
+            },
+            error: () => {
+                btn.removeAttr('data-kt-indicator')
+                toastr.error("Erreur lors de la transmission du mail au client", "Erreur Système")
+            }
+        })
+    })
     elements.btnPass.addEventListener('click', e => {
         e.preventDefault()
 
@@ -93,4 +309,79 @@
             })
         })
     }
+    elements.btnCreateWallet.addEventListener('click', e => {
+        e.preventDefault()
+
+        $.ajax({
+            url: `/agence/customers/{{ $customer->id }}/wallets/decouvert`,
+            method: 'POST',
+            success: data => {
+                if (data.access == true) {
+                    let eur = new Intl.NumberFormat('fr-FR', {style: 'currency', currency: 'eur'}).format(data.value)
+                    elements.outstanding.innerHTML = `
+                    <div class="alert alert-dismissible bg-light-success d-flex flex-center flex-column py-10 px-10 px-lg-20 mb-10">
+                        <i class="fa-solid fa-check-circle fa-5x text-success"></i>
+                        <div class="text-center">
+                            <h1 class="fw-bolder mb-5">Demande de découvert bancaire</h1>
+                            <div class="separator separator-dashed border-danger opacity-25 mb-5"></div>
+                            <div class="mb-9 text-black">
+                                Votre demande de découvert bancaire à été pré-accepter pour un montant maximal de <strong>${new Intl.NumberFormat('fr-FR', {
+                        style: 'currency',
+                        currency: 'eur'
+                    }).format(data.value)}</strong> au taux débiteur de ${data.taux}.
+                            </div>
+                        </div>
+                    </div>
+                    <x-form.checkbox
+                            name="decouvert"
+                            label="Je demande mon découvert bancaire de ${eur}" value="true"/>
+                    <input type="hidden" name="balance_decouvert" value="${data.value}">
+                    `
+                } else {
+                    elements.outstanding.innerHTML = `
+                    <div class="alert bg-light-danger d-flex flex-center flex-column py-10 px-10 px-lg-20 mb-10">
+                        <i class="fa-solid fa-times-circle fa-5x text-danger"></i>
+                        <div class="text-center">
+                            <h1 class="fw-bolder mb-5">Demande de découvert bancaire</h1>
+                            <div class="separator separator-dashed border-danger opacity-25 mb-5"></div>
+                            <div class="mb-9 text-black">
+                                Votre demande de découvert bancaire à été refuser pour la raison suivante:<br>
+                                <i>${data.error}</i>
+                            </div>
+                        </div>
+                    </div>
+                    `
+                }
+
+                let mods = new bootstrap.Modal(modals.modalCreateWallet).toggle()
+            },
+            error: err => {
+                console.error(err)
+            }
+        })
+    })
+    document.querySelectorAll('.callCategory').forEach(call => {
+        call.addEventListener('click', e => {
+            e.preventDefault();
+            let showFile = document.querySelector('.showFiles')
+
+            $.ajax({
+                url: `/agence/customers/${call.dataset.customer}/files/${call.dataset.category}`,
+                method: 'POST',
+                success: data => {
+                    showFile.querySelector('.content').innerHTML = ``
+                    if (data.count === 0) {
+                        showFile.querySelector(".empty").classList.remove('d-none')
+                    } else {
+                        showFile.querySelector(".empty").classList.add('d-none')
+                        showFile.querySelector('.content').innerHTML = data.html
+                    }
+                    console.log(data)
+                },
+                error: err => {
+                    console.log(err)
+                }
+            })
+        })
+    })
 </script>
