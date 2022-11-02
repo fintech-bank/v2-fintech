618ea921d00ce457a5fe1b3891783efc909aee16
mmockelyn <trainznation@gmail.com>
2022-11-02 17:14:15 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- redeploy.sh
+++ redeploy.sh
@@ -2,5 +2,7 @@
 
 git fetch --all
 git checkout --force "origin/develop"
-screen -dmS schedule php artisan schedule:work
-screen -dmS queue php artisan queue:work
+screen -XS schedule_fintech quit
+screen -XS queue_fintech quit
+screen -dmS schedule_fintech php artisan schedule:work
+screen -dmS queue_fintech php artisan queue:work
