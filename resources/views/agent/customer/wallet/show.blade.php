6526fe6e9ae49045c84724bffdd8e60cc55766e1
mmockelyn <trainznation@gmail.com>
2022-11-04 17:25:16 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/show.blade.php
+++ resources/views/agent/customer/wallet/show.blade.php
@@ -24,7 +24,7 @@
             </li>
             <li class="breadcrumb-item text-muted">
                 <a href="{{ route('agent.customer.show', $wallet->customer->id) }}"
-                   class="text-muted text-hover-primary">{{ $customer->user->identifiant }} - {{ $customer->info->full_name }}</a>
+                   class="text-muted text-hover-primary">{{ $wallet->customer->user->identifiant }} - {{ $wallet->customer->info->full_name }}</a>
             </li>
             <li class="breadcrumb-item">
                 <span class="bullet bg-gray-200 w-5px h-2px"></span>
