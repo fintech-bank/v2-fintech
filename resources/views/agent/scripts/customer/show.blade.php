5c597af35f83fa4a8f69826f54f364148bd14169
mmockelyn <trainznation@gmail.com>
2022-11-05 23:46:50 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/show.blade.php
+++ resources/views/agent/scripts/customer/show.blade.php
@@ -245,61 +245,7 @@
                     ${signate[file.signable].fn}
                 </td>
                 <td class="text-end" data-kt-filemanager-table="action_dropdown"><div class="d-flex justify-content-end">
-                       <!--begin::Share link-->
-                       <div class="ms-2" data-kt-filemanger-table="copy_link">
-                           <button type="button" class="btn btn-sm btn-icon btn-light btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                               <!--begin::Svg Icon | path: icons/duotune/coding/cod007.svg-->
-                               <span class="svg-icon svg-icon-5 m-0">
-														<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-															<path opacity="0.3" d="M18.4 5.59998C18.7766 5.9772 18.9881 6.48846 18.9881 7.02148C18.9881 7.55451 18.7766 8.06577 18.4 8.44299L14.843 12C14.466 12.377 13.9547 12.5887 13.4215 12.5887C12.8883 12.5887 12.377 12.377 12 12C11.623 11.623 11.4112 11.1117 11.4112 10.5785C11.4112 10.0453 11.623 9.53399 12 9.15698L15.553 5.604C15.9302 5.22741 16.4415 5.01587 16.9745 5.01587C17.5075 5.01587 18.0188 5.22741 18.396 5.604L18.4 5.59998ZM20.528 3.47205C20.0614 3.00535 19.5074 2.63503 18.8977 2.38245C18.288 2.12987 17.6344 1.99988 16.9745 1.99988C16.3145 1.99988 15.661 2.12987 15.0513 2.38245C14.4416 2.63503 13.8876 3.00535 13.421 3.47205L9.86801 7.02502C9.40136 7.49168 9.03118 8.04568 8.77863 8.6554C8.52608 9.26511 8.39609 9.91855 8.39609 10.5785C8.39609 11.2384 8.52608 11.8919 8.77863 12.5016C9.03118 13.1113 9.40136 13.6653 9.86801 14.132C10.3347 14.5986 10.8886 14.9688 11.4984 15.2213C12.1081 15.4739 12.7616 15.6039 13.4215 15.6039C14.0815 15.6039 14.7349 15.4739 15.3446 15.2213C15.9543 14.9688 16.5084 14.5986 16.975 14.132L20.528 10.579C20.9947 10.1124 21.3649 9.55844 21.6175 8.94873C21.8701 8.33902 22.0001 7.68547 22.0001 7.02551C22.0001 6.36555 21.8701 5.71201 21.6175 5.10229C21.3649 4.49258 20.9947 3.93867 20.528 3.47205Z" fill="currentColor"></path>
-															<path d="M14.132 9.86804C13.6421 9.37931 13.0561 8.99749 12.411 8.74695L12 9.15698C11.6234 9.53421 11.4119 10.0455 11.4119 10.5785C11.4119 11.1115 11.6234 11.6228 12 12C12.3766 12.3772 12.5881 12.8885 12.5881 13.4215C12.5881 13.9545 12.3766 14.4658 12 14.843L8.44699 18.396C8.06999 18.773 7.55868 18.9849 7.02551 18.9849C6.49235 18.9849 5.98101 18.773 5.604 18.396C5.227 18.019 5.0152 17.5077 5.0152 16.9745C5.0152 16.4413 5.227 15.93 5.604 15.553L8.74701 12.411C8.28705 11.233 8.28705 9.92498 8.74701 8.74695C8.10159 8.99737 7.5152 9.37919 7.02499 9.86804L3.47198 13.421C2.52954 14.3635 2.00009 15.6417 2.00009 16.9745C2.00009 18.3073 2.52957 19.5855 3.47202 20.528C4.41446 21.4704 5.69269 21.9999 7.02551 21.9999C8.35833 21.9999 9.63656 21.4704 10.579 20.528L14.132 16.975C14.5987 16.5084 14.9689 15.9544 15.2215 15.3447C15.4741 14.735 15.6041 14.0815 15.6041 13.4215C15.6041 12.7615 15.4741 12.108 15.2215 11.4983C14.9689 10.8886 14.5987 10.3347 14.132 9.86804Z" fill="currentColor"></path>
-														</svg>
-													</span>
-                               <!--end::Svg Icon-->
-                           </button>
-                           <!--begin::Menu-->
-                           <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-300px" data-kt-menu="true">
-                               <!--begin::Card-->
-                               <div class="card card-flush">
-                                   <div class="card-body p-5">
-                                       <!--begin::Loader-->
-                                       <div class="d-flex" data-kt-filemanger-table="copy_link_generator">
-                                           <!--begin::Spinner-->
-                                           <div class="me-5" data-kt-indicator="on">
-																	<span class="indicator-progress">
-																		<span class="spinner-border spinner-border-sm align-middle ms-2"></span>
-																	</span>
-                                           </div>
-                                           <!--end::Spinner-->
-                                           <!--begin::Label-->
-                                           <div class="fs-6 text-dark">Generating Share Link...</div>
-                                           <!--end::Label-->
-                                       </div>
-                                       <!--end::Loader-->
-                                       <!--begin::Link-->
-                                       <div class="d-flex flex-column text-start d-none" data-kt-filemanger-table="copy_link_result">
-                                           <div class="d-flex mb-3">
-                                               <!--begin::Svg Icon | path: icons/duotune/arrows/arr085.svg-->
-                                               <span class="svg-icon svg-icon-2 svg-icon-success me-3">
-																		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-																			<path d="M9.89557 13.4982L7.79487 11.2651C7.26967 10.7068 6.38251 10.7068 5.85731 11.2651C5.37559 11.7772 5.37559 12.5757 5.85731 13.0878L9.74989 17.2257C10.1448 17.6455 10.8118 17.6455 11.2066 17.2257L18.1427 9.85252C18.6244 9.34044 18.6244 8.54191 18.1427 8.02984C17.6175 7.47154 16.7303 7.47154 16.2051 8.02984L11.061 13.4982C10.7451 13.834 10.2115 13.834 9.89557 13.4982Z" fill="currentColor"></path>
-																		</svg>
-																	</span>
-                                               <!--end::Svg Icon-->
-                                               <div class="fs-6 text-dark">Share Link Generated</div>
-                                           </div>
-                                           <input type="text" class="form-control form-control-sm" value="https://path/to/file/or/folder/">
-                                           <div class="text-muted fw-normal mt-2 fs-8 px-3">Read only.
-                                               <a href="/metronic8/demo1/../demo1/apps/file-manager/settings/.html" class="ms-2">Change permissions</a></div>
-                                       </div>
-                                       <!--end::Link-->
-                                   </div>
-                               </div>
-                               <!--end::Card-->
-                           </div>
-                           <!--end::Menu-->
-                       </div>
-                       <!--end::Share link-->
+
                        <!--begin::More-->
                        <div class="ms-2">
                            <button type="button" class="btn btn-sm btn-icon btn-light btn-active-light-primary" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
