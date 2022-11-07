f9d91d9db4a5bb1a4e8f7ad393346e380c4314ce
mmockelyn <trainznation@gmail.com>
2022-11-07 17:45:57 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/show.blade.php
+++ resources/views/agent/scripts/customer/show.blade.php
@@ -285,7 +285,7 @@
             url: `/api/manager/files/${file.dataset.documentReference}`,
             success: data => {
                 console.log(data)
-                data.signate === 0 ? modals.modalContentFile.querySelector('.modal-footer').classList.add('d-none') : modals.modalContentFile.querySelector('.modal-footer').classList.remove('d-none');
+                data.signable === 0 ? modals.modalContentFile.querySelector('.modal-footer').classList.add('d-none') : modals.modalContentFile.querySelector('.modal-footer').classList.remove('d-none');
                 modal.show()
                 pdf(data.url_folder)
 
