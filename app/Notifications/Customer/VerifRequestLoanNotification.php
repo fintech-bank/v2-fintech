6896855cf105b5f4fea3c3f93fc787c72216aee3
mmockelyn <trainznation@gmail.com>
2022-11-07 20:37:19 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Notifications/Customer/VerifRequestLoanNotification.php
+++ app/Notifications/Customer/VerifRequestLoanNotification.php
@@ -1,61 +1,115 @@
 <?php
-
 namespace App\Notifications\Customer;
 
+use Akibatech\FreeMobileSms\Notifications\FreeMobileChannel;
+use Akibatech\FreeMobileSms\Notifications\FreeMobileMessage;
+use App\Models\Customer\CustomerPret;
+use App\Models\Customer\CustomerSepa;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use NotificationChannels\Twilio\TwilioChannel;
+use NotificationChannels\Twilio\TwilioSmsMessage;
 
 class VerifRequestLoanNotification extends Notification
 {
-    use Queueable;
+
+    public string $title;
+    public string $link;
+    public string $message;
+    public CustomerPret $pret;
 
     /**
-     * Create a new notification instance.
-     *
-     * @return void
+     * @param CustomerPret $pret
      */
-    public function __construct()
+    public function __construct(CustomerPret $pret)
     {
-        //
+        $this->title = "Votre demande de pret bancaire";
+        $this->message = $this->getMessage();
+        $this->link = "";
+        $this->pret = $pret;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
+    private function getMessage()
+    {
+        $message = "Votre demande de pret <strong>N°{$this->pret->reference}</strong> à été enregistré en date du <strong>{$this->pret->created_at->format('d/m/Y')}</strong> et est maintenant en cours d'étude.";
+        $message .= "Une notification vous sera envoyer lors de son acceptation ou de son refus par le service financier.";
+        return $message;
+    }
+
+    private function choiceChannel()
+    {
+        if (config("app.env") == "local") {
+            if($this->pret->customer->setting->notif_sms) {
+                return [FreeMobileChannel::class];
+            }
+
+            if($this->pret->customer->setting->notif_mail) {
+                return "mail";
+            }
+
+            return "database";
+        } else {
+
+            if($this->pret->customer->setting->notif_sms) {
+                return [TwilioChannel::class];
+            }
+
+            if($this->pret->customer->setting->notif_mail) {
+                return "mail";
+            }
+
+            return "database";
+        }
+    }
+
     public function via($notifiable)
     {
-        return ['mail'];
+        return $this->choiceChannel();
     }
 
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+        $message = (new MailMessage);
+        $message->subject($this->title)
+            ->view('emails.customer.verif_request_loan', [
+                'pret' => $this->pret,
+                'content' => $this->message,
+                'customer' => $this->pret->customer
+            ]);
+
+        $message->actionUrl = $this->link;
+        $message->actionText = "Plus d'information";
+
+        return $message;
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function toArray($notifiable)
     {
         return [
-            //
+            "icon" => "fa-euro-sign",
+            "color" => "info",
+            "title" => $this->title,
+            "text" => $this->message,
+            "time" => now(),
+            "link" => $this->link,
         ];
     }
+
+    public function toFreeMobile($notifiable)
+    {
+        $message = (new FreeMobileMessage());
+        $message->message(strip_tags($this->message));
+
+        return $message;
+    }
+
+    public function toTwilio($notifiable)
+    {
+        $message = (new TwilioSmsMessage());
+        $message->content(strip_tags($this->message));
+
+        return $message;
+    }
 }
+?>
