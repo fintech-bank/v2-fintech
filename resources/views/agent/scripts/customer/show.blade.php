ec6d9c6d4f10fa7216b53d0342059e728a5f3496
mmockelyn <trainznation@gmail.com>
2022-11-07 17:32:07 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/show.blade.php
+++ resources/views/agent/scripts/customer/show.blade.php
@@ -42,7 +42,7 @@
         blockShowFiles: messageBlock(elements.cardShowFiles, "Chargement des fichiers")
     }
 
-    let pdf = (canvas, url) => {
+    let pdf = (url) => {
         let pdfjsLib = window['pdfjs-dist/build/pdf'];
         pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.0.279/pdf.worker.min.js';
 
