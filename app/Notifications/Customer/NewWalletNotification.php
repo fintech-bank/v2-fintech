68fcb4c1511430aeea7e12305d0c3e6aa46a2b26
mmockelyn <trainznation@gmail.com>
2022-11-10 13:45:13 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Notifications/Customer/NewWalletNotification.php
+++ app/Notifications/Customer/NewWalletNotification.php
@@ -1,84 +1,87 @@
 <?php
-
 namespace App\Notifications\Customer;
 
+use Akibatech\FreeMobileSms\Notifications\FreeMobileChannel;
+use Akibatech\FreeMobileSms\Notifications\FreeMobileMessage;
 use App\Models\Customer\Customer;
 use App\Models\Customer\CustomerWallet;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use NotificationChannels\Twilio\TwilioChannel;
+use NotificationChannels\Twilio\TwilioSmsMessage;
 
 class NewWalletNotification extends Notification
 {
-    use Queueable;
 
+    public string $title;
+    public string $link;
+    public string $message;
     public Customer $customer;
     public CustomerWallet $wallet;
-    public array $documents;
 
     /**
-     * Create a new notification instance.
-     *
      * @param Customer $customer
      * @param CustomerWallet $wallet
-     * @param array $documents
      */
-    public function __construct(Customer $customer, CustomerWallet $wallet, array $documents)
+    public function __construct(Customer $customer, CustomerWallet $wallet)
     {
-        //
+        $this->title = "Nouveau compte bancaire disponible";
+        $this->message = $this->getMessage();
+        $this->link = "";
         $this->customer = $customer;
-        $this->documents = $documents;
         $this->wallet = $wallet;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param mixed $notifiable
-     * @return array
-     */
-    public function via($notifiable)
+    private function getMessage()
     {
-        return ['mail', 'database'];
+        $message = "<p>Votre compte bancaire N°".$this->wallet->number_account." à été créer par notre service financier.</p>";
+        $message .= "<p>Afin de terminer l'ouverture de ce compte, un e-mail vous à été envoyer avec une demande de signature pour le document : 'Convention de compte'</p>";
+        return $message;
     }
 
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param mixed $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
+    private function choiceChannel()
+    {
+        if (config("app.env") == "local") {
+            if($this->customer->setting->notif_mail) {
+                return "mail";
+            }
+
+            return "database";
+        } else {
+            if($this->customer->setting->notif_mail) {
+                return "mail";
+            }
+
+            return "database";
+        }
+    }
+
+    public function via($notifiable)
+    {
+        return $this->choiceChannel();
+    }
+
     public function toMail($notifiable)
     {
-        $message = (new MailMessage())->view('emails.customer.new_wallet', [
-            'customer' => $this->customer,
-            'wallet' => $this->wallet
+        $message = (new MailMessage);
+        $message->view("emails.customer.new_wallet", [
+            "content" => $this->message,
+            "customer" => $this->customer
         ]);
-
-        $message->success();
-        $message->introLines = [
-            'Bien évidement nous continuons à vous accompagnés dans votre quotidien !'
-        ];
-        $message->subject("Votre nouveau compte bancaire est disponible");
 
         return $message;
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param mixed $notifiable
-     * @return array
-     */
     public function toArray($notifiable)
     {
         return [
-            'icon' => 'fa-wallet',
-            'color' => 'primary',
-            'title' => "Nouveau compte bancaire",
-            'text' => "Un nouveau compte bancaire est disponible pour son utilisation.",
-            'time' => now()->shortAbsoluteDiffForHumans(),
+            "icon" => "fa-wallet",
+            "color" => "success",
+            "title" => $this->title,
+            "text" => $this->message,
+            "time" => now(),
+            "link" => $this->link,
         ];
     }
 }
