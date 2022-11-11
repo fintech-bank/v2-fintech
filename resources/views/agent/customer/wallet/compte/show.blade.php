2136c7c38a5b7b4915e25b2a6e8b9f9a6b474132
mmockelyn <trainznation@gmail.com>
2022-11-11 23:22:17 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/compte/show.blade.php
+++ resources/views/agent/customer/wallet/compte/show.blade.php
@@ -244,6 +244,19 @@
                     </table>
                 </div>
             </div>
+            <div class="card shadow-sm">
+                <div class="card-header">
+                    <h3 class="card-title"></h3>
+                    <div class="card-toolbar">
+                        <!--<button type="button" class="btn btn-sm btn-light">
+                            Action
+                        </button>-->
+                    </div>
+                </div>
+                <div class="card-body">
+                    
+                </div>   
+            </div>
         </div>
     </div>
     <div class="modal fade" tabindex="-1" id="showRib">
