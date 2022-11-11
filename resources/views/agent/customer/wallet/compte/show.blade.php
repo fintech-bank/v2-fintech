5acd65b2dd0244ce6c53370cda935cb1235cd0ea
mmockelyn <trainznation@gmail.com>
2022-11-12 00:05:29 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/compte/show.blade.php
+++ resources/views/agent/customer/wallet/compte/show.blade.php
@@ -289,8 +289,34 @@
                         </div>
                     </div>
                 </div>
-                <div class="card-body">
-
+                <div class="card-body pt-0">
+                    <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_transaction_table">
+                        <thead>
+                            <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
+                                <th class="min-w-100px">Date</th>
+                                <th class="min-w-175px">Libellé</th>
+                                <th class="text-end">Montant</th>
+                                <th class="text-end"></th>
+                            </tr>
+                        </thead>
+                        <tbody class="fw-semibold text-gray-600">
+                            @foreach($wallet->transactions()->where('confirmed', true)->get() as $transaction)
+                                <tr>
+                                    <td data-order="{{ $transaction->confirmed_at->format('Y-m-d') }}">{{ $transaction->confirmed_at->format("d/m/Y") }}</td>
+                                    <td class="d-none" data-order="{{ $transaction->type }}"></td>
+                                    <div class="d-flex flex-row align-items-center">
+                                        {!! $transaction->type_symbol !!}
+                                        <div class="d-flex flex-column">
+                                            {{ $transaction->description }}
+                                            <div class="text-muted">{{ $transaction->designation }}</div>
+                                        </div>
+                                    </div>
+                                    <td>{{ $transaction->amount_format }}</td>
+                                    <td></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
