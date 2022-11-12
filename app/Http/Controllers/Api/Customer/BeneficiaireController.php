26758c8a2e16c260f1e1a23d45b55eb34b58d940
mmockelyn <trainznation@gmail.com>
2022-11-12 21:47:06 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Api/Customer/BeneficiaireController.php
+++ app/Http/Controllers/Api/Customer/BeneficiaireController.php
@@ -3,12 +3,38 @@
 namespace App\Http\Controllers\Api\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer\CustomerBeneficiaire;
+use App\Notifications\Customer\NewBeneficiaireNotification;
 use Illuminate\Http\Request;
+use Intervention\Validation\Rules\Iban;
 
 class BeneficiaireController extends Controller
 {
     public function store($customer_id, Request $request)
     {
-        dd($request->all());
+        $request->validate([
+            'type' => 'required',
+            'bankname' => 'required',
+            'iban' => new Iban()
+        ]);
+
+        $beneficiaire = CustomerBeneficiaire::create([
+            'uuid' => \Str::uuid(),
+            'type' => $request->get('type'),
+            'company' => $request->get('type') != 'retail' ? $request->get('company') : null,
+            'civility' => $request->get('type') == 'retail' ? $request->get('civility') : null,
+            'firstname' => $request->get('type') == 'retail' ? $request->get('firstname') : null,
+            'lastname' => $request->get('type') == 'retail' ? $request->get('lastname') : null,
+            'bankname' => $request->get('bankname'),
+            'iban' => $request->get('iban'),
+            'bic' => $request->get('bic'),
+            'titulaire' => $request->has('titulaire') ? 1 : 0,
+            'customer_id' => $customer_id,
+            'bank_id' => $request->get('bank_id')
+        ]);
+
+        $beneficiaire->customer->info->notify(new NewBeneficiaireNotification($beneficiaire->customer));
+
+        return response()->json();
     }
 }
