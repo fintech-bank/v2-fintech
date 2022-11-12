ef63f4367a2ad9a1b51d0ca89fd8d1277a6b8159
mmockelyn <trainznation@gmail.com>
2022-11-12 21:52:01 +0100

    feat(Agent Client Création Client) : Edition de la partie client

---
+++ app/Notifications/Customer/NewBeneficiaireNotification.php
@@ -0,0 +1,80 @@
+<?php
+namespace App\Notifications\Customer;
+
+use Akibatech\FreeMobileSms\Notifications\FreeMobileChannel;
+use Akibatech\FreeMobileSms\Notifications\FreeMobileMessage;
+use App\Models\Customer\Customer;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use NotificationChannels\Twilio\TwilioChannel;
+use NotificationChannels\Twilio\TwilioSmsMessage;
+
+class NewBeneficiaireNotification extends Notification
+{
+
+    public string $title;
+    public string $link;
+    public string $message;
+    public Customer $customer;
+
+    public function __construct(Customer $customer)
+    {
+        $this->title = "Nouveau Bénéficiaire";
+        $this->message = $this->getMessage();
+        $this->link = "";
+        $this->customer = $customer;
+    }
+
+    private function getMessage()
+    {
+        $message = "Un nouveau bénéficiaire à été ajouté à votre espace.<br>Vous pouvez effectuer des virements vers ce bénéficiaire dès maintenant.";
+        return $message;
+    }
+
+    private function choiceChannel()
+    {
+        if (config("app.env") == "local") {
+            if($this->customer->setting->notif_mail) {
+                return "mail";
+            }
+
+            return "database";
+        } else {
+            if($this->customer->setting->notif_mail) {
+                return "mail";
+            }
+
+            return "database";
+        }
+    }
+
+    public function via($notifiable)
+    {
+        return $this->choiceChannel();
+    }
+
+    public function toMail($notifiable)
+    {
+        $message = (new MailMessage);
+        $message->subject($this->title);
+        $message->view("emails.customer.new_beneficiaire", [
+            "content" => $this->message,
+            "customer" => $this->customer
+        ]);
+
+        return $message;
+    }
+
+    public function toArray($notifiable)
+    {
+        return [
+            "icon" => "fa-user-circle",
+            "color" => "primary",
+            "title" => $this->title,
+            "text" => $this->message,
+            "time" => now(),
+            "link" => $this->link,
+        ];
+    }
+}
