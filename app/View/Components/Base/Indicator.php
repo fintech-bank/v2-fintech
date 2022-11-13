8ad549d7f5922d8d1649a4004fcb793906ea375d
mmockelyn <trainznation@gmail.com>
2022-11-13 18:09:38 +0100

    feat(Agent Client Création Client) : Edition de la partie client

---
+++ app/View/Components/Base/Indicator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\View\Components\Base;
+
+use Illuminate\View\Component;
+
+class Indicator extends Component
+{
+    private bool $textOnly;
+    /**
+     * @var null
+     */
+    private $text;
+
+    /**
+     * Create a new component instance.
+     *
+     * @param bool $textOnly
+     * @param null $text
+     */
+    public function __construct($textOnly = true, $text = null)
+    {
+        //
+        $this->textOnly = $textOnly;
+        $this->text = $text;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.base.indicator');
+    }
+}
