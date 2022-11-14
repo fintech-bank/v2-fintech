ffb1b2f757f0346e16cfa7636b1473f3bceebc1d
mmockelyn <trainznation@gmail.com>
2022-11-14 21:52:26 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Console/Commands/LifeCommand.php
+++ app/Console/Commands/LifeCommand.php
@@ -212,6 +212,7 @@ class LifeCommand extends Command
         $this->line("Date: ".now()->format("d/m/Y à H:i"));
         $this->line('Nombre de nouveau client: ' . $r);
         $this->output->table(['client', 'Etat du compte'], $arr);
+        dd($this->getOutput());
         $this->slack->send("Nouveau client", [$this->getOutput()]);
     }
 
