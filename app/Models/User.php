fc1c989f1ab7c2de2971ba732b7b3c9c8bcddbec
mmockelyn <trainznation@gmail.com>
2022-11-14 14:32:23 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Models/User.php
+++ app/Models/User.php
@@ -330,11 +330,14 @@ class User extends Authenticatable
                     <div class="fs-1 text-white fw-bolder">Information sur le mot de passe</div>
                     <div class="fs-3 text-white">Le mot de passe par default est toujours actif pour ce client.</div>
                 </div>
-                <x-base.button
-                    class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-sm btn-outline btn-outline-light ms-sm-auto btnNotifyPassword"
-                    text="Notifier le client"
-                    data="{{ [['name' => 'customer', 'value' => $customer->id]] }}"
-                />
+                <button class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-outline btn-outline-light ms-sm-auto btnNotifyPassword">
+                    <span class="indicator-label">
+                        Notifier le client
+                    </span>
+                    <span class="indicator-progress">
+                        Veuillez patienter... <span class="spinner-border spinner-border-sm align-middle ms-2"></span>
+                    </span>
+                </button>
             </div>
             <?php
         }
