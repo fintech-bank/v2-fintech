72478ea943b177b82fd31150fb138e31ab867f1c
mmockelyn <trainznation@gmail.com>
2022-11-14 14:21:53 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Models/User.php
+++ app/Models/User.php
@@ -330,9 +330,7 @@ class User extends Authenticatable
                     <div class="fs-1 text-white fw-bolder">Information sur le mot de passe</div>
                     <div class="fs-3 text-white">Le mot de passe par default est toujours actif pour ce client.</div>
                 </div>
-                <div class="d-flex w-auto justify-content-end">
-                    <button class="btn btn-outline btn-outline-light">Notifier le client</button>
-                </div>
+                <button class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto">Notifier le client</button>
             </div>
             <?php
         }
