98b86c28fdb3ad9ab83a41a614db4c1dd12f985c
mmockelyn <trainznation@gmail.com>
2022-11-14 02:58:08 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- config/app.php
+++ config/app.php
@@ -199,6 +199,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
         App\Providers\LaravelBackupPanelServiceProvider::class,
         App\Providers\MessengerServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
