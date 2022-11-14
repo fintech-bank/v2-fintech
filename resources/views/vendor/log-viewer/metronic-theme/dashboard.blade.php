dc067ee8c5e99d8d918c9c9b6b96a0159687c67b
mmockelyn <trainznation@gmail.com>
2022-11-14 17:17:26 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/vendor/log-viewer/metronic-theme/dashboard.blade.php
+++ resources/views/vendor/log-viewer/metronic-theme/dashboard.blade.php
@@ -38,7 +38,9 @@
     </div>
 
     <div class="row">
-        <div class="col-md-4 col-sm-12"></div>
+        <div class="col-md-4 col-sm-12">
+            <div id="stats-doughnut-chart" height="300" class="mb-3"></div>
+        </div>
         <div class="col-md-8 col-sm-12">
             <div class="row">
                 @foreach($percents as $level => $item)
@@ -63,5 +65,106 @@
 @endsection
 
 @section("script")
+    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/radar.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+    <script type="text/javascript">
+        am5.ready(() => {
+            let root = am5.Root.new('stats-doughnut-chart')
+            root.setTheme([
+                am5themes_Animated.new(root)
+            ])
 
+            let chart = root.container.children.push(am5radar.RadarChart.new(root, {
+                panX: false,
+                panY: false,
+                wheelX: "panX",
+                wheelY: "zoomX",
+                innerRadius: am5.percent(20),
+                startAngle: -90,
+                endAngle: 180
+            }))
+
+            let data = [{
+                category: "Urgence",
+                value: 80,
+                full: 100,
+                columnSettings: {
+                    fill: chart.get("colors").getIndex(0)
+                }
+            }];
+
+            let cursor = chart.set("cursor", am5radar.RadarCursor.new(root, {
+                behavior: "zoomX"
+            }));
+
+            cursor.lineY.set("visible", false);
+
+            let xRenderer = am5radar.AxisRendererCircular.new(root, {
+                //minGridDistance: 50
+            });
+
+            xRenderer.labels.template.setAll({
+                radius: 10
+            });
+
+            xRenderer.grid.template.setAll({
+                forceHidden: true
+            });
+
+            let xAxis = chart.xAxes.push(am5xy.ValueAxis.new(root, {
+                renderer: xRenderer,
+                min: 0,
+                max: 100,
+                strictMinMax: true,
+                numberFormat: "#'%'",
+                tooltip: am5.Tooltip.new(root, {})
+            }));
+
+            let yRenderer = am5radar.AxisRendererRadial.new(root, {
+                minGridDistance: 20
+            });
+
+            yRenderer.labels.template.setAll({
+                centerX: am5.p100,
+                fontWeight: "500",
+                fontSize: 18,
+                templateField: "columnSettings"
+            });
+
+            yRenderer.grid.template.setAll({
+                forceHidden: true
+            });
+
+            let yAxis = chart.yAxes.push(am5xy.CategoryAxis.new(root, {
+                categoryField: "category",
+                renderer: yRenderer
+            }));
+
+            yAxis.data.setAll(data);
+
+            var series1 = chart.series.push(am5radar.RadarColumnSeries.new(root, {
+                xAxis: xAxis,
+                yAxis: yAxis,
+                clustered: false,
+                valueXField: "full",
+                categoryYField: "category",
+                fill: root.interfaceColors.get("alternativeBackground")
+            }));
+
+            series1.columns.template.setAll({
+                width: am5.p100,
+                fillOpacity: 0.08,
+                strokeOpacity: 0,
+                cornerRadius: 20
+            });
+
+            series1.data.setAll(data);
+
+            series1.appear(1000);
+            chart.appear(1000, 100);
+        })
+    </script>
 @endsection
