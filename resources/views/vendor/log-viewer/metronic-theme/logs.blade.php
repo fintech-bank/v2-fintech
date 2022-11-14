6bb369377a6db8a8faa049f7103c0414d84ae38a
mmockelyn <trainznation@gmail.com>
2022-11-14 17:47:23 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/vendor/log-viewer/metronic-theme/logs.blade.php
+++ resources/views/vendor/log-viewer/metronic-theme/logs.blade.php
@@ -62,7 +62,7 @@
                                 @if ($key == 'date')
                                     <span class="badge badge-primary">{{ $value }}</span>
                                 @elseif ($value == 0)
-                                    <span class="badge level-{{ $key }}">{{ $value }}</span>
+                                    <span class="badge level-{{ $key }} {{ $key == 'all' ? 'badge-secondary' : '' }}">{{ $value }}</span>
                                 @else
                                     <a href="{{ route('log-viewer::logs.filter', [$date, $key]) }}">
                                         <span class="badge level-{{ $key }}">{{ $value }}</span>
