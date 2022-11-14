d18685f2722e7c23167880883319b819e92ef6c0
mmockelyn <trainznation@gmail.com>
2022-11-14 16:41:14 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/vendor/log-viewer/metronic-theme/dashboard.blade.php
+++ resources/views/vendor/log-viewer/metronic-theme/dashboard.blade.php
@@ -32,6 +32,20 @@
 @endsection
 
 @section("content")
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+        </li>
+    </ul>
     <div class="row">
         <div class="col-md-4 col-sm-12"></div>
         <div class="col-md-8 col-sm-12">
