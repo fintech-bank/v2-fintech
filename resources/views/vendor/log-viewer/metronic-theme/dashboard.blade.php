1547da331820de7c2235ac54162b24ee2ec9d6b0
mmockelyn <trainznation@gmail.com>
2022-11-14 16:44:29 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/vendor/log-viewer/metronic-theme/dashboard.blade.php
+++ resources/views/vendor/log-viewer/metronic-theme/dashboard.blade.php
@@ -53,8 +53,8 @@
                 @foreach($percents as $level => $item)
                     <div class="col-md-3 col-sm-12">
                         <div class="d-flex flex-row align-items-center rounded rounded-2 level-{{ $level }}">
-                            <div class="symbol symbol-50px me-4">
-                                <div class="symbol-label fs-2 fw-semibold">{!! log_styler()->icon($level) !!}</div>
+                            <div class="symbol symbol-50px p-0 me-4">
+                                <div class="symbol-label fs-2 fw-semibold level-{{ $level }}-light">{!! log_styler()->icon($level) !!}</div>
                             </div>
                             <div class="d-flex flex-column p-5">
                                 <div class="fw-bolder">{{ $item['name'] }}</div>
