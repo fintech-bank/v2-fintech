2e560cad10270705440c7587bc4844d2f07d1642
mmockelyn <trainznation@gmail.com>
2022-11-14 23:56:13 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/customer/convention_carte_physique.blade.php
+++ resources/views/pdf/customer/convention_carte_physique.blade.php
@@ -1,5 +1,7 @@
 @extends('pdf.layouts.app')
 
 @section("content")
-
+    <div class="d-flex flex-column justify-content-center">
+        <div class="fw-bolder fs-1">CONTRAT CARTE BANCAIRE {{ Str::upper($card->support->name) }} À AUTORISATION QUASI-SYSTÉMATIQUE</div>
+    </div>
 @endsection
