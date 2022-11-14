ce2a10ad5214af3b2f7e608e80c7aa75efffbca7
mmockelyn <trainznation@gmail.com>
2022-11-15 00:03:28 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/customer/convention_carte_physique.blade.php
+++ resources/views/pdf/customer/convention_carte_physique.blade.php
@@ -2,5 +2,5 @@
 
 @section("content")
     <div class="text-center fs-4 fw-bold fs-underline">CONTRAT CARTE BANCAIRE {{ Str::upper($data->card->support->name) }} À AUTORISATION QUASISYSTÉMATIQUE</div>
-    <div class="text-center fs-2 fs-underline">Fourniture d’une carte de débit (carte de paiement internationale à débit {{ $data->card->debit_text }})</div>
+    <div class="text-center fs-2 fs-underline">Fourniture d’une carte de débit (carte de paiement internationale à débit {{ $data->card->debit_format }})</div>
 @endsection
