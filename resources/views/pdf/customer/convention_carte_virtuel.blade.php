66a52f5becf004d2dcd801f511b6e4c649d1fc42
mmockelyn <trainznation@gmail.com>
2022-11-15 00:51:24 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/customer/convention_carte_virtuel.blade.php
+++ resources/views/pdf/customer/convention_carte_virtuel.blade.php
@@ -12,4 +12,26 @@
     <p>Je suis titulaire d'un contrat Carte CB-{{ Str::upper($data->card->support->name) }} à autorisation quasi-systématique n°
         {{ $document ? $document->reference : generateReference() }} domiciliée sur le
         compte {{ $data->card->wallet->name_account }}.</p>
+
+    <p>Le titulaire de la carte et/ou du compte (ci-après « le titulaire ») doit déclarer par téléphone dans les meilleurs délais, la perte, le
+        vol de la carte, ou l'utilisation frauduleuse des données de la carte au centre d'opposition de la Banque : 01.77.86.24.24 (prix
+        d'un appel local).</p>
+
+    <p>
+        Il reconnait avoir reçu et pris connaissance des conditions particulières et générales d'utilisation du Service e-Carte Bleue et
+        des conditions générales du fonctionnement des cartes {{ config('app.name') }}, (réf. CONTRAT PORTEUR VERSION 18-C)
+        ainsi que la notice d'information du contrat de "Livraison non conforme et non livraison d'un bien" et en accepte les termes sans
+        réserve.
+    </p>
+
+    <div class="fs-4 fw-bolder">Adhésion et déclarations du ou des titulaires</div>
+    <p>
+        Le(s) titulaire(s) déclare(nt) avoir pris connaissance, lu et compris la notice d’information sur le traitement des données à
+        caractère personnel.
+    </p>
+    <p>
+        Le titulaire autorise la {{ config('app.name') }} à prélever automatiquement le montant de la cotisation annuelle
+        relative au Service e-Carte Bleue, selon les conditions tarifaires en vigueur, sur le compte désigné ci-dessus.
+    </p>
+    <div class="m-5
 @endsection
