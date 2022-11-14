de6d1f32eed8e713ed0316ae147b944d6b01a862
mmockelyn <trainznation@gmail.com>
2022-11-14 04:28:06 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/compte/show.blade.php
+++ resources/views/agent/customer/wallet/compte/show.blade.php
@@ -812,68 +812,35 @@
                                     <path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="black" />
                                 </svg>
                             </span>
-                            <input type="text" data-kt-card-table-filter="search" class="form-control form-control-solid w-250px ps-15" placeholder="Rechercher une carte bancaire" />
+                            <input type="text" data-kt-card-filter="search" class="form-control form-control-solid w-250px ps-15" placeholder="Rechercher une carte bancaire" />
                         </div>
                     </div>
                     <div class="card-toolbar">
-                        <div class="d-flex justify-content-end" data-kt-customer-table-toolbar="base">
-                            <button type="button" class="btn btn-light-primary me-3" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
-                                <span class="svg-icon svg-icon-2">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                        <path d="M19.0759 3H4.72777C3.95892 3 3.47768 3.83148 3.86067 4.49814L8.56967 12.6949C9.17923 13.7559 9.5 14.9582 9.5 16.1819V19.5072C9.5 20.2189 10.2223 20.7028 10.8805 20.432L13.8805 19.1977C14.2553 19.0435 14.5 18.6783 14.5 18.273V13.8372C14.5 12.8089 14.8171 11.8056 15.408 10.964L19.8943 4.57465C20.3596 3.912 19.8856 3 19.0759 3Z" fill="black" />
-                                    </svg>
-                                </span>
-                                Filtrer
-                            </button>
-                            <div class="menu menu-sub menu-sub-dropdown w-300px w-md-325px" data-kt-menu="true" id="kt-toolbar-filter">
-                                <div class="px-7 py-5">
-                                    <div class="fs-4 text-dark fw-bolder">Options de filtre</div>
+                        <div class="d-flex justify-content-end" data-kt-card-toolbar="base">
+                            <div class="d-flex flex-stack">
+                                <div class="w-100 mw-150px me-3">
+                                    <!--begin::Select2-->
+                                    <select class="form-select form-select-solid" data-control="select2" data-hide-search="true" data-placeholder="Créancier" data-kt-sepa-filter="creditor">
+                                        <option></option>
+                                        <option value="all">Tous</option>
+                                        @foreach($wallet->creditors as $creditor)
+                                            <option value="{{ $creditor->name }}">{{ $creditor->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    <!--end::Select2-->
                                 </div>
-                                <div class="separator border-gray-200"></div>
-                                <div class="px-7 py-5">
-                                    <div class="mb-10">
-                                        <label class="form-label fs-5 fw-bold mb-3">Status de la carte:</label>
-                                        <div class="d-flex flex-column flex-wrap fw-bold" data-kt-card-table-filter="status">
-                                            <label class="form-check form-check-sm form-check-custom form-check-solid mb-3 me-5">
-                                                <input class="form-check-input" type="radio" name="status" value="all" checked="checked" />
-                                                <span class="form-check-label text-gray-600">Tous</span>
-                                            </label>
-                                            <label class="form-check form-check-sm form-check-custom form-check-solid mb-3 me-5">
-                                                <input class="form-check-input" type="radio" name="status" value="active" />
-                                                <span class="form-check-label text-gray-600">Carte Active</span>
-                                            </label>
-                                            <label class="form-check form-check-sm form-check-custom form-check-solid mb-3 me-5">
-                                                <input class="form-check-input" type="radio" name="status" value="inactive" />
-                                                <span class="form-check-label text-gray-600">Carte Inactive</span>
-                                            </label>
-                                            <label class="form-check form-check-sm form-check-custom form-check-solid mb-3 me-5">
-                                                <input class="form-check-input" type="radio" name="status" value="canceled" />
-                                                <span class="form-check-label text-gray-600">Carte Annuler</span>
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <div class="mb-10">
-                                        <label class="form-label fs-5 fw-bold mb-3">Type de carte:</label>
-                                        <div class="d-flex flex-column flex-wrap fw-bold" data-kt-card-table-filter="type">
-                                            <label class="form-check form-check-sm form-check-custom form-check-solid mb-3 me-5">
-                                                <input class="form-check-input" type="radio" name="type" value="all" checked="checked" />
-                                                <span class="form-check-label text-gray-600">Tous</span>
-                                            </label>
-                                            <label class="form-check form-check-sm form-check-custom form-check-solid mb-3 me-5">
-                                                <input class="form-check-input" type="radio" name="type" value="physique" />
-                                                <span class="form-check-label text-gray-600">Physique</span>
-                                            </label>
-
-                                            <label class="form-check form-check-sm form-check-custom form-check-solid mb-3 me-5">
-                                                <input class="form-check-input" type="radio" name="type" value="virtuel" />
-                                                <span class="form-check-label text-gray-600">Virtuel</span>
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <div class="d-flex justify-content-end">
-                                        <button type="reset" class="btn btn-light btn-active-light-primary me-2" data-kt-menu-dismiss="true" data-kt-card-table-filter="reset">Réinitialiser</button>
-                                        <button type="submit" class="btn btn-primary" data-kt-menu-dismiss="true" data-kt-card-table-filter="filter">Appliquer</button>
-                                    </div>
+                                <div class="w-100 mw-150px me-3">
+                                    <!--begin::Select2-->
+                                    <select class="form-select form-select-solid" data-control="select2" data-hide-search="true" data-placeholder="Status du prélèvement" data-kt-sepa-filter="status">
+                                        <option></option>
+                                        <option value="all">Tous</option>
+                                        <option value="waiting">En attente</option>
+                                        <option value="processed">Traité</option>
+                                        <option value="rejected">Rejeté</option>
+                                        <option value="return">Retourné</option>
+                                        <option value="refunded">Remboursé</option>
+                                    </select>
+                                    <!--end::Select2-->
                                 </div>
                             </div>
                             <a class="btn btn-bank" data-bs-toggle="modal" href="#add_credit_card"><i class="fa-solid fa-plus-square me-2"></i> Nouvelle Carte Bancaire</a>
