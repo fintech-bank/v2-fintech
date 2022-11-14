3db57a2f46ebb35735141c248c67e05a730dc9e4
mmockelyn <trainznation@gmail.com>
2022-11-14 17:50:44 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/vendor/log-viewer/metronic-theme/logs.blade.php
+++ resources/views/vendor/log-viewer/metronic-theme/logs.blade.php
@@ -93,6 +93,30 @@
             </table>
         </div>
     </div>
+    <div id="delete-log-modal" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <form id="delete-log-form" action="{{ route('log-viewer::logs.delete') }}" method="POST">
+                <input type="hidden" name="_method" value="DELETE">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                <input type="hidden" name="date" value="">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">@lang('Delete log file')</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p></p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-sm btn-secondary mr-auto" data-dismiss="modal">@lang('Cancel')</button>
+                        <button type="submit" class="btn btn-sm btn-danger" data-loading-text="@lang('Loading')&hellip;">@lang('Delete')</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
 
 @section("script")
