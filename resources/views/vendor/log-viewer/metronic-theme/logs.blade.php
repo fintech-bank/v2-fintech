9a32be77c497bd90bbc3135301351e229e9541fd
mmockelyn <trainznation@gmail.com>
2022-11-14 17:51:24 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/vendor/log-viewer/metronic-theme/logs.blade.php
+++ resources/views/vendor/log-viewer/metronic-theme/logs.blade.php
@@ -121,6 +121,56 @@
 
 @section("script")
     <script type="text/javascript">
+        $(function () {
+            var deleteLogModal = $('div#delete-log-modal'),
+                deleteLogForm  = $('form#delete-log-form'),
+                submitBtn      = deleteLogForm.find('button[type=submit]');
 
+            $("a[href='#delete-log-modal']").on('click', function(event) {
+                event.preventDefault();
+                var date    = $(this).data('log-date'),
+                    message = "{{ __('Are you sure you want to delete this log file: :date ?') }}";
+
+                deleteLogForm.find('input[name=date]').val(date);
+                deleteLogModal.find('.modal-body p').html(message.replace(':date', date));
+
+                deleteLogModal.modal('show');
+            });
+
+            deleteLogForm.on('submit', function(event) {
+                event.preventDefault();
+                submitBtn.button('loading');
+
+                $.ajax({
+                    url:      $(this).attr('action'),
+                    type:     $(this).attr('method'),
+                    dataType: 'json',
+                    data:     $(this).serialize(),
+                    success: function(data) {
+                        submitBtn.button('reset');
+                        if (data.result === 'success') {
+                            deleteLogModal.modal('hide');
+                            location.reload();
+                        }
+                        else {
+                            alert('AJAX ERROR ! Check the console !');
+                            console.error(data);
+                        }
+                    },
+                    error: function(xhr, textStatus, errorThrown) {
+                        alert('AJAX ERROR ! Check the console !');
+                        console.error(errorThrown);
+                        submitBtn.button('reset');
+                    }
+                });
+
+                return false;
+            });
+
+            deleteLogModal.on('hidden.bs.modal', function() {
+                deleteLogForm.find('input[name=date]').val('');
+                deleteLogModal.find('.modal-body p').html('');
+            });
+        });
     </script>
 @endsection
