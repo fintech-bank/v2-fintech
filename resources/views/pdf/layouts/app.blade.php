f54ae01ff0fed2a063422cf6c5eede7452fdf37e
mmockelyn <trainznation@gmail.com>
2022-11-14 23:57:17 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/layouts/app.blade.php
+++ resources/views/pdf/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <title>{{ $title }}</title>
     <link href='//fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,300,700&amp;subset=all' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="{{ asset('css/pdf.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     @include('pdf.layouts.header', ["type" => $header_type])
