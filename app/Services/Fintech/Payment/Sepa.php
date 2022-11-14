4a5cca70c780d3b6ed29ffdb2205101b0f6686cc
mmockelyn <trainznation@gmail.com>
2022-11-14 01:32:17 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Services/Fintech/Payment/Sepa.php
+++ app/Services/Fintech/Payment/Sepa.php
@@ -11,6 +11,6 @@ class Sepa
 
     public function rembSepaRequest(\App\Models\Customer\CustomerSepa $sepa)
     {
-        return \Http::post('https://payment.fintech.ovh/api/sepa/remb', ["ics" => $sepa->creditors()->first()->identifiant])->clientError();
+        return \Http::post('https://payment.fintech.ovh/api/sepa/remb', ["ics" => $sepa->creditors()->first()->identifiant])->body();
     }
 }
