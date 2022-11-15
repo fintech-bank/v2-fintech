7f936b0a9393df05979f1d52a56f07ec69e7b89a
mmockelyn <trainznation@gmail.com>
2022-11-15 01:31:14 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Agent/Customer/CreditCardController.php
+++ app/Http/Controllers/Agent/Customer/CreditCardController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Agent\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer\CustomerCreditCard;
 
 class CreditCardController extends Controller
 {
-    public function index()
+    public function index($customer_id, $number_account, $card_id)
     {
+        $card = CustomerCreditCard::find($card_id);
 
+        dd($card);
     }
 }
