a1c69ac435684493e058903b8949189b5eb96180
mmockelyn <trainznation@gmail.com>
2022-11-15 01:58:21 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/card/show.blade.php
+++ resources/views/agent/customer/wallet/card/show.blade.php
@@ -53,6 +53,8 @@
             <div class="fw-bolder fs-2">CB {{ $card->support->name }}</div>
             <span class="mb-3">{{ $card->debit_format }}</span>
             <a href="">{{ $card->wallet->name_account_generic }}</a>
+            <button class="btn btn-lg btn-circle btn-outline btn-outline-danger">Vérouiller la carte</button>
+            <button class="btn btn-lg btn-circle btn-danger">Opposition</button>
         </div>
     </div>
 @endsection
