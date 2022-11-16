c4e8b818593db1a5511f27827587161dff975110
mmockelyn <trainznation@gmail.com>
2022-11-17 00:30:45 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- database/migrations/2022_04_11_025346_create_packages_table.php
+++ database/migrations/2022_04_11_025346_create_packages_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->float('price');
             $table->enum('type_prlv', ['mensual', 'trim', 'sem', 'annual']);
-            $table->enum('type_cpt', ['part', 'pro', 'orga'])->default('part');
+            $table->enum('type_cpt', ['part', 'pro', 'orga', 'assoc'])->default('part');
             $table->boolean('visa_classic')->default(true);
             $table->boolean('check_deposit')->default(true);
             $table->boolean('payment_withdraw')->default(true);
