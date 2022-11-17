96b7f17d20ab4424298d4d12b6ffe99430d2cfa1
mmockelyn <trainznation@gmail.com>
2022-11-17 14:40:47 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Scope/BeneficiaireTrait.php
+++ app/Scope/BeneficiaireTrait.php
@@ -6,19 +6,5 @@ use Illuminate\Support\Str;
 
 trait BeneficiaireTrait
 {
-    public function createBenef($type, $bankname, $iban, $bic, $company = null, $civility = null, $firstname = null, $lastname = null, $titulaire = 0)
-    {
-        return $this->create([
-            'uuid' => Str::uuid(),
-            'type' => $type,
-            'company' => $company,
-            'civility' => $civility,
-            'firstname' => $firstname,
-            'lastname' => $lastname,
-            'bankname' => $bankname,
-            'iban' => $iban,
-            'bic' => $bic,
-            'titulaire' => $titulaire,
-        ]);
-    }
+
 }
