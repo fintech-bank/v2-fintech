5c3f5fdde7786c530e1ecd63244e79146051e70d
mmockelyn <trainznation@gmail.com>
2022-11-17 21:01:25 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Scope/CustomerEpargneTrait.php
+++ app/Scope/CustomerEpargneTrait.php
@@ -6,6 +6,6 @@ trait CustomerEpargneTrait
 {
     public function calcProfit($profit_actuel, $capital, $percent_plan)
     {
-        return $profit_actuel + ($capital * $percent_plan);
+        return $profit_actuel + ($capital * $percent_plan / 100);
     }
 }
