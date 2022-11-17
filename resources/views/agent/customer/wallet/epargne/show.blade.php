76687829784994aeeb46f6f958ec45f521783c34
mmockelyn <trainznation@gmail.com>
2022-11-17 19:55:48 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/epargne/show.blade.php
+++ resources/views/agent/customer/wallet/epargne/show.blade.php
@@ -284,6 +284,11 @@
                                 <div class="fw-bolder">Montant prélevé par mois</div>
                                 <div class="fs-4 ps-3">{{ $wallet->epargne->monthly_payment_format }}</div>
                             </div>
+                            <div class="separator border-2 my-5"></div>
+                            <div class="d-flex flex-column">
+                                <div class="fw-bolder">Compte Lié</div>
+                                <div class="fs-4 ps-3">{{ $wallet->epargne->payment->number_account }}</div>
+                            </div>
                         </div>
                     </div>
                 </div>
