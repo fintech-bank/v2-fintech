f9669db9827442abdb7400995f259fc6390fa65e
mmockelyn <trainznation@gmail.com>
2022-11-18 00:29:55 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/show.blade.php
+++ resources/views/agent/scripts/customer/show.blade.php
@@ -23,6 +23,7 @@
         btnVerifyIdentity: document.querySelector('.btnVerifyIdentity'),
         btnVerifyAddress: document.querySelector('.btnVerifyAddress'),
         btnVerifyIncome: document.querySelector('.btnVerifyIncome'),
+        chartEndet: document.querySelector('#chart_endettement'),
     }
     let modals = {
         modalUpdateStatusAccount: document.querySelector('#updateStatus'),
@@ -373,6 +374,66 @@
             </tr>
             `
         })
+    }
+
+    let chartEnd = () => {
+        let chart = new ApexCharts(elements.chartEndet, {
+            series: [76],
+            chart: {
+                type: 'radialBar',
+                offsetY: -20,
+                sparkline: {
+                    enabled: true
+                }
+            },
+            plotOptions: {
+                radialBar: {
+                    startAngle: -90,
+                    endAngle: 90,
+                    track: {
+                        background: "#e7e7e7",
+                        strokeWidth: '97%',
+                        margin: 5, // margin is in pixels
+                        dropShadow: {
+                            enabled: true,
+                            top: 2,
+                            left: 0,
+                            color: '#999',
+                            opacity: 1,
+                            blur: 2
+                        }
+                    },
+                    dataLabels: {
+                        name: {
+                            show: false
+                        },
+                        value: {
+                            offsetY: -2,
+                            fontSize: '22px'
+                        }
+                    }
+                }
+            },
+            grid: {
+                padding: {
+                    top: -10
+                }
+            },
+            fill: {
+                type: 'gradient',
+                gradient: {
+                    shade: 'light',
+                    shadeIntensity: 0.4,
+                    inverseColors: false,
+                    opacityFrom: 1,
+                    opacityTo: 1,
+                    stops: [0, 50, 53, 91]
+                },
+            },
+            labels: ['Average Results'],
+        })
+        chart.render()
+
     }
 
     @if($customer->info->type != 'part')
