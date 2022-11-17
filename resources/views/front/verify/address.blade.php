27496bb0ba808aa57e23370d33d686c96d9ad58a
mmockelyn <trainznation@gmail.com>
2022-11-17 13:42:42 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/front/verify/address.blade.php
+++ resources/views/front/verify/address.blade.php
@@ -8,7 +8,8 @@
     <script type="text/javascript">
         const client = new Persona.Client({
             templateId: 'itmpl_qvuQuXt48aMJYd6o345x7Ldm',
-            environmentId: 'env_4dTQPEjvQqhdSBciNzE7YzBi',
+            environment: 'sandbox',
+            referenceId: "{{ isset($customer) ? $customer->persona_reference_id : '' }}",
             onReady: () => client.open(),
             onComplete: ({ inquiryId, status, fields }) => {
                 console.log(inquiryId)
@@ -16,7 +17,18 @@
                 console.log(fields)
             },
             onCancel: ({ inquiryId, sessionToken }) => {},
-            onError: (error) => {}
+            onError: (error) => {},
+            fields: {
+                nameFirst: "{{ $customer->info->firstname }}",
+                nameLast: "{{ $customer->info->lastname }}",
+                birthdate: "{{ $customer->info->datebirth->format('Y-m-d') }}",
+                addressStreet1: "{{ $customer->info->address }}",
+                addressCity: "{{ $customer->info->city }}",
+                addressPostalCode: "{{ $customer->info->postal }}",
+                addressCountryCode: "{{ \Illuminate\Support\Str::upper(\Illuminate\Support\Str::limit($customer->info->address, 2, '')) }}",
+                phoneNumber: "{{ $customer->info->mobile }}",
+                emailAddress: "{{ $customer->user->email }}",
+            }
         });
     </script>
 @endsection
