11e39033fe01096a4d57880cc06b256f95a1996d
mmockelyn <trainznation@gmail.com>
2022-11-18 20:15:51 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/general/fiche_de_dialogue.blade.php
+++ resources/views/pdf/general/fiche_de_dialogue.blade.php
@@ -2,5 +2,10 @@
 
 @section("content")
     <div class="fs-3 fw-bolder">{{ $customer->info->full_name }},</div>
-    <p>Vous trouverez ci-après les principales caractéristiques de votre demande d'ouverture de compte de {{ $customer->info->type_text }}</p>
+    <p class="mb-5">Vous trouverez ci-après les principales caractéristiques de votre demande d'ouverture de compte de {{ $customer->info->type_text }}</p>
+
+    <div class="border border-2 p-5">
+        <div class="fs-5">Vos Informations personnelles</div>
+
+    </div>
 @endsection
