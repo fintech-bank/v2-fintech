d230a4fe639393d0ea5f8ebb34807f6a8a7abd5a
mmockelyn <trainznation@gmail.com>
2022-11-18 17:50:42 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/wallet/facelia.blade.php
+++ resources/views/agent/scripts/customer/wallet/facelia.blade.php
@@ -14,7 +14,85 @@
     if (elements.btnProgressPret) {
         elements.btnProgressPret.addEventListener('click', e => {
             Swal.fire({
-                title: "Voulez-vous passer ce crédit en étude ?",
+                title: "Voulez-vous valider définitivement le crédit N°{{ $facelia->pret->reference }} ?",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: "OUI",
+                cancelButtonText: "NON",
+            }).then((result) => {
+                if(result.isConfirmed) {
+                    $.ajax({
+                        url: '/api/customer/{{ $facelia->card->wallet->customer->id }}/wallet/{{ $facelia->card->wallet->number_account }}/pret/{{ $facelia->pret->reference }}',
+                        method: 'PUT',
+                        data: {'action': 'state', 'state': 'progress'},
+                        success: () => {
+                            toastr.success(`Le status du pret à été mise à jours`, `Crédit FACELIA`)
+
+                            setTimeout(() => {
+                                window.location.reload()
+                            }, 1200)
+                        }
+                    })
+                }
+            })
+        })
+    }
+    if (elements.btnAcceptPret) {
+        elements.btnAcceptPret.addEventListener('click', e => {
+            Swal.fire({
+                title: "Voulez-vous accepté le crédit N° {{ $facelia->pret->reference }} ?",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: "OUI",
+                cancelButtonText: "NON",
+            }).then((result) => {
+                if(result.isConfirmed) {
+                    $.ajax({
+                        url: '/api/customer/{{ $facelia->card->wallet->customer->id }}/wallet/{{ $facelia->card->wallet->number_account }}/pret/{{ $facelia->pret->reference }}',
+                        method: 'PUT',
+                        data: {'action': 'state', 'state': 'accepted'},
+                        success: () => {
+                            toastr.success(`Le status du pret à été mise à jours`, `Crédit FACELIA`)
+
+                            setTimeout(() => {
+                                window.location.reload()
+                            }, 1200)
+                        }
+                    })
+                }
+            })
+        })
+    }
+    if (elements.btnRejectPret) {
+        elements.btnRejectPret.addEventListener('click', e => {
+            Swal.fire({
+                title: "Voulez-vous refusé le crédit N° {{ $facelia->pret->reference }} ?",
+                icon: 'question',
+                showCancelButton: true,
+                confirmButtonText: "OUI",
+                cancelButtonText: "NON",
+            }).then((result) => {
+                if(result.isConfirmed) {
+                    $.ajax({
+                        url: '/api/customer/{{ $facelia->card->wallet->customer->id }}/wallet/{{ $facelia->card->wallet->number_account }}/pret/{{ $facelia->pret->reference }}',
+                        method: 'PUT',
+                        data: {'action': 'state', 'state': 'refused'},
+                        success: () => {
+                            toastr.success(`Le status du pret à été mise à jours`, `Crédit FACELIA`)
+
+                            setTimeout(() => {
+                                window.location.reload()
+                            }, 1200)
+                        }
+                    })
+                }
+            })
+        })
+    }
+    if (elements.btnStudyPret) {
+        elements.btnStudyPret.addEventListener('click', e => {
+            Swal.fire({
+                title: "Voulez-vous étudié le crédit N° {{ $facelia->pret->reference }} ?",
                 icon: 'question',
                 showCancelButton: true,
                 confirmButtonText: "OUI",
@@ -26,7 +104,11 @@
                         method: 'PUT',
                         data: {'action': 'state', 'state': 'study'},
                         success: () => {
-                            console.log("OK")
+                            toastr.success(`Le status du pret à été mise à jours`, `Crédit FACELIA`)
+
+                            setTimeout(() => {
+                                window.location.reload()
+                            }, 1200)
                         }
                     })
                 }
