303901fa9e90ec30a18f176b8941558c7dc9a701
mmockelyn <trainznation@gmail.com>
2022-11-18 21:40:47 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/general/mandat_prelevement_sepa.blade.php
+++ resources/views/pdf/general/mandat_prelevement_sepa.blade.php
@@ -27,9 +27,12 @@
             <tr class="border-bottom border-gray-600">
                 <td class="fs-2">
                     <div class="fw-bolder mb-2">Vos Informations</div>
-
                 </td>
             </tr>
+        <tr>
+            <td class="fs-2">Nom - Prénom</td>
+            <td class="fs-2">{{ $customer->info->firstname }} {{ $customer->info->lastname }}</td>
+        </tr>
         </tbody>
     </table>
 @endsection
