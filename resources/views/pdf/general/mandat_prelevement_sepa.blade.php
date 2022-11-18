7824c89e4f75c7607b6e4cbdde9ac13869e0997d
mmockelyn <trainznation@gmail.com>
2022-11-18 21:32:45 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/general/mandat_prelevement_sepa.blade.php
+++ resources/views/pdf/general/mandat_prelevement_sepa.blade.php
@@ -1,7 +1,7 @@
 @extends('pdf.layouts.app')
 
 @section("content")
-    <div class="border border-2 rounded p-5">
+    <div class="border border-2 rounded p-1">
         <div class="text-center">
             <div class="fw-bolder fs-4">Mandat de prélèvement SEPA</div>
             <div class="fs-2">Référence Unique de Mandat : XP{{ generateReference(15) }}</div>
