80a350c9a9fc26f291e0dc83e3e23c74aa37b119
mmockelyn <trainznation@gmail.com>
2022-11-18 22:40:46 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/insurance/condition_general_mon_assurance_au_quotidien.blade.php
+++ resources/views/pdf/insurance/condition_general_mon_assurance_au_quotidien.blade.php
@@ -1,5 +1,9 @@
 @extends('pdf.layouts.app')
 
 @section("content")
-
+    <div class="text-center">
+        <div class="fs-5 fw-bolder">MON ASSURANCE AU QUOTIDIEN</div>
+        <div class="fs-4">NOTICE D'INFORMATION</div>
+        <div class="text-muted">REF.{{ random_numeric(3) }} {{ random_numeric(3) }} {{ random_string_alpha_upper(1) }}</div>
+    </div>
 @endsection
