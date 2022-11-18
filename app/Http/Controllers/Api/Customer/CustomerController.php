23f4cb377a2780dbe7557d80b0f5c25fc726a3f7
mmockelyn <trainznation@gmail.com>
2022-11-18 19:04:44 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Api/Customer/CustomerController.php
+++ app/Http/Controllers/Api/Customer/CustomerController.php
@@ -209,7 +209,7 @@ class CustomerController extends Controller
     {
         $customer = Customer::find($customer_id);
         $income = $customer->income;
-        $charge = $customer->charge->rent + $customer->charge->credit + $customer->charge->divers
+        $charge = $customer->charge->rent + $customer->charge->credit + $customer->charge->divers;
 
         $reste_vivre = $customer->income->pro_incoming - ($customer->charge->rent + $customer->charge->credit + $customer->charge->divers);
         $end = $income / $charge * 100;
