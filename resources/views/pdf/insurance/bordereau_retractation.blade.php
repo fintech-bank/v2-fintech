91064da214d7a7133b812fecf1709ba6c5ab82ec
mmockelyn <trainznation@gmail.com>
2022-11-18 23:10:25 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/insurance/bordereau_retractation.blade.php
+++ resources/views/pdf/insurance/bordereau_retractation.blade.php
@@ -1,5 +1,5 @@
 @extends('pdf.layouts.app')
 
 @section("content")
-    <div class="fs-3 fw-bolder uppercase text-center">FORMULAIRE DE RETRACTATION</div>
+    <div class="fs-2 fw-bolder uppercase text-center">FORMULAIRE DE RETRACTATION</div>
 @endsection
