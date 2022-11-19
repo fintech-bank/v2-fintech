bad19c95708e5d2ce15ab597715625d54ffe8844
mmockelyn <trainznation@gmail.com>
2022-11-19 02:16:02 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/loan/autorisation_decouvert_permanent.blade.php
+++ resources/views/pdf/loan/autorisation_decouvert_permanent.blade.php
@@ -10,7 +10,7 @@
         Offre de contrat de crédit émise par {{ config('app.name') }}, à Nantes, le {{ isset($document) ? $document->created_at->format('d/m/Y') : now()->format('d/m/Y') }}
         Durée de validité de l’offre : 15 jours à compter du {{ isset($document) ? $document->created_at->format('d/m/Y') : now()->format('d/m/Y') }}.
     </p>
-    <div class="uppercase underline bg-secondary fs-4 p-2 text-white">Préteur</div>
+    <div class="uppercase underline bg-secondary fw-bolder fs-4 p-2 text-white my-2">Préteur</div>
     <p>
         {{ config('app.name') }}, SAS à capital variable régie par les
         articles L512-2 et suivants du code monétaire et financier et l’ensemble des textes relatifs aux établissements de crédit, immatriculée au registre du commerce et des sociétés de Nantes sous le n°521 809 061, dont
@@ -18,4 +18,11 @@
         assurance immatriculé à l’ORIAS sous le n° 07 004 504.
     </p>
     <p class="fw-bolder fs-italic">Ci-après dénommée le « Prêteur » ou la « Banque »</p>
+    <div class="uppercase underline bg-secondary fs-4 p-2 text-white my-2">Emprunteur(s)</div>
+    <p>
+        <span class="fw-bolder">Titulaire du compte:</span><br>
+        Nom, Prénom: {{ $customer->info->full_name }}<br>
+        Date de naissance: {{ $customer->info->datebirth->format("d/m/Y") }} -- Lieu: {{ $customer->info->citybirth }}<br>
+        Adresse: {{ $customer->info->line_address }}
+    </p>
 @endsection
