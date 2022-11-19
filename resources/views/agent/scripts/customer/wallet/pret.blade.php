95f424a006d8a80bf29dfc79cf72bfe602e55d87
mmockelyn <trainznation@gmail.com>
2022-11-20 00:58:36 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/wallet/pret.blade.php
+++ resources/views/agent/scripts/customer/wallet/pret.blade.php
@@ -59,7 +59,7 @@
                 if(!data) {
                     elements.cniField.classList.add('is-valid')
                     elements.cniField.classList.add('is-invalid')
-                    let p = elements.cniField.insertAfter('p')
+                    let p = elements.cniField.after('p')
                     p.classList.add('text-danger')
                     p.innerHTML = 'Numéro de carte erroné !'
                 } else {
