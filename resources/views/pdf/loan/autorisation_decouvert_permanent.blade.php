8afc4a118a037249d304bbe661f0481acc3e6c35
mmockelyn <trainznation@gmail.com>
2022-11-19 02:11:46 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/loan/autorisation_decouvert_permanent.blade.php
+++ resources/views/pdf/loan/autorisation_decouvert_permanent.blade.php
@@ -5,4 +5,17 @@
         <div class="">OFFRE DE CONTRAT DE CREDIT</div>
         <div class="">AUTORISATION DE DÉCOUVERT</div>
     </div>
+    <p>
+        La présente offre de crédit est faite en application de l’article L.312-1 et suivants du code de la consommation.
+        Offre de contrat de crédit émise par {{ config('app.name') }}, à Nantes, le {{ isset($document) ? $document->created_at->format('d/m/Y') : now()->format('d/m/Y') }}
+        Durée de validité de l’offre : 15 jours à compter du {{ isset($document) ? $document->created_at->format('d/m/Y') : now()->format('d/m/Y') }}.
+    </p>
+    <div class="uppercase underline bg-secondary fs-4">Préteur</div>
+    <p>
+        {{ config('app.name') }}, SAS à capital variable régie par les
+        articles L512-2 et suivants du code monétaire et financier et l’ensemble des textes relatifs aux établissements de crédit, immatriculée au registre du commerce et des sociétés de Nantes sous le n°521 809 061, dont
+        le siège social est situé 4 Rue du Coudray - 44000 Nantes Cedex, intermédiaire en
+        assurance immatriculé à l’ORIAS sous le n° 07 004 504.
+    </p>
+    <p class="fw-bolder fs-italic">Ci-après dénommée le « Prêteur » ou la « Banque »</p>
 @endsection
