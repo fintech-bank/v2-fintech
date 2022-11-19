04e633bf5fadea0021814fd7f6fb5ba0ae7ace7a
mmockelyn <trainznation@gmail.com>
2022-11-19 22:48:43 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Api/Insurance/ClaimController.php
+++ app/Http/Controllers/Api/Insurance/ClaimController.php
@@ -24,31 +24,29 @@ class ClaimController extends Controller
             'customer_insurance_id' => $insurance->id,
         ]);
 
-        if(auth()->user()->agent) {
-            ob_start();
-            ?>
-            <p>La déclaration de sinitre <strong>N°<?= $claim->reference ?></strong> à été créé ce jour.</p>
-            <p>Veuillez revoir les informations ci-dessous et valider cette déclaration.</p>
-            <ul>
-                <li><strong>Référence de l'incident:</strong> <?= $claim->reference; ?></li>
-                <li><strong>Date / Heure de l'incident:</strong> <?= $claim->incidentDate->format('d/m/Y'); ?> <?= $claim->incidentTime->format('H:i'); ?></li>
-                <li><strong>Etes-vous responsable ?</strong> <?= $claim->responsability ? 'OUI' : 'NON' ?></li>
-                <li><strong>Description de l'incident:</strong> <?= $claim->incidentDesc ?></li>
-            </ul>
-            <p>Vous devez également nous soumettre un ou plusieurs justificatifs en relation avec ce sinistre.</p>
-            <x-form.input-file
-                name="justificatif"
-                label="Justificatif du sinistre" />
-            <?php
-            $comment = ob_get_clean();
-            RequestHelper::create(
-                $insurance->customer,
-                'Accepter la déclaration de sinistre',
-                $comment,
-                CustomerInsuranceClaim::class,
-                $claim->id,
-            );
-        }
+        ob_start();
+        ?>
+        <p>La déclaration de sinitre <strong>N°<?= $claim->reference ?></strong> à été créé ce jour.</p>
+        <p>Veuillez revoir les informations ci-dessous et valider cette déclaration.</p>
+        <ul>
+            <li><strong>Référence de l'incident:</strong> <?= $claim->reference; ?></li>
+            <li><strong>Date / Heure de l'incident:</strong> <?= $claim->incidentDate->format('d/m/Y'); ?> <?= $claim->incidentTime->format('H:i'); ?></li>
+            <li><strong>Etes-vous responsable ?</strong> <?= $claim->responsability ? 'OUI' : 'NON' ?></li>
+            <li><strong>Description de l'incident:</strong> <?= $claim->incidentDesc ?></li>
+        </ul>
+        <p>Vous devez également nous soumettre un ou plusieurs justificatifs en relation avec ce sinistre.</p>
+        <x-form.input-file
+            name="justificatif"
+            label="Justificatif du sinistre" />
+        <?php
+        $comment = ob_get_clean();
+        RequestHelper::create(
+            $insurance->customer,
+            'Accepter la déclaration de sinistre',
+            $comment,
+            CustomerInsuranceClaim::class,
+            $claim->id,
+        );
 
         return response()->json();
     }
