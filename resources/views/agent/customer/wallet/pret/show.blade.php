426ee7a064351656e5a38bb459cea8565d1f90ce
mmockelyn <trainznation@gmail.com>
2022-11-19 21:40:02 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/pret/show.blade.php
+++ resources/views/agent/customer/wallet/pret/show.blade.php
@@ -436,7 +436,7 @@
                 <form id="formAddClaim" action="" method="post">
                     <div class="modal-body">
                         <x-base.alert
-                            type="simple"
+                            type="basic"
                             color="primary"
                             icon="info-circle"
                             title="Déclaration de sinistre pour un client"
