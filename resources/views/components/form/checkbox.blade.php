5871891b03c347ddc76855d50530b474b91abaa4
mmockelyn <trainznation@gmail.com>
2022-11-19 15:43:30 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/components/form/checkbox.blade.php
+++ resources/views/components/form/checkbox.blade.php
@@ -1,7 +1,7 @@
 <div class="mb-5 {{ $class ?? '' }}">
     <div class="form-check form-check-custom form-check-solid">
         <input class="form-check-input" type="checkbox" name="{{ $name }}" value="{{ $value }}" id="{{ $name }}" {{ $checked == true ? 'checked' : '' }}/>
-        <label class="form-check-label" for="{{ $name }}">
+        <label class="form-check-label {{ $class ?? '' }}" for="{{ $name }}">
             {{ $label }}
         </label>
     </div>
