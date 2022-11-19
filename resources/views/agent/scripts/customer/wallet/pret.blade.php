e0505e88c8543f3f932e0ae86421fe5bb2853e82
mmockelyn <trainznation@gmail.com>
2022-11-20 00:48:14 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/wallet/pret.blade.php
+++ resources/views/agent/scripts/customer/wallet/pret.blade.php
@@ -56,6 +56,9 @@
             },
             success: data => {
                 console.log(data)
+                if(!data) {
+                    console.log(elements.cniField.parentNode)
+                }
             }
         })
     }
