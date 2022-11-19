d1660aa3f392feaf3e004220df5d9c6e689aaf84
mmockelyn <trainznation@gmail.com>
2022-11-19 14:17:51 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/wallet/pret.blade.php
+++ resources/views/agent/scripts/customer/wallet/pret.blade.php
@@ -1,8 +1,20 @@
 <script type="text/javascript">
-    let tables = {}
+    let tables = {
+        tableTransaction: document.querySelector("#kt_transaction_table"),
+    }
     let elements = {}
     let modals = {}
     let forms = {}
-    let dataTable = {}
+    let dataTable = {
+        datatableTransaction: $(tables.tableTransaction).DataTable({
+            "scrollY": "350px",
+            "scrollCollapse": true,
+            "paging": false,
+            "dom": "<'table-responsive'tr>",
+            info: !1,
+            order: [],
+            pageLength: 10,
+        }),
+    }
     let block = {}
 </script>
