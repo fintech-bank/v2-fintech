df206acb74d4678ddff78c7e5113b1cb92005481
mmockelyn <trainznation@gmail.com>
2022-11-20 20:19:41 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Api/Core/GeoController.php
+++ app/Http/Controllers/Api/Core/GeoController.php
@@ -16,15 +16,12 @@ class GeoController extends Controller
         ?>
         <div class="mb-10">
             <label for="<?= $request->get('name') ?>" class="form-label"><?= $request->has('label') ? $request->get('label') : $request->get('name') ?></label>
-            <select id="<?= $request->get('name') ?>" name="<?= $request->get('name') ?>" class="form-control form-control-solid" data-placeholder="<?= $request->get('placeholder') ?>">
+            <select id="<?= $request->get('name') ?>" name="<?= $request->get('name') ?>" class="form-control form-control-solid selectpicker" data-placeholder="<?= $request->get('placeholder') ?>">
                 <?php foreach ($results as $result): ?>
                     <option value="<?= $result->name; ?>"><?= $result->name; ?></option>
                 <?php endforeach; ?>
             </select>
         </div>
-        <script>
-            $("#<?= $request->get('name') ?>").selectpicker()
-        </script>
         <?php
 
         return response()->json(ob_get_clean());
