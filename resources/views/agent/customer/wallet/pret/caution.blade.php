e09e56637e1f850960e0b2442d423e3305276b53
mmockelyn <trainznation@gmail.com>
2022-11-20 19:30:19 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/pret/caution.blade.php
+++ resources/views/agent/customer/wallet/pret/caution.blade.php
@@ -128,7 +128,7 @@
                         <div class="col-md-4 col-sm-12">
                             <div class="mb-10">
                                 <label for="city_naissance" class="form-label">Ville de naissance</label>
-                                <select id="city_naissance" name="city_naissance" class="form-control form-control-solid selectpicker" data-placeholder="Selectionner un département de naissance">
+                                <select id="city_naissance" name="city_naissance" data-live-search="true" class="form-control form-control-solid selectpicker" data-placeholder="Selectionner une ville de naissance">
                                     <option value=""></option>
                                     @foreach(\App\Helper\GeoHelper::getCitiesFromCountry('France') as $city)
                                         <option value="{{ $city }}">{{ $city }}</option>
