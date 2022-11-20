1e19deec61bd906260bd541b6bce09984a6b5e28
mmockelyn <trainznation@gmail.com>
2022-11-20 22:17:09 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -3,7 +3,7 @@
     <label for="{{ $name }}" class="{{ $required ? 'required' : '' }} form-label">
         {{ $label }}
     </label>
-    <select id="{{ $name }}" class="form-select form-select-solid" data-control="select2" @isset($placeholder) data-placeholder="{{ $placeholder }}" @else data-placeholder="{{ $label }}" @endisset name="{{ $name }}">
+    <select id="{{ $name }}" class="form-select form-select-solid selectpicker" @isset($placeholder) data-placeholder="{{ $placeholder }}" @else data-placeholder="{{ $label }}" @endisset name="{{ $name }}">
         @if(array_key_exists('key', $value))
             <option value="{{ $value['key'] }}">{{ $value['value'] }}</option>
         @else
