009d8d2fa0010a2ba06f098d3d795484eee34024
mmockelyn <trainznation@gmail.com>
2022-11-20 20:35:08 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/wallet/caution.blade.php
+++ resources/views/agent/scripts/customer/wallet/caution.blade.php
@@ -10,9 +10,6 @@
     let dataTable = {}
     let block = {}
 
-    elements.selectDep.querySelector('select').setAttribute('disabled', '')
-    elements.selectCity.querySelector('select').setAttribute('disabled', '')
-
 
     let stateBirthByCountry = (item) => {
         let country = item.value
