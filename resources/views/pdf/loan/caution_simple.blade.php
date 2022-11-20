e21386225e9631efbedf434ef33be028c9cd2a39
mmockelyn <trainznation@gmail.com>
2022-11-20 15:41:55 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/pdf/loan/caution_simple.blade.php
+++ resources/views/pdf/loan/caution_simple.blade.php
@@ -1,5 +1,5 @@
 @extends('pdf.layouts.app')
 
 @section("content")
-    <div class="fw-bolder fs-3 border border-2 text-center p-2">Acte de cautionnement</div>
+    <div class="fw-bolder fs-1 border border-2 text-center p-2">Acte de cautionnement</div>
 @endsection
