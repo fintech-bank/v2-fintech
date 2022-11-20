7eb65eacb6bf0eb727dd93e04f02d347991bae79
mmockelyn <trainznation@gmail.com>
2022-11-20 20:49:51 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/pret/caution.blade.php
+++ resources/views/agent/customer/wallet/pret/caution.blade.php
@@ -117,7 +117,7 @@
                         <div class="col-md-4 col-sm-12 selectCountry">
                             <div class="mb-10">
                                 <label for="country_naissance" class="form-label">Pays de naissance</label>
-                                <select id="country_naissance" name="country_naissance" class="form-control form-control-solid selectpicker" data-live-search="true" data-placeholder="Selectionner un pays de naissance" onchange="stateBirthByCountry(this)">
+                                <select id="country_naissance" name="country_naissance" class="form-control form-control-solid selectpicker" data-live-search="true" data-placeholder="Selectionner un pays de naissance">
                                     <option value=""></option>
                                     @foreach(\App\Helper\CountryHelper::getAll() as $country)
                                         <option value="{{ $country->name->common }}" data-content="<div class='d-flex flex-row mb-1 h-20px'><div class='symbol symbol-20px me-2'><img src='{{ $country->flags->png }}' alt=''/></div> {{ $country->name->common }}</div>">{{ $country->name->common }}</option>
