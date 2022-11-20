5363820beb882ac3a82ed190b8790ff6310d2010
mmockelyn <trainznation@gmail.com>
2022-11-20 21:49:32 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Services/Sirene.php
+++ app/Services/Sirene.php
@@ -6,11 +6,11 @@ class Sirene
 {
     public static function siren($siren)
     {
-        return collect(\Http::withBasicAuth(config("insee.consumer_key"), config("insee.consumer_secret"))->post('https://api.insee.fr/entreprises/sirene/V3/siren', ["q" => "siren:".$siren])->object());
+        return collect(\Http::withToken('05c06cd6-ef2c-3c8c-bf55-f4e757d5c113')->post('https://api.insee.fr/entreprises/sirene/V3/siren', ["q" => "siren:".$siren])->object());
     }
 
     public static function siret($siret)
     {
-        return \Http::withBasicAuth(config("insee.consumer_key"), config("insee.consumer_secret"))->post('https://api.insee.fr/entreprises/sirene/V3/siret', ["q" => "siret:".$siret])->status();
+        return \Http::withToken('05c06cd6-ef2c-3c8c-bf55-f4e757d5c113')->post('https://api.insee.fr/entreprises/sirene/V3/siret', ["q" => "siret:".$siret])->status();
     }
 }
