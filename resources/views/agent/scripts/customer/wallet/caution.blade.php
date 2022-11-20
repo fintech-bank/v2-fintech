e3aeea389edd92c2869f0d8ce63294ce1b85af51
mmockelyn <trainznation@gmail.com>
2022-11-20 21:59:31 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/wallet/caution.blade.php
+++ resources/views/agent/scripts/customer/wallet/caution.blade.php
@@ -48,7 +48,7 @@
                         let p = document.createElement('p')
                         elements.inputSiret.classList.remove('is-valid')
                         elements.inputSiret.classList.add('is-invalid')
-                        elements.inputSiret.append(p)
+                        elements.inputSiret.parentNode.append(p)
                         p.classList.remove('text-success')
                         p.classList.add('text-danger')
                         p.innerHTML = data.header.message
@@ -56,7 +56,7 @@
                         let p = document.createElement('p')
                         elements.inputSiret.classList.remove('is-invalid')
                         elements.inputSiret.classList.add('is-valid')
-                        elements.inputSiret.append(p)
+                        elements.inputSiret.parentNode.append(p)
                         p.classList.remove('text-danger')
                         p.classList.add('text-success')
                         p.innerHTML = data.header.message
