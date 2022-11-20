7e0b590e5ffe82cfcd72db68451801495df4debd
mmockelyn <trainznation@gmail.com>
2022-11-20 19:02:15 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/scripts/customer/wallet/caution.blade.php
+++ resources/views/agent/scripts/customer/wallet/caution.blade.php
@@ -5,4 +5,22 @@
     let forms = {}
     let dataTable = {}
     let block = {}
+
+    let countryBirthOptions = (item) => {
+        if (!item.id) {
+            return item.text;
+        }
+        let span = document.createElement('span');
+        let imgUrl = item.element.getAttribute('data-flag');
+        let template = '';
+        template += '<img src="' + imgUrl + '" class="rounded-circle w-20px h-20px me-2" alt="image" />';
+        template += item.text;
+        span.innerHTML = template;
+        return $(span);
+    }
+
+    $("#country_naissance").select2({
+        templateSelection: countryBirthOptions,
+        templateResult: countryBirthOptions
+    })
 </script>
