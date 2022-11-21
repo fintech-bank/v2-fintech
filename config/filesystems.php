1052b9dbfd762e228b68433271007bcf7b57cf5c
mmockelyn <trainznation@gmail.com>
2022-11-22 00:52:50 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- config/filesystems.php
+++ config/filesystems.php
@@ -57,15 +57,12 @@ return [
         ],
 
         'gdd' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
+            'driver' => 'sftp',
+            'host' => '192.168.1.50',
+            'port' => '5555',
+            'username' => env('SSH_USERNAME'),
+            'password' => env("SSH_PASSWORD"),
+            'root' => '/www/wwwroot/files.fintech.ovh/gdd/'
         ],
 
         'ficap' => [
