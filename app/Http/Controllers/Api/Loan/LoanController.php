94eb82697e85ff9c988330c03ebf657b357f065d
mmockelyn <trainznation@gmail.com>
2022-11-21 17:37:17 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Api/Loan/LoanController.php
+++ app/Http/Controllers/Api/Loan/LoanController.php
@@ -6,16 +6,21 @@ use App\Helper\CustomerLoanHelper;
 use App\Http\Controllers\Api\ApiController;
 use App\Http\Controllers\Controller;
 use App\Models\Customer\CustomerPret;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class LoanController extends ApiController
 {
-    public function list(Request $request)
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function list($limit = 10, $start = null, $end = null)
     {
         $data = collect([
-            'limit' => $request->get('limit'),
-            'start' => $request->get('start'),
-            'end' => $request->get('end')
+            'limit' => $limit,
+            'start' => $start == null ? now()->startOfYear() : $start,
+            'end' => $end == null ? now()->endOfYear() : $end,
         ])->first();
 
         $call = CustomerPret::whereBetween('created_at', [$data->start, $data->end])->limit($data->limit)->get();
