3a301226ae574079be5c0ee5105f62b1b58997d7
mmockelyn <trainznation@gmail.com>
2022-11-22 19:32:23 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Services/Mapbox.php
+++ app/Services/Mapbox.php
@@ -4,8 +4,20 @@ namespace App\Services;
 
 class Mapbox
 {
-    public function call()
+    /**
+     * insurance/supermarket/bar/contractor/shopping center
+     * @param $category
+     * @return object|array
+     */
+    public function call($category): object|array
     {
-        return \Http::get('https://api.mapbox.com/geocoding/v5/mapbox.places/airport.json?type=poi&proximity=-74.70850,40.78375&access_token='.config('services.mapbox.api_key'))->object();
+        $data = [
+            'type' => 'poi',
+            'limit' => 5,
+            'language' => 'fr',
+            'country' => 'fr',
+            'access_token' => config('services.mapbox.api_key')
+        ];
+        return \Http::get('https://api.mapbox.com/geocoding/v5/mapbox.places/'.$category.'.json', $data)->object();
     }
 }
