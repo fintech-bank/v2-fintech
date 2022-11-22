9c8351616954475adbc527f61eb00c94e07c53ae
mmockelyn <trainznation@gmail.com>
2022-11-22 23:20:44 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -110,7 +110,7 @@ class HomeController extends Controller
         $insurance = $customer->insurances()->first();
         $pret = $customer->prets()->first();
 
-        dd(collect(Vicopo::https(85100))[0]->city);
+        dd(round(85158, 2));
 
         return $document->generatePDF('loan.caution_simple', $customer, null, ["pret" => $pret, "wallet" => $wallet], false, false, null, true, 'simple');
     }
