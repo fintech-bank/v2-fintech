d80aa78c443dcd22b8402c07527dd7b99c07b620
mmockelyn <trainznation@gmail.com>
2022-11-22 03:29:50 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Api/Document/DocumentController.php
+++ app/Http/Controllers/Api/Document/DocumentController.php
@@ -62,7 +62,7 @@ class DocumentController extends ApiController
     private function verifyCaution($num_phone, $code)
     {
         $caution = CustomerPretCaution::where('phone', $num_phone)->first();
-        dd(base64_encode($caution->code_sign), $code);
+        dd(base64_decode($caution->code_sign), $code);
 
         if(base64_decode($caution->code_sign) == $code) {
             $caution->update([
