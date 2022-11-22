d9a047d909c40e3c2e2df30ffdc206b98230e507
mmockelyn <trainznation@gmail.com>
2022-11-22 23:21:45 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -110,7 +110,7 @@ class HomeController extends Controller
         $insurance = $customer->insurances()->first();
         $pret = $customer->prets()->first();
 
-        dd(intval(round(85158, 2)));
+        dd(round(85158, 2, PHP_ROUND_HALF_UP));
 
         return $document->generatePDF('loan.caution_simple', $customer, null, ["pret" => $pret, "wallet" => $wallet], false, false, null, true, 'simple');
     }
