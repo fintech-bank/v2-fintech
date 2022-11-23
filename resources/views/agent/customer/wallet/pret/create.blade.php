bde5be4d0b2c16619f1263d536e74cb5954c342c
mmockelyn <trainznation@gmail.com>
2022-11-23 02:27:31 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/pret/create.blade.php
+++ resources/views/agent/customer/wallet/pret/create.blade.php
@@ -183,19 +183,10 @@
 
                                 <x-form.select
                                     name="wallet_payment_id"
-                                    label="Compte de Paiement"
-                                    placeholder="Selectionner un compte de paiement..."
+                                    label="Type d'asurance"
+                                    placeholder="Selectionner un type d'assurance..."
                                     :datas="\App\Models\Customer\CustomerPret::dataTypeInsurance()->all()" />
 
-                                <div class="mb-10">
-                                    <label for="assurance_type" class="form-label required">Type d'assurance</label>
-                                    <select class="form-control form-control-solid" data-control="select2" name="assurance_type" data-placeholder="Selectionner un type d'assurance" required>
-                                        <option value=""></option>
-                                        <option value="D">Décès</option>
-                                        <option value="DIM">Décès, Invalidité, Maladie</option>
-                                        <option value="DIMC">Décès, Invalidité, Maladie, Perte d'emploi</option>
-                                    </select>
-                                </div>
                             </div>
                             <!--begin::Step 1-->
 
--- resources/views/components/form/select.blade.php
+++ resources/views/components/form/select.blade.php
@@ -1,8 +1,8 @@
 <div class="mb-10">
-    <label for="{{ $name }}" class="form-label {{ isset($required) ? "required" : "" }}">
+    <label for="{{ $name }}" class="form-label {{ $required !== null ? "required" : "" }}">
         {{ $label }}
     </label>
-    <select id="{{ $name }}" class="form-control selectpicker" name="{{ $name }}" data-live-search="true" data-header="{{ isset($placeholder) ?? $label }}">
+    <select id="{{ $name }}" class="form-control selectpicker" name="{{ $name }}" data-live-search="true" data-header="{{ isset($placeholder) ?? $label }}" {{ $required !== null ? 'required' : "" }}>
         <option value=""></option>
         @foreach($datas as $data)
             <option value="{{ $data['id'] }}" @if(isset($value) && $data['id'] == $value) selected="selected" @endif data-content="{!! $data['value'] !!}">{{ $data['value'] }}</option>
