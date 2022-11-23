4aac5d0dc0a8f658ebc89268049ca4dedf5cd17a
mmockelyn <trainznation@gmail.com>
2022-11-23 03:00:57 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Helper/CustomerSituationHelper.php
+++ app/Helper/CustomerSituationHelper.php
@@ -6,55 +6,50 @@ class CustomerSituationHelper
 {
     public static function dataLegalCapacity()
     {
-        return json_encode([
-            [
-                'name' => 'Majeur Capable',
-            ],
-            [
-                'name' => 'Majeur sous tutelle',
-            ],
-            [
-                'name' => 'Mineur',
-            ],
+        return collect([
+            ['id' => "Majeur Capable", "name" => "Majeur Capable"],
+            ['id' => "Majeur sous tutelle", "name" => "Majeur sous tutelle"],
+            ['id' => "Mineur", "name" => "Mineur"],
         ]);
     }
 
     public static function dataFamilySituation()
     {
-        return json_encode([
-            ['name' => 'Célibataire'],
-            ['name' => 'Divorcé'],
-            ['name' => 'Marié'],
-            ['name' => 'Pacsé'],
-            ['name' => 'Séparé de corps'],
-            ['name' => 'Union Libre'],
-            ['name' => 'Veuf(ve)'],
+        return collect([
+            ['id' => "Célibataire", "name" => "Célibataire"],
+            ['id' => "Divorcé", "name" => "Divorcé"],
+            ['id' => "Marié", "name" => "Marié"],
+            ['id' => "Pacsé", "name" => "Pacsé"],
+            ['id' => "Séparé de corps", "name" => "Séparé de corps"],
+            ['id' => "Union Libre", "name" => "Union Libre"],
+            ['id' => "Veuf(ve)", "name" => "Veuf(ve)"],
         ]);
     }
 
     public static function dataLogement()
     {
-        return json_encode([
-            ['name' => 'Propriétaire'],
-            ['name' => 'Locataire'],
-            ['name' => "Logé par l'employeur"],
-            ['name' => 'Logé à titre gratuit'],
-            ['name' => 'Logé par les parents'],
-            ['name' => 'Sans Domicile Fixe'],
-            ['name' => 'Hôtel, Autres'],
+        return collect([
+            ["id" => "Propriétaire", "name" => "Propriétaire"],
+            ["id" => "Locataire", "name" => "Locataire"],
+            ["id" => "Logé par l'employeur", "name" => "Logé par l'employeur"],
+            ["id" => "Logé à titre gratuit", "name" => "Logé à titre gratuit"],
+            ["id" => "Logé par les parents", "name" => "Logé par les parents"],
+            ["id" => "Sans Domicile Fixe", "name" => "Sans Domicile Fixe"],
+            ["id" => "Hôtel, Autres", "name" => "Hôtel, Autres"],
         ]);
     }
 
     public static function dataProCategories()
     {
-        return json_encode([
-            ['name' => 'Agriculteur'],
-            ['name' => "Artisan, Commerçant, Chef d'Entreprise"],
-            ['name' => 'Cadre'],
-            ['name' => 'Employé'],
-            ['name' => 'Ouvriers'],
-            ['name' => 'Retraiter'],
-            ['name' => 'Sans Emploie'],
+        return collect([
+            ['id' => 'Agriculteur', 'name' => 'Agriculteur'],
+            ['id' => "Artisan, Commerçant, Chef d'Entreprise", 'name' => "Artisan, Commerçant, Chef d'Entreprise"],
+            ['id' => "Artisan, Commerçant, Chef d'Entreprise", 'name' => "Artisan, Commerçant, Chef d'Entreprise"],
+            ['id' => "Cadre", 'name' => "Cadre"],
+            ['id' => "Employé", 'name' => "Employé"],
+            ['id' => "Ouvriers", 'name' => "Ouvriers"],
+            ['id' => "Retraiter", 'name' => "Retraiter"],
+            ['id' => "Sans Emploie", 'name' => "Sans Emploie"],
         ]);
     }
 
