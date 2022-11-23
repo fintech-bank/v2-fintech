712e2e333b71826e72933ceadb859dd710a79d96
mmockelyn <trainznation@gmail.com>
2022-11-23 03:01:55 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Helper/CustomerSituationHelper.php
+++ app/Helper/CustomerSituationHelper.php
@@ -7,49 +7,49 @@ class CustomerSituationHelper
     public static function dataLegalCapacity()
     {
         return collect([
-            ['id' => "Majeur Capable", "name" => "Majeur Capable"],
-            ['id' => "Majeur sous tutelle", "name" => "Majeur sous tutelle"],
-            ['id' => "Mineur", "name" => "Mineur"],
+            ['id' => "Majeur Capable", "value" => "Majeur Capable"],
+            ['id' => "Majeur sous tutelle", "value" => "Majeur sous tutelle"],
+            ['id' => "Mineur", "value" => "Mineur"],
         ]);
     }
 
     public static function dataFamilySituation()
     {
         return collect([
-            ['id' => "Célibataire", "name" => "Célibataire"],
-            ['id' => "Divorcé", "name" => "Divorcé"],
-            ['id' => "Marié", "name" => "Marié"],
-            ['id' => "Pacsé", "name" => "Pacsé"],
-            ['id' => "Séparé de corps", "name" => "Séparé de corps"],
-            ['id' => "Union Libre", "name" => "Union Libre"],
-            ['id' => "Veuf(ve)", "name" => "Veuf(ve)"],
+            ['id' => "Célibataire", "value" => "Célibataire"],
+            ['id' => "Divorcé", "value" => "Divorcé"],
+            ['id' => "Marié", "value" => "Marié"],
+            ['id' => "Pacsé", "value" => "Pacsé"],
+            ['id' => "Séparé de corps", "value" => "Séparé de corps"],
+            ['id' => "Union Libre", "value" => "Union Libre"],
+            ['id' => "Veuf(ve)", "value" => "Veuf(ve)"],
         ]);
     }
 
     public static function dataLogement()
     {
         return collect([
-            ["id" => "Propriétaire", "name" => "Propriétaire"],
-            ["id" => "Locataire", "name" => "Locataire"],
-            ["id" => "Logé par l'employeur", "name" => "Logé par l'employeur"],
-            ["id" => "Logé à titre gratuit", "name" => "Logé à titre gratuit"],
-            ["id" => "Logé par les parents", "name" => "Logé par les parents"],
-            ["id" => "Sans Domicile Fixe", "name" => "Sans Domicile Fixe"],
-            ["id" => "Hôtel, Autres", "name" => "Hôtel, Autres"],
+            ["id" => "Propriétaire", "value" => "Propriétaire"],
+            ["id" => "Locataire", "value" => "Locataire"],
+            ["id" => "Logé par l'employeur", "value" => "Logé par l'employeur"],
+            ["id" => "Logé à titre gratuit", "value" => "Logé à titre gratuit"],
+            ["id" => "Logé par les parents", "value" => "Logé par les parents"],
+            ["id" => "Sans Domicile Fixe", "value" => "Sans Domicile Fixe"],
+            ["id" => "Hôtel, Autres", "value" => "Hôtel, Autres"],
         ]);
     }
 
     public static function dataProCategories()
     {
         return collect([
-            ['id' => 'Agriculteur', 'name' => 'Agriculteur'],
-            ['id' => "Artisan, Commerçant, Chef d'Entreprise", 'name' => "Artisan, Commerçant, Chef d'Entreprise"],
-            ['id' => "Artisan, Commerçant, Chef d'Entreprise", 'name' => "Artisan, Commerçant, Chef d'Entreprise"],
-            ['id' => "Cadre", 'name' => "Cadre"],
-            ['id' => "Employé", 'name' => "Employé"],
-            ['id' => "Ouvriers", 'name' => "Ouvriers"],
-            ['id' => "Retraiter", 'name' => "Retraiter"],
-            ['id' => "Sans Emploie", 'name' => "Sans Emploie"],
+            ['id' => 'Agriculteur', 'value' => 'Agriculteur'],
+            ['id' => "Artisan, Commerçant, Chef d'Entreprise", 'value' => "Artisan, Commerçant, Chef d'Entreprise"],
+            ['id' => "Artisan, Commerçant, Chef d'Entreprise", 'value' => "Artisan, Commerçant, Chef d'Entreprise"],
+            ['id' => "Cadre", 'value' => "Cadre"],
+            ['id' => "Employé", 'value' => "Employé"],
+            ['id' => "Ouvriers", 'value' => "Ouvriers"],
+            ['id' => "Retraiter", 'value' => "Retraiter"],
+            ['id' => "Sans Emploie", 'value' => "Sans Emploie"],
         ]);
     }
 
