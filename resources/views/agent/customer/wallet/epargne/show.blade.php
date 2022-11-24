efa89bbee952f7566a03bdd21097aa2e0d4d501d
mmockelyn <trainznation@gmail.com>
2022-11-24 18:30:09 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/epargne/show.blade.php
+++ resources/views/agent/customer/wallet/epargne/show.blade.php
@@ -159,96 +159,9 @@
     </div>
     <div class="tab-content" id="myTabContent">
         <div class="tab-pane fade show active" id="transactions" role="tabpanel">
-            <div class="card shadow-sm">
-                <div class="card-header">
-                    <div class="card-title">
-                        <!--begin::Search-->
-                        <div class="d-flex align-items-center position-relative my-1">
-                            <span class="svg-icon svg-icon-1 position-absolute ms-4">
-								<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<rect opacity="0.5" x="17.0365" y="15.1223" width="8.15546" height="2" rx="1" transform="rotate(45 17.0365 15.1223)" fill="currentColor" />
-									<path d="M11 19C6.55556 19 3 15.4444 3 11C3 6.55556 6.55556 3 11 3C15.4444 3 19 6.55556 19 11C19 15.4444 15.4444 19 11 19ZM11 5C7.53333 5 5 7.53333 5 11C5 14.4667 7.53333 17 11 17C14.4667 17 17 14.4667 17 11C17 7.53333 14.4667 5 11 5Z" fill="currentColor" />
-								</svg>
-							</span>
-                            <input type="text" data-kt-transaction-filter="search" class="form-control form-control-solid w-250px ps-14" placeholder="Rechercher..." />
-                        </div>
-                    </div>
-                    <div class="card-toolbar flex-row-fluid justify-content-end gap-5">
-                        <div class="input-group w-250px">
-                            <input class="form-control form-control-solid rounded rounded-end-0" placeholder="Ranger de date" id="kt_transaction_flatpickr" />
-                            <button class="btn btn-icon btn-light" id="kt_transaction_flatpickr_clear">
-                                <span class="svg-icon svg-icon-2">
-									<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<rect opacity="0.5" x="7.05025" y="15.5356" width="12" height="2" rx="1" transform="rotate(-45 7.05025 15.5356)" fill="currentColor" />
-										<rect x="8.46447" y="7.05029" width="12" height="2" rx="1" transform="rotate(45 8.46447 7.05029)" fill="currentColor" />
-									</svg>
-								</span>
-                            </button>
-                        </div>
-                        <div class="w-100 mw-150px">
-                            <!--begin::Select2-->
-                            <select class="form-select form-select-solid" data-control="select2" data-hide-search="true" data-placeholder="Types" data-kt-transaction-filter="types">
-                                <option></option>
-                                <option value="all">Tous</option>
-                                <option value="depot">Dépot</option>
-                                <option value="retrait">Retrait</option>
-                                <option value="payment">Paiement CB</option>
-                                <option value="virement">Virement bancaire</option>
-                                <option value="sepa">Prélèvement</option>
-                                <option value="frais">Frais Bancaire</option>
-                                <option value="souscription">Souscription</option>
-                                <option value="autre">Autre</option>
-                                <option value="facelia">Crédit Facelia</option>
-                            </select>
-                            <!--end::Select2-->
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body pt-0">
-                    <table class="table align-middle table-row-dashed fs-6 gy-5" id="kt_transaction_table">
-                        <thead>
-                        <tr class="text-start text-gray-400 fw-bold fs-7 text-uppercase gs-0">
-                            <th class="min-w-100px">Date</th>
-                            <th class="d-none"></th>
-                            <th class="min-w-175px">Libellé</th>
-                            <th class="text-end">Montant</th>
-                            <th class="text-end"></th>
-                        </tr>
-                        </thead>
-                        <tbody class="fw-semibold text-gray-600">
-                        @foreach($wallet->transactions()->where('confirmed', true)->orderBy('confirmed_at', 'desc')->get() as $transaction)
-                            <tr>
-                                <td data-order="{{ $transaction->confirmed ? $transaction->confirmed_at->format('Y-m-d') : $transaction->updated_at->format('Y-m-d') }}">
-                                    {{ $transaction->confirmed ? $transaction->confirmed_at->format('Y-m-d') : $transaction->updated_at->format('Y-m-d') }}
-                                </td>
-                                <td class="d-none" data-order="{{ $transaction->type }}">{{ $transaction->type }}</td>
-                                <td>
-                                    <div class="d-flex flex-row align-items-center">
-                                        {!! $transaction->type_symbol !!}
-                                        <div class="d-flex flex-column">
-                                            {{ $transaction->description }}
-                                            <div class="text-muted">{{ $transaction->designation }}</div>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td class="text-end">
-                                    @if($transaction->amount < 0)
-                                        <span class="text-danger">{{ $transaction->amount_format }}</span>
-                                    @else
-                                        <div class="text-success fw-semibold">+{{ $transaction->amount_format }}</div>
-                                    @endif
-                                </td>
-                                <td class="text-end">
-                                    @if($transaction->type == 'frais' || $transaction->type == 'souscription')
-                                        <button class="btn btn-xs btn-danger btn-icon btnRemb" data-transaction="{{ $transaction->uuid }}" data-bs-toggle="tooltip" title="Rembourser"><i class="fa-solid fa-ban"></i> </button>
-                                    @endif
-                                </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+            <x-base.underline
+                title="Transaction au {{ now()->format('d/m/Y') }}"
+                class="w-450px" />
         </div>
         <div class="tab-pane fade" id="infos" role="tabpanel">
             <div class="row">
