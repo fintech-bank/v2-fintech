4a6878629eeb88980d889ca225d60d389f04fda5
mmockelyn <trainznation@gmail.com>
2022-11-24 18:55:29 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/epargne/show.blade.php
+++ resources/views/agent/customer/wallet/epargne/show.blade.php
@@ -204,8 +204,12 @@
                         @foreach($wallet->transactions as $transaction)
                             <div class="card card-flush my-1 accordion-item">
                                 <div class="card-header accordion-header">
-                                    <div class="d-flex flex-row justify-content-between p-5" data-bs-toggle="collapse" data-bs-target="#kt_accordion_1_body_1" aria-expanded="true" aria-controls="kt_accordion_1_body_1">
-
+                                    <div class="d-flex flex-row justify-content-between align-items-center p-5" data-bs-toggle="collapse" data-bs-target="#kt_accordion_1_body_1" aria-expanded="true" aria-controls="kt_accordion_1_body_1">
+                                        {!! $transaction->type_symbol !!}
+                                        {{ $transaction->designation }}
+                                        <div class="text-end">
+                                            {{ $transaction->amount_format }}
+                                        </div>
                                     </div>
                                     <!--<button class="accordion-button fs-4 fw-semibold" type="button" data-bs-toggle="collapse" data-bs-target="#kt_accordion_1_body_1" aria-expanded="true" aria-controls="kt_accordion_1_body_1">
 
