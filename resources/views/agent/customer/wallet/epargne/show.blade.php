5dfd52fc71be1e98f94ff889fd40de9c48b84373
mmockelyn <trainznation@gmail.com>
2022-11-24 20:30:41 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/epargne/show.blade.php
+++ resources/views/agent/customer/wallet/epargne/show.blade.php
@@ -163,88 +163,68 @@
                 title="Transaction au {{ now()->format('d/m/Y') }}"
                 class="w-650px" />
             <div class="separator border-gray-300 my-5"></div>
-            <div
-                id="kt_docs_search_handler_basic"
+            <div class="w-50" data-kt-search-element="suggestions">
 
-                data-kt-search-keypress="true"
-                data-kt-search-min-length="2"
-                data-kt-search-enter="true"
-                data-kt-search-layout="inline">
-                <div class="w-50" data-kt-search-element="suggestions">
-
-                    @foreach($wallet->transactions as $transaction)
-                        <div class="mb-5">
-                            <a class="d-flex flex-row h-50px p-5 justify-content-between align-items-center rounded bg-white mb-0" data-bs-toggle="collapse" href="#content">
-                                <div class="d-flex flex-row align-items-center text-black">
-                                    {!! $transaction->getTypeSymbolAttribute() !!}
-                                    <div class="d-flex flex-column">
-                                        {{ $transaction->designation }}<br>
-                                        <div class="text-muted">
-                                            {{ $transaction->confirmed ? $transaction->confirmed_at->format('d/m/Y') : ($transaction->differed ? $transaction->differed_at->format('d/m/Y') : $transaction->updated_at->format("d/m/Y")) }}
-                                        </div>
-                                    </div>
-                                </div>
-                                @if($transaction->amount < 0)
-                                    <span class="text-danger fw-bolder">{{ $transaction->amount_format }}</span>
-                                @else
-                                    <span class="text-success fw-bolder">+ {{ $transaction->amount_format }}</span>
-                                @endif
-                            </a>
-                            <div class="collapse" id="content">
-                                <div class="card card-body">
-                                    <div class="ps-5 text-muted mb-5">{{ $transaction->type_text }}</div>
-                                    <div class="mb-5">
-                                        <x-base.underline
-                                            title="Détails de l'opération"
-                                            class="mb-2"
-                                            size-text="fs-3"
-                                            size="3"
-                                            color="{{ $transaction->type_color }}" />
-                                        <div class="d-flex flex-row justify-content-around">
-                                            <div>Transaction effectuée le: {{ $transaction->updated_at->format("d/m/Y") }}</div>
-                                            <div>Comptabilisé à la date du: {{ $transaction->confirmed ? $transaction->confirmed_at->format("d/m/Y") : ($transaction->differed ? $transaction->differed_at->format("d/m/Y") : $transaction->updated_at->format("d/m/Y")) }}</div>
-                                        </div>
-                                    </div>
-                                    <div class="mb-5">
-                                        <x-base.underline
-                                            title="Libellé complet"
-                                            class="mb-2"
-                                            size-text="fs-3"
-                                            size="3"
-                                            color="{{ $transaction->type_color }}" />
-                                        <div class="d-flex flex-column">
-                                            <div class="fw-bold">{{ $transaction->designation }}</div>
-                                            <div>{{ $transaction->description }}</div>
-                                        </div>
-                                    </div>
-                                    <div class="text-center">
-                                        <button class="btn btn-lg btn-circle btn-success btnAcceptTransaction me-2" data-transaction="{{ $transaction->id }}"><i class="fa-solid fa-check-circle me-2"></i> Accepter</button>
-                                        <button class="btn btn-lg btn-circle btn-danger btnRejectTransaction me-2" data-transaction="{{ $transaction->id }}"><i class="fa-solid fa-xmark-circle me-2"></i> Refuser</button>
-                                        @if($transaction->type == 'payment')
-                                            <button class="btn btn-lg btn-circle btn-info btnOppositPayment me-2" data-transaction="{{ $transaction->id }}"><i class="fa-solid fa-ban me-2"></i> Opposition</button>
-                                        @endif
-                                        @if($transaction->type == 'frais')
-                                            <button class="btn btn-lg btn-circle btn-info btnRemb me-2" data-transaction="{{ $transaction->id }}"><i class="fa-solid fa-exchange me-2"></i> Remboursement</button>
-                                        @endif
+                @foreach($wallet->transactions as $transaction)
+                    <div class="mb-5">
+                        <a class="d-flex flex-row h-50px p-5 justify-content-between align-items-center rounded bg-white mb-0" data-bs-toggle="collapse" href="#content">
+                            <div class="d-flex flex-row align-items-center text-black">
+                                {!! $transaction->getTypeSymbolAttribute() !!}
+                                <div class="d-flex flex-column">
+                                    {{ $transaction->designation }}<br>
+                                    <div class="text-muted">
+                                        {{ $transaction->confirmed ? $transaction->confirmed_at->format('d/m/Y') : ($transaction->differed ? $transaction->differed_at->format('d/m/Y') : $transaction->updated_at->format("d/m/Y")) }}
                                     </div>
                                 </div>
                             </div>
+                            @if($transaction->amount < 0)
+                                <span class="text-danger fw-bolder">{{ $transaction->amount_format }}</span>
+                            @else
+                                <span class="text-success fw-bolder">+ {{ $transaction->amount_format }}</span>
+                            @endif
+                        </a>
+                        <div class="collapse" id="content">
+                            <div class="card card-body">
+                                <div class="ps-5 text-muted mb-5">{{ $transaction->type_text }}</div>
+                                <div class="mb-5">
+                                    <x-base.underline
+                                        title="Détails de l'opération"
+                                        class="mb-2"
+                                        size-text="fs-3"
+                                        size="3"
+                                        color="{{ $transaction->type_color }}" />
+                                    <div class="d-flex flex-row justify-content-around">
+                                        <div>Transaction effectuée le: {{ $transaction->updated_at->format("d/m/Y") }}</div>
+                                        <div>Comptabilisé à la date du: {{ $transaction->confirmed ? $transaction->confirmed_at->format("d/m/Y") : ($transaction->differed ? $transaction->differed_at->format("d/m/Y") : $transaction->updated_at->format("d/m/Y")) }}</div>
+                                    </div>
+                                </div>
+                                <div class="mb-5">
+                                    <x-base.underline
+                                        title="Libellé complet"
+                                        class="mb-2"
+                                        size-text="fs-3"
+                                        size="3"
+                                        color="{{ $transaction->type_color }}" />
+                                    <div class="d-flex flex-column">
+                                        <div class="fw-bold">{{ $transaction->designation }}</div>
+                                        <div>{{ $transaction->description }}</div>
+                                    </div>
+                                </div>
+                                <div class="text-center">
+                                    <button class="btn btn-lg btn-circle btn-success btnAcceptTransaction me-2" data-transaction="{{ $transaction->id }}"><i class="fa-solid fa-check-circle me-2"></i> Accepter</button>
+                                    <button class="btn btn-lg btn-circle btn-danger btnRejectTransaction me-2" data-transaction="{{ $transaction->id }}"><i class="fa-solid fa-xmark-circle me-2"></i> Refuser</button>
+                                    @if($transaction->type == 'payment')
+                                        <button class="btn btn-lg btn-circle btn-info btnOppositPayment me-2" data-transaction="{{ $transaction->id }}"><i class="fa-solid fa-ban me-2"></i> Opposition</button>
+                                    @endif
+                                    @if($transaction->type == 'frais')
+                                        <button class="btn btn-lg btn-circle btn-info btnRemb me-2" data-transaction="{{ $transaction->id }}"><i class="fa-solid fa-exchange me-2"></i> Remboursement</button>
+                                    @endif
+                                </div>
+                            </div>
                         </div>
-                    @endforeach
+                    </div>
+                @endforeach
 
-                </div>
-                <div data-kt-search-element="results" class="w-50 d-none">
-                    ...
-                </div>
-                <div data-kt-search-element="empty" class="w-50 text-center d-none">
-                    <div class="fw-semibold py-0 mb-10">
-                        <div class="text-gray-600 fs-3 mb-2">Nous sommes désolés, il n'y a aucun résultat correspondant à votre recherche.</div>
-                        <div class="text-gray-400 fs-6">Veuillez vérifier l'orthographe des mots saisis ou complétez-les par un nouveau mot clé.</div>
-                    </div>
-                    <div class="text-center px-4">
-                        <img class="mw-100 mh-200px" alt="image" src="/assets/media/illustrations/sigma-1/2.png">
-                    </div>
-                </div>
             </div>
         </div>
         <div class="tab-pane fade" id="infos" role="tabpanel">
