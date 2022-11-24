996646dfb7a58dd1a8cd2051332a74980dd17cfc
mmockelyn <trainznation@gmail.com>
2022-11-24 21:17:53 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/epargne/show.blade.php
+++ resources/views/agent/customer/wallet/epargne/show.blade.php
@@ -345,10 +345,10 @@
                                         {{ $transfer->transfer_date->format("d/m/Y") }}
                                     </div>
                                 </div>
-                                <div class="card-body">
+                                <div class="card-body h-20px">
                                     <div class="d-flex flex-row justify-content-between">
                                         {{ $transfer->type_text }}
-                                        <a href="" class="btn btn-link"><i class="fa-solid fa-refresh me-2"></i> Renouveler</a>
+                                        <a href="" class="btn btn-link"><i class="fa-solid fs-2 fa-refresh me-2"></i> Renouveler</a>
                                     </div>
                                 </div>
                             </div>
