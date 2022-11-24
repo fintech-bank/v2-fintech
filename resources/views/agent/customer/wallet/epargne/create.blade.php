c994d2f7e70675654d85dba6cced3f0e50fcb9db
mmockelyn <trainznation@gmail.com>
2022-11-24 01:10:13 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/epargne/create.blade.php
+++ resources/views/agent/customer/wallet/epargne/create.blade.php
@@ -109,7 +109,7 @@
                             </button>-->
                         </div>
                     </div>
-                    <div class="card-body">
+                    <div class="card-body d-none">
                         <div class="mb-7">
                             <h5 class="mb-3">Intêret annuel</h5>
                             <div class="mb-0" data-content="profit_percent"></div>
@@ -134,6 +134,9 @@
                             <h5 class="mb-3">Limite de capacité</h5>
                             <div class="mb-0" data-content="limit_amount"></div>
                         </div>
+                    </div>
+                    <div class="card-footer">
+                        <x-form.button text="Souscrire" />
                     </div>
                 </div>
             </div>
