f281a00c2ee6f4bd679d62d297e3e38fae20638d
mmockelyn <trainznation@gmail.com>
2022-11-24 23:13:10 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/epargne/show.blade.php
+++ resources/views/agent/customer/wallet/epargne/show.blade.php
@@ -141,13 +141,13 @@
                         </li>
                     @endif
 
-                    @if($wallet->epargne->plan->info_retrait->retrait_type->money || $wallet->epargne->plan->info_retrait->retrait_type->card)
+                    @if(json_decode($wallet->epargne->plan->info_retrait)->retrait_type->money || json_decode($wallet->epargne->plan->info_retrait)->retrait_type->card)
                         <li class="nav-item mt-2">
                             <a class="nav-link text-active-primary ms-0 me-10 py-5" data-bs-toggle="tab" href="#withdraw"><i class="fa-solid fa-money-bill-transfer me-2"></i> Retrait bancaire</a>
                         </li>
                     @endif
 
-                    @if($wallet->epargne->plan->info_versement->depot_type->money || $wallet->epargne->plan->info_versement->depot_type->card || $wallet->epargne->plan->info_versement->depot_type->check)
+                    @if(json_decode($wallet->epargne->plan->info_versement)->depot_type->money || json_decode($wallet->epargne->plan->info_versement)->depot_type->card || json_decode($wallet->epargne->plan->info_versement)->depot_type->check)
                         <li class="nav-item mt-2">
                             <a class="nav-link text-active-primary ms-0 me-10 py-5" data-bs-toggle="tab" href="#deposit"><i class="fa-solid fa-money-bill-transfer me-2"></i> Dépot bancaire</a>
                         </li>
