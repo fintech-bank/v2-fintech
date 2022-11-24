c08f3a66459cef002aec6ef1c5c8b977340cd4a0
mmockelyn <trainznation@gmail.com>
2022-11-24 17:37:25 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/agent/customer/wallet/epargne/show.blade.php
+++ resources/views/agent/customer/wallet/epargne/show.blade.php
@@ -136,6 +136,18 @@
                     </li>
                 @endif
 
+                @if($wallet->epargne->plan->info_retrait->retrait_type->money || $wallet->epargne->plan->info_retrait->retrait_type->card)
+                    <li class="nav-item mt-2">
+                        <a class="nav-link text-active-primary ms-0 me-10 py-5" data-bs-toggle="tab" href="#withdraw"><i class="fa-solid fa-up-down me-2"></i> Retrait bancaire</a>
+                    </li>
+                @endif
+
+                @if($wallet->epargne->plan->info_versement->depot_type->money || $wallet->epargne->plan->info_versement->depot_type->card || $wallet->epargne->plan->info_versement->depot_type->check)
+                    <li class="nav-item mt-2">
+                        <a class="nav-link text-active-primary ms-0 me-10 py-5" data-bs-toggle="tab" href="#deposit"><i class="fa-solid fa-up-down me-2"></i> Dépot bancaire</a>
+                    </li>
+                @endif
+
                 <!--end::Nav item-->
             </ul>
             <!--begin::Navs-->
