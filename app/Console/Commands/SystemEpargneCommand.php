21c86e88de51a17c3b0d94ac8ef88795c661203f
mmockelyn <trainznation@gmail.com>
2022-11-24 02:47:18 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Console/Commands/SystemEpargneCommand.php
+++ app/Console/Commands/SystemEpargneCommand.php
@@ -50,7 +50,7 @@ class SystemEpargneCommand extends Command
 
     private function activeWallet()
     {
-        $wallets = CustomerWallet::epargne()->active()->get();
+        $wallets = CustomerWallet::toEpargne()->active()->get();
         $i = 0;
 
         foreach ($wallets as $wallet) {
