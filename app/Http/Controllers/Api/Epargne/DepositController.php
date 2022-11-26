db7b028c3a319b67fdb2b06944c84e5564984ffd
mmockelyn <trainznation@gmail.com>
2022-11-26 23:10:02 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Api/Epargne/DepositController.php
+++ app/Http/Controllers/Api/Epargne/DepositController.php
@@ -2,16 +2,65 @@
 
 namespace App\Http\Controllers\Api\Epargne;
 
+use App\Helper\CustomerTransactionHelper;
+use App\Http\Controllers\Api\ApiController;
 use App\Http\Controllers\Controller;
 use App\Models\Customer\CustomerEpargne;
+use App\Models\Customer\CustomerMoneyDeposit;
 use Illuminate\Http\Request;
 
-class DepositController extends Controller
+class DepositController extends ApiController
 {
     public function store($reference_epargne, Request $request)
     {
-        dd($request->all());
         $epargne = CustomerEpargne::where('reference', $reference_epargne)->first();
 
+        return match ($request->get('type_deposit')) {
+            "money" => $this->depositMoney($epargne, $request),
+            "check" => $this->depositCheck($epargne, $request)
+        };
+    }
+
+    private function depositMoney(CustomerEpargne $epargne, Request $request)
+    {
+        $deposit = CustomerMoneyDeposit::create([
+            'reference' => generateReference(),
+            'amount' => $request->get('amount_money'),
+            'status' => 'terminated',
+            'code' => base64_encode(0000),
+            'customer_wallet_id' => $epargne->wallet->id,
+            'customer_withdraw_dab_id' => 1
+        ]);
+
+        $transaction = CustomerTransactionHelper::createCredit(
+            $epargne->wallet_id,
+            'depot',
+            "Dépot d'espèce en Agence",
+            "Dépot {$deposit->reference} | {$deposit->created_at->format('d/m H:i')}",
+            $deposit->amount,
+            true,
+            now()
+        );
+
+        $deposit->update(['customer_transaction_id' => $transaction->id]);
+
+        return $this->sendSuccess(null, [$deposit]);
+    }
+
+    private function depositCheck(CustomerEpargne $epargne, Request $request)
+    {
+        $lists = collect();
+
+        for ($i=0; $i <= 10; $i++) {
+            $lists->push([
+                'number' => $request->get('number')[$i],
+                'amount' => $request->get('amount')[$i],
+                'name_deposit' => $request->get('name_deposit')[$i],
+                'bank_deposit' => $request->get('bank_deposit')[$i],
+                'verified' => $request->has('verified')[$i],
+            ]);
+        }
+
+        return $lists;
     }
 }
