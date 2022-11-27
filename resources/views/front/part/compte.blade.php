6e7a817bc418bbb15e373cf720f34025b9bb1949
mmockelyn <trainznation@gmail.com>
2022-11-27 22:57:35 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/front/part/compte.blade.php
+++ resources/views/front/part/compte.blade.php
@@ -4,18 +4,19 @@
     <div class="d-flex flex-row w-100 min-h-350px min-h-lg-500px bgi-no-repeat bgi-size-contain bgi-position-x-center bgi-position-y-bottom landing-dark-bg" style="background-image: url('/assets/media/svg/illustrations/landing.svg')">
         <div class="d-flex flex-column w-75">
             <div class="d-flex flex-row p-20 align-items-center">
-                <div class="badge badge-lg badge-info me-10">Crédit Facelia</div>
-                <div class="text-info uppercase ">Vos avantages</div>
+                <div class="badge badge-lg badge-info me-10">Exclusivité</div>
+                <div class="text-info uppercase ">Jusqu'au 30 décembre 2022 inclus</div>
             </div>
-            <div class="fs-2tx fw-bolder text-white uppercase px-20 underline mb-20">C'est facile avec le crédit facélia*</div>
+            <div class="fs-2tx fw-bolder text-white uppercase px-20 underline mb-20">OFFRES SURPRISES JUSQU’À 160 € OFFERTS</div>
             <div class="d-flex flex-row justify-content-start align-items-center px-20">
                 <i class="fa-regular fa-circle-pause text-white fa-5x me-10"></i>
-                <p class="text-white fs-2x l-1-5">Reportez et modulez vos échéances simplement. On
-                    vous accompagne dans toutes les étapes du financement
-                    de votre projet.</p>
+                <p class="text-white fs-2x l-1-5">
+                    <strong>On vous offre 80 € pour l’ouverture de votre compte et 80€ supplémentaires</strong> en cas de mobilité bancaire avec notre service Bienvenue.<br>
+                    <strong>Vous n’avez pas encore de carte chez nous ?</strong> La première année est gratuite*.
+                </p>
             </div>
             <div class="w-100 px-20 mt-10">
-                <a href="" class="btn btn-circle btn-lg btn-light w-300px text-bank fw-bolder">En savoir plus</a>
+                <a href="{{ route('auth.register.firstStep') }}" class="btn btn-circle btn-lg btn-light w-300px text-bank fw-bolder">En profiter dès maintenant</a>
             </div>
         </div>
         <img src="https://particuliers.societegenerale.fr/static/Particuliers/assets/img/campagnes/2022/11/black-friday/black-friday-D.webp" alt="" class="img-fluid h-100">
