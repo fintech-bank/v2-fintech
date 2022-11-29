885592e159df3368cd7f90451d9c8d090b4aec56
mmockelyn <trainznation@gmail.com>
2022-11-29 20:36:22 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
     {
         return view('customer.dashboard', [
             'customer' => Customer::find(auth()->user()->customers->id),
-            'gauge' => $chart->build()
+            'chart' => $chart->build()
         ]);
     }
 }
