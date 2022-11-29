3071d259d56ea7932d4a52b006a6040265c1c9dd
mmockelyn <trainznation@gmail.com>
2022-11-29 17:31:53 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -16,6 +16,17 @@
             </div>
             <button class="btn btn-circle btn-lg btn-primary">J'en profite</button>
         </div>
+        <div class="mt-10">
+            <div class="row">
+                <div class="col-md-4 col-sm-12">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
