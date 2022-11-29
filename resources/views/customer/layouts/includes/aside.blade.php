b29b25acb81a4ee6247c4c07c2a42b978bae9325
mmockelyn <trainznation@gmail.com>
2022-11-29 17:22:20 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/layouts/includes/aside.blade.php
+++ resources/views/customer/layouts/includes/aside.blade.php
@@ -29,7 +29,7 @@
             <!--begin::Menu-->
             <div class="menu menu-column menu-rounded menu-sub-indention px-3" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
                 <!--begin:Menu item-->
-                <div class="menu-item {{ route_is(['customer.dashboard']) }}">
+                <div class="menu-item {{ route_is(['customer.dashboard']) ? 'here' : ''}}">
                     <!--begin:Menu link-->
                     <a class="menu-link" href="{{ route('customer.dashboard') }}">
 						<span class="menu-icon">
