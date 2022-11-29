bd5c33b2012e347ef6a894fdff9cedeaa346488e
mmockelyn <trainznation@gmail.com>
2022-11-29 20:35:52 +0100

    feat(Agent Client Création Client) : Edition de la partie client

---
+++ app/Http/Controllers/Customer/Account/NotifyController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Customer\Account;
+
+use App\Http\Controllers\Controller;
+
+class NotifyController extends Controller
+{
+    public function index()
+    {
+
+    }
+}
