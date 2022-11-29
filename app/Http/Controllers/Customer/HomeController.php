5d8f27e31cab6ae39a075103ce4090be4704ea35
mmockelyn <trainznation@gmail.com>
2022-11-29 17:39:10 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer\Customer;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
-        return view('customer.dashboard');
+        return view('customer.dashboard', [
+            'customer' => Customer::find(auth()->user()->customers->id)
+        ]);
     }
 }
--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -25,6 +25,32 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-md-4 col-sm-12">
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <div class="d-flex flex-row justify-content-between align-items-center p-5 rounded shadow-lg">
+                                @foreach($customer->wallets()->where('status', 'active')->get() as $wallet)
+                                    @if($wallet->type == 'compte')
+                                        <div class="d-flex flex-row">
+                                            <div class="symbol symbol-50px me-3">
+                                                <div class="symbol-label fs-2 fw-semibold bg-success text-inverse-success"><i class="fa-solid fa-wallet"></i> </div>
+                                            </div>
+                                            <div class="d-flex flex-column">
+                                                <strong>{{ $wallet->name_account_generic }}</strong>
+                                                {{ $wallet->type_text }}
+                                            </div>
+                                        </div>
+                                        {{ $wallet->solde_remaining }}
+                                    @elseif($wallet->type == 'epargne')
+
+                                    @else
+
+                                    @endif
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
