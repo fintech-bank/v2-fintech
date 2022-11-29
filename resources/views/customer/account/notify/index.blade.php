3d0935c34dbdd7389946682b646edd620ac7c726
mmockelyn <trainznation@gmail.com>
2022-11-30 00:49:55 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/notify/index.blade.php
+++ resources/views/customer/account/notify/index.blade.php
@@ -5,6 +5,11 @@
 @endsection
 
 @section('toolbar')
+    <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
+        <!--begin::Title-->
+        <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">Fil de notification</h1>
+        <!--end::Title-->
+    </div>
 @endsection
 
 @section("content")
