a486c574e2c51e55399c79fa8dbbbbab99253fc1
mmockelyn <trainznation@gmail.com>
2022-11-29 17:28:17 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -9,7 +9,13 @@
 
 @section("content")
     <div id="app" class="rounded">
-
+        <div class="d-flex flex-row justify-content-between align-items-center bg-light-primary rounded m-0 p-5">
+            <div class="d-flex flex-column">
+                <strong>Les offres du moment</strong>
+                <div class="text-muted">Découvez vos avantages</div>
+            </div>
+            <button class="btn btn-circle btn-lg btn-primary">J'en profite</button>
+        </div>
     </div>
 @endsection
 
