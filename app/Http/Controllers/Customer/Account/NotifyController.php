02b7f065aa0ddee8eb0a78217b2eb37561ad5021
mmockelyn <trainznation@gmail.com>
2022-11-30 13:37:39 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/Account/NotifyController.php
+++ app/Http/Controllers/Customer/Account/NotifyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Customer\Account;
 
 use App\Http\Controllers\Controller;
 use App\Models\Customer\Customer;
+use Illuminate\Notifications\DatabaseNotification;
 
 class NotifyController extends Controller
 {
@@ -11,6 +12,16 @@ class NotifyController extends Controller
     {
         return view('customer.account.notify.index', [
             'notifications' => Customer::find(auth()->user()->customers->id)->info->notifications
+        ]);
+    }
+
+    public function show($notify_id)
+    {
+        $notify = DatabaseNotification::find($notify_id);
+        $notify->markAsRead();
+
+        return view('customer.account.notify.show', [
+            'notification' => $notify
         ]);
     }
 }
---
+++ resources/views/customer/account/notify/show.blade.php
@@ -0,0 +1,35 @@
+@extends("customer.layouts.app")
+
+@section("css")
+
+@endsection
+
+@section('toolbar')
+    <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
+        <!--begin::Title-->
+        <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">Fil de notification</h1>
+        <!--end::Title-->
+    </div>
+@endsection
+
+@section("content")
+    <div id="app" class="rounded container">
+        <div class="bg-gray-300 p-5">
+            <div class="card shadow-sm">
+                <div class="card-header">
+                    <h3 class="card-title"><i class="fa-solid {{ $notification->data['icon'] }} me-5"></i> {{ $notification->data['category'] }}</h3>
+                    <div class="card-toolbar text-muted">
+                        {{ \Carbon\Carbon::createFromTimestamp(strtotime($notification->data['time']))->format("d/m/Y") }}
+                    </div>
+                </div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section("script")
+    @include("customer.scripts.account.notify.show")
+@endsection
