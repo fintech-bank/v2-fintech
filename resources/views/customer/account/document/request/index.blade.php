4b04a4d3d03e826edafbdcacbc41dc0dce5fe126
mmockelyn <trainznation@gmail.com>
2022-11-30 18:47:47 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/document/request/index.blade.php
+++ resources/views/customer/account/document/request/index.blade.php
@@ -57,22 +57,28 @@
                 color="warning"
                 title="Mes demandes en traitement" />
 
-            @foreach($requests->where('status', 'progress')->get() as $request)
-                <a href="{{ route('customer.account.documents.request.show', $request->reference) }}" class="d-flex flex-row justify-content-between align-items-center shadow rounded h-75px mb-10 hover-zoom text-black" target="_blank">
-                    <div class="d-flex flex-row align-items-center">
-                        <div class="p-0 w-8px bg-{{ $request->getStatus('color') }} h-75px rounded-start me-5">&nbsp;</div>
-                        <div class="d-flex flex-column">
-                            <span class="fs-2 fw-bold">{{ $request->sujet }}</span>
-                            <div class="text-muted fs-6">{{ $request->updated_at->format('d/m/Y') }}</div>
+            @if($request->where('status', 'progress')->count() != 0)
+                @foreach($requests->where('status', 'progress')->get() as $request)
+                    <a href="{{ route('customer.account.documents.request.show', $request->reference) }}" class="d-flex flex-row justify-content-between align-items-center shadow rounded h-75px mb-10 hover-zoom text-black" target="_blank">
+                        <div class="d-flex flex-row align-items-center">
+                            <div class="p-0 w-8px bg-{{ $request->getStatus('color') }} h-75px rounded-start me-5">&nbsp;</div>
+                            <div class="d-flex flex-column">
+                                <span class="fs-2 fw-bold">{{ $request->sujet }}</span>
+                                <div class="text-muted fs-6">{{ $request->updated_at->format('d/m/Y') }}</div>
+                            </div>
                         </div>
-                    </div>
-                    <span class="d-flex flex-row">
+                        <span class="d-flex flex-row">
                         <span class="">{{ $request->model_data }}</span>
                         <span class="vr mx-2 "></span>
                         <i class="fa-solid fa-arrow-right-long fs-2 me-5"></i>
                     </span>
-                </a>
-            @endforeach
+                    </a>
+                @endforeach
+            @else
+                <div class="d-flex flex-center w-100 p-5">
+                    Aucune requête en traitement pour le moment !
+                </div>
+            @endif
         </div>
     </div>
 @endsection
