814de6f2998d83c58e0aa833328ef3b43ec7fb09
mmockelyn <trainznation@gmail.com>
2022-11-30 16:15:16 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/Account/Document/DocumentController.php
+++ app/Http/Controllers/Customer/Account/Document/DocumentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Customer\Account\Document;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer\Customer;
 
 class DocumentController extends Controller
 {
@@ -10,6 +11,15 @@ class DocumentController extends Controller
     {
         return view('customer.account.document.index', [
             'customer' => auth()->user()->customers
+        ]);
+    }
+
+    public function category($category_id)
+    {
+        $documents = Customer::find(auth()->user()->customers->id)->documents()->where('document_category_id', $category_id)->get();
+
+        return view('customer.account.document.category', [
+            'documents' => $documents
         ]);
     }
 }
