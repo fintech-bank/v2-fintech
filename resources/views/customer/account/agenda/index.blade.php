b62f81431da7c1d13f394d39d73cf10ab8fc07ab
mmockelyn <trainznation@gmail.com>
2022-11-30 19:43:22 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/agenda/index.blade.php
+++ resources/views/customer/account/agenda/index.blade.php
@@ -18,26 +18,19 @@
                 class="w-auto my-5"/>
         </div>
         <div class="d-flex flex-center w-50 bg-gray-300 rounded p-5 mx-auto">
-            <a href="" class="d-flex flex-row justify-content-between bg-white align-items-center shadow rounded h-75px mb-10 hover-zoom text-black w-100">
-                <div class="d-flex flex-row align-items-center py-5">
-                    <div class="p-0 w-8px bg-bank h-75px rounded-start me-5">&nbsp;</div>
-                    <div class="d-flex flex-column">
-                        <span class="fs-2 fw-bold">M. MOCKELYN Maxime</span>
-                        <div class="fs-5">{{ formatDateFrench(\Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 10:00:00")), true) }} ({{ \Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 10:00:00"))->longAbsoluteDiffForHumans(\Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 11:00:00"))) }})</div>
-                        <div class="fs-5 text-muted">Par téléphone</div>
-                    </div>
-                </div>
-            </a>
             @foreach($events as $event)
-                <div class="d-flex flex-row justify-content-between align-items-center shadow rounded h-75px mb-10 hover-zoom text-black">
+                <a href="{{ route('customer.account.agenda.show', $event->id) }}" class="d-flex flex-row justify-content-between bg-white align-items-center shadow rounded h-75px mb-10 hover-zoom text-black w-100">
                     <div class="d-flex flex-row align-items-center">
                         <div class="p-0 w-8px bg-bank h-75px rounded-start me-5">&nbsp;</div>
                         <div class="d-flex flex-column">
-                            <span class="fs-2 fw-bold">{{ $event->reason }}</span>
+                            <span class="fs-2 fw-bold">M. MOCKELYN Maxime</span>
+                            <div class="fs-5">{{ formatDateFrench(\Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 10:00:00")), true) }} ({{ \Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 10:00:00"))->longAbsoluteDiffForHumans(\Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 11:00:00"))) }})</div>
+                            <div class="fs-5 text-muted">Par téléphone</div>
                         </div>
                     </div>
-                </div>
+                </a>
             @endforeach
+            <button class="btn btn-lg btn-circle btn-bank">Demander un rendez-vous</button>
         </div>
     </div>
 @endsection
