52703eedb1ebe53f71820385f81e2ab469f300d2
mmockelyn <trainznation@gmail.com>
2022-11-30 01:14:17 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/notify/index.blade.php
+++ resources/views/customer/account/notify/index.blade.php
@@ -17,7 +17,19 @@
         <div class="card shadow-sm rounded">
             <div class="card-body bg-gray-300 rounded">
                 @foreach($notifications as $notification)
-                    {{$notification}}
+                    <div class="card shadow-sm">
+                        <div class="card-header">
+                            <h3 class="card-title"><i class="fa-solid {{ $notification->data->icon }} me-5"></i> </h3>
+                            <div class="card-toolbar">
+                                <!--<button type="button" class="btn btn-sm btn-light">
+                                    Action
+                                </button>-->
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            
+                        </div>   
+                    </div>
                 @endforeach
             </div>
         </div>
