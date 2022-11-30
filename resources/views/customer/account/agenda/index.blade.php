2eb186c4db675bdd4e24c191ea34a658099d4755
mmockelyn <trainznation@gmail.com>
2022-11-30 22:00:13 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/agenda/index.blade.php
+++ resources/views/customer/account/agenda/index.blade.php
@@ -229,6 +229,12 @@
                                         @foreach(\App\Models\Core\Event::getDataReason() as $reason)
                                             <div class="">
                                                 <input type="radio" class="btn-check" name="reason" value="{{ $reason['name'] }}" checked="checked"  id="{{ Str::slug($reason['name']) }}"/>
+                                                <label class="btn btn-outline btn-outline-dashed btn-active-light-primary p-7 d-flex align-items-center mb-5" for="kt_radio_buttons_2_option_1">
+                                                    <span class="d-block fw-semibold text-start">
+                                                        <span class="text-dark fw-bold d-block fs-3">{{ $reason['name'] }}</span>
+                                                    </span>
+                                                    <i class="fa-solid fa-chevron-right fs-4x ms-4"></i>
+                                                </label>
                                             </div>
                                         @endforeach
 
