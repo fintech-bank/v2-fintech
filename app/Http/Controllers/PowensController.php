20acc4021454e4e10d4e48ac5ef67d59c1b21c0b
mmockelyn <trainznation@gmail.com>
2022-11-30 17:27:26 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/PowensController.php
+++ app/Http/Controllers/PowensController.php
@@ -8,7 +8,7 @@ class PowensController extends Controller
 {
     public function webview()
     {
-        return redirect()->to("https://fintech-sandbox.biapi.pro/2.0/auth/webview/connect?client_id=14116510&redirect_uri=".route('powens.success'));
+        return redirect()->to("https://fintech-sandbox.biapi.pro/2.0/auth/webview/connect?client_id=14116510&redirect_uri=https://v2.fintech.ovh/powens/webview/success");
     }
 
     public function success(Request $request)
