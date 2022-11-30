dbfa77c820a5ad9be2b25f4b8d6a39ada2e77263
mmockelyn <trainznation@gmail.com>
2022-11-30 18:03:08 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Services/Powens/Client.php
+++ app/Services/Powens/Client.php
@@ -6,6 +6,6 @@ class Client extends Powens
 {
     public function list()
     {
-        return \Http::withToken($this->auth_token)->get($this->endpoint."users/me")->object();
+        return \Http::withToken($this->auth_token)->get($this->endpoint."users/me/accounts")->object();
     }
 }
