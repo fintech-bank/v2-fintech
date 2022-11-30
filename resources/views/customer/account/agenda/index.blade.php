e0a343a670dbe85bcdce5193ee1bc0a80fd5add2
mmockelyn <trainznation@gmail.com>
2022-11-30 22:35:13 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/agenda/index.blade.php
+++ resources/views/customer/account/agenda/index.blade.php
@@ -246,7 +246,7 @@
                                             size-text="fs-1" class="w-auto mb-10"/>
                                     </div>
                                     <div class="d-flex flex-row justify-content-around">
-                                        <label class="d-flex flex-column w-200px rounded border">
+                                        <label class="d-flex flex-column w-200px rounded border p-5 text-center">
                                             <i class="fa-solid fa-building fs-2x"></i>
                                             <span class="">En agence</span>
                                             <span class="fs-6">Durée estimée : 1h00</span>
