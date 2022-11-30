1c5d9b4fdae66833dacf050708c37dce4ec9ca63
mmockelyn <trainznation@gmail.com>
2022-11-30 19:45:51 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/agenda/index.blade.php
+++ resources/views/customer/account/agenda/index.blade.php
@@ -18,19 +18,25 @@
                 class="w-auto my-5"/>
         </div>
         <div class="d-flex flex-center w-50 bg-gray-300 rounded p-5 mx-auto">
-            @foreach($events as $event)
-                <a href="{{ route('customer.account.agenda.show', $event->id) }}" class="d-flex flex-row justify-content-between bg-white align-items-center shadow rounded h-75px mb-10 hover-zoom text-black w-100">
-                    <div class="d-flex flex-row align-items-center">
-                        <div class="p-0 w-8px bg-bank h-75px rounded-start me-5">&nbsp;</div>
-                        <div class="d-flex flex-column">
-                            <span class="fs-2 fw-bold">M. MOCKELYN Maxime</span>
-                            <div class="fs-5">{{ formatDateFrench(\Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 10:00:00")), true) }} ({{ \Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 10:00:00"))->longAbsoluteDiffForHumans(\Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 11:00:00"))) }})</div>
-                            <div class="fs-5 text-muted">Par téléphone</div>
+            @if($events->count() == 0)
+                <div class="d-flex flex-center w-100">
+                    <i class="fa-solid fa-exclamation-circle text-warning opacity-15 me-5"></i> Vous n'avez aucun rendez-vous programmé
+                </div>
+            @else
+                @foreach($events as $event)
+                    <a href="{{ route('customer.account.agenda.show', $event->id) }}" class="d-flex flex-row justify-content-between bg-white align-items-center shadow rounded h-75px mb-10 hover-zoom text-black w-100">
+                        <div class="d-flex flex-row align-items-center">
+                            <div class="p-0 w-8px bg-bank h-75px rounded-start me-5">&nbsp;</div>
+                            <div class="d-flex flex-column">
+                                <span class="fs-2 fw-bold">M. MOCKELYN Maxime</span>
+                                <div class="fs-5">{{ formatDateFrench(\Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 10:00:00")), true) }} ({{ \Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 10:00:00"))->longAbsoluteDiffForHumans(\Carbon\Carbon::createFromTimestamp(strtotime("2022-12-01 11:00:00"))) }})</div>
+                                <div class="fs-5 text-muted">Par téléphone</div>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            @endforeach
-            <button class="btn btn-lg btn-circle btn-bank">Demander un rendez-vous</button>
+                    </a>
+                @endforeach
+            @endif
+            <button class="btn btn-lg btn-circle btn-bank">Prendre un rendez-vous</button>
         </div>
     </div>
 @endsection
