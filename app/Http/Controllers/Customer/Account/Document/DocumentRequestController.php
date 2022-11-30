bc4884cbbafffac8b47d34f6b7d514acc3628d45
mmockelyn <trainznation@gmail.com>
2022-11-30 18:10:00 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/Account/Document/DocumentRequestController.php
+++ app/Http/Controllers/Customer/Account/Document/DocumentRequestController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Customer\Account\Document;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer\Customer;
 
 class DocumentRequestController extends Controller
 {
     public function index()
     {
-
+        $customer = Customer::find(auth()->user()->customers->id);
+        return view('customer.account.document.request.index', [
+            'requests' => $customer->requests
+        ]);
     }
 }
---
+++ resources/views/customer/account/document/request/index.blade.php
@@ -0,0 +1,32 @@
+@extends("customer.layouts.app")
+
+@section("css")
+
+@endsection
+
+@section('toolbar')
+    <div class="page-title d-flex flex-column justify-content-center flex-wrap me-3">
+        <!--begin::Title-->
+        <h1 class="page-heading d-flex text-dark fw-bold fs-3 flex-column justify-content-center my-0">Demandes & Documents</h1>
+        <!--end::Title-->
+    </div>
+@endsection
+
+@section("content")
+    <div id="app" class="rounded container">
+        <div class="d-flex flex-center w-100">
+            <div class="btn-group btn-group-lg mb-10">
+                @foreach(\App\Models\Core\DocumentCategory::with('documents')->get() as $category)
+                    <a href="{{ route('customer.account.documents.category', $category->id) }}" class="btn btn-lg {{ Route::is('customer.account.documents.category') ? 'btn-primary' : 'btn-secondary' }}">{{ $category->name }}</a>
+                @endforeach
+                <a href="{{ route('customer.account.documents.externe.index') }}" class="btn btn-lg {{ Route::is('customer.account.documents.externe.index') ? 'btn-primary' : 'btn-secondary' }}">Autres Documents</a>
+                <a href="{{ route('customer.account.documents.request.index') }}" class="btn btn-lg {{ Route::is('customer.account.documents.request.index') ? 'btn-primary' : 'btn-secondary' }}">Mes Requêtes</a>
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+@section("script")
+    @include("customer.scripts.account.notify.index")
+@endsection
