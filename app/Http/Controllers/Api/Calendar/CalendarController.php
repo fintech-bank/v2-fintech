8bf0d8db2ccf74af0db6aa18580b4e72f11ada7a
mmockelyn <trainznation@gmail.com>
2022-12-01 20:36:24 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Api/Calendar/CalendarController.php
+++ app/Http/Controllers/Api/Calendar/CalendarController.php
@@ -75,7 +75,7 @@ class CalendarController extends Controller
         $event = Event::create([
             'type' => $request->get('type'),
             'reason' => $reason['value'],
-            'subreason' => $request->get('subreason'),
+            'subreason' => $request->get('subreason_id'),
             'question' => $request->get('question'),
             'canal' => $request->get('canal'),
             'lieu' => $request->get('canal') == 'agency' ? $agent->agency->name : '',
