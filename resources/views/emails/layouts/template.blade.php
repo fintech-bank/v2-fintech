cfb7a787956f7b3515984b59a7aa1df5ad15d8fc
mmockelyn <trainznation@gmail.com>
2022-12-01 20:10:09 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/emails/layouts/template.blade.php
+++ resources/views/emails/layouts/template.blade.php
@@ -424,7 +424,11 @@
                                                                     style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:24px;margin-bottom:11px;color:#333333;font-size:16px">
                                                                     <strong>Bonjour {{ $customer->info->full_name }},</strong></p></td>
                                                         </tr>
-                                                        @yield("content")
+                                                        <tr>
+                                                            <td>
+                                                                @yield("content")
+                                                            </td>
+                                                        </tr>
                                                         @isset($actionText)
                                                             <tr>
                                                                 <td align="center" style="padding:0;Margin:0"><!--[if mso]>
