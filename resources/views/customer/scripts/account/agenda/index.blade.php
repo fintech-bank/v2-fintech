0a8b9735fdcf80db694b55aa6783add0589cce9d
mmockelyn <trainznation@gmail.com>
2022-12-01 17:47:14 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/scripts/account/agenda/index.blade.php
+++ resources/views/customer/scripts/account/agenda/index.blade.php
@@ -76,7 +76,7 @@
             maxTime: '18:29',
             stepMinute: 30,
             width: null,
-            returnFormat: 'jsdate',
+            returnFormat: 'iso8601',
             onChange: (event, inst) => {
                 console.log(event)
                 $("[name='start_at']").val(event.valueText)
