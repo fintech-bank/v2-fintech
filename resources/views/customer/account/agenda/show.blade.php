7c3ca71afcfd390bac868b1398111fd735f5f595
mmockelyn <trainznation@gmail.com>
2022-12-01 21:35:22 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/agenda/show.blade.php
+++ resources/views/customer/account/agenda/show.blade.php
@@ -25,17 +25,17 @@
                             </div>
                         </div>
                         <div class="separator separator-dotted border-gray-200 my-5"></div>
-                        <div class="d-flex flex-row align-items-center">
-                            <div class="symbol symbol-50px symbol-circle me-5">
-                                <div class="symbol-label fs-2 fw-semibold text-success"><i class="fa-solid fa-phone"></i></div>
+                        <div class="d-flex flex-row justify-content-between align-items-center">
+                            <div class="d-flex flex-row">
+                                <div class="symbol symbol-50px symbol-circle me-5">
+                                    <div class="symbol-label fs-2 fw-semibold text-success"><i class="fa-solid fa-phone"></i></div>
+                                </div>
+                                <div class="d-flex flex-column">
+                                    <strong>Contact téléphonique</strong>
+                                    <div class="text-muted"><i>{{ $event->agent->phone }}</i></div>
+                                </div>
                             </div>
-                            <div class="d-flex flex-column">
-                                <strong>Contact téléphonique</strong>
-                                <div class="text-muted"><i>{{ $event->agent->phone }}</i></div>
-                            </div>
-                            <div class="d-flex flex-end">
-                                <a href="tel:{{ $event->agent->phone }}" class="btn btn-sm btn-success btn-icon"><i class="fa-solid fa-phone"></i></a>
-                            </div>
+                            <a href="tel:{{ $event->agent->phone }}" class="btn btn-sm btn-success btn-icon"><i class="fa-solid fa-phone"></i></a>
                         </div>
                     </div>
                 </div>
