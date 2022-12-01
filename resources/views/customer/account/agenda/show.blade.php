8d3a4c7ca541a0f7183df2b1919a819be0de2497
mmockelyn <trainznation@gmail.com>
2022-12-01 21:14:17 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/agenda/show.blade.php
+++ resources/views/customer/account/agenda/show.blade.php
@@ -8,7 +8,22 @@
 
 @section("content")
     <div id="app" class="rounded container">
-
+        <div class="row">
+            <div class="col-md-4 col-sm-12 mb-5">
+                <div class="card shadow-sm">
+                    <div class="card-header">
+                        <h3 class="card-title"><i class="fa-solid fa-user text-black me-2"></i> Agent</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="d-flex flex-row">
+                            <div class="symbol symbol-50px symbol-circle">
+                                {!! $event->agent->user->avatar_symbol !!}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
