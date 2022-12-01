e0cf02e566514b2116685b3f90d36830c367c7e7
mmockelyn <trainznation@gmail.com>
2022-12-01 21:24:31 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/agenda/show.blade.php
+++ resources/views/customer/account/agenda/show.blade.php
@@ -10,12 +10,12 @@
     <div id="app" class="rounded container">
         <div class="row">
             <div class="col-md-4 col-sm-12 mb-5">
-                <div class="card shadow-sm">
+                <div class="card shadow-sm mb-10">
                     <div class="card-header">
                         <h3 class="card-title"><i class="fa-solid fa-user text-black me-2"></i> Agent</h3>
                     </div>
                     <div class="card-body">
-                        <div class="d-flex flex-row align-items-center mb-10">
+                        <div class="d-flex flex-row align-items-center">
                             <div class="symbol symbol-50px symbol-circle me-5">
                                 {!! $event->agent->user->avatar_symbol !!}
                             </div>
@@ -24,13 +24,14 @@
                                 <div class="text-muted"><i>{{ $event->agent->poste }}</i></div>
                             </div>
                         </div>
+                        <div class="separator my-10"></div>
                         <div class="d-flex flex-row align-items-center">
                             <div class="symbol symbol-50px symbol-circle me-5">
                                 <div class="symbol-label fs-2 fw-semibold text-success"><i class="fa-solid fa-phone"></i></div>
                             </div>
                             <div class="d-flex flex-column">
                                 <strong>Contact téléphonique</strong>
-                                <div class="text-muted"><i>{{ $event->agent->poste }}</i></div>
+                                <div class="text-muted"><i>{{ $event->agent->phone }}</i></div>
                             </div>
                         </div>
                     </div>
