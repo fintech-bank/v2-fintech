ec02d6e482cb906476c1e5fedaa6396830881ed7
mmockelyn <trainznation@gmail.com>
2022-12-02 00:44:01 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/Profil/Security/SecurityController.php
+++ app/Http/Controllers/Customer/Profil/Security/SecurityController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Customer\Profil\Security;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer\Customer;
 
 class SecurityController extends Controller
 {
     public function __invoke()
     {
-        return view('customer.account.profil.security.index');
+        return view('customer.account.profil.security.index', [
+            'customer' => Customer::find(auth()->user()->customers->id)
+        ]);
     }
 }
--- resources/views/customer/account/profil/security/index.blade.php
+++ resources/views/customer/account/profil/security/index.blade.php
@@ -14,9 +14,33 @@
 
 @section("content")
     <div id="app" class="rounded container">
-        <div class="text-center">
-            <div class="fs-2tx uppercase">Profil de {{ auth()->user()->customers->info->full_name }}</div>
+        <div class="text-center mb-10">
+            <div class="fs-2tx uppercase">Profil de {{ $customer->info->full_name }}</div>
             <div class="fs-1">Gérez ici vos moyens de sécurité : code secret, numéro de téléphone sécurité et Pass Sécurité.</div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-sm-12 mb-5">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <div class="fw-bolder fs-1">Numéro de téléphone de sécurité</div>
+                        <div class="d-flex flex-row border border-gray-300 p-8 align-items-center">
+                            @if($customer->info->mobileVerified)
+                                <i class="fa-solid fa-check-circle text-success fs-2tx me-5"></i>
+                                <div class="d-flex flex-column">
+                                    <span>Votre numéro de téléphone de sécurité est le:</span>
+                                    <div class="fw-bolder fs-1">{{ $customer->info->getMobileNumber('obscure') }}</div>
+                                </div>
+                            @else
+                                <i class="fa-solid fa-xmark-circle text-danger fs-2tx me-5"></i>
+                                <div class="d-flex flex-column">
+                                    <span>Votre numéro de téléphone de sécurité est le:</span>
+                                    <div class="fw-bolder fs-1">Aucun numéro de téléphone définie</div>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
