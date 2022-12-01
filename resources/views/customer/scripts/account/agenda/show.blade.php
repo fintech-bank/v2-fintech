722c4df7c9f24548b758e3055b4e89877a34f99c
mmockelyn <trainznation@gmail.com>
2022-12-01 23:00:22 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/scripts/account/agenda/show.blade.php
+++ resources/views/customer/scripts/account/agenda/show.blade.php
@@ -13,7 +13,30 @@
     if(elements.btnCancel) {
         elements.btnCancel.addEventListener('click', e => {
             e.preventDefault()
+            e.target.setAttribute('data-kt-indicator', 'on')
 
+            $.ajax({
+                url: `/api/calendar/${e.target.dataset.event}`,
+                method: 'DELETE',
+                success: data => {
+                    e.target.removeAttribute('data-kt-indicator')
+
+                    toastr.success(`Votre rendez-vous à été annulé avec succès`, `Votre rendez-vous`)
+
+                    setTimeout(() => {
+                        window.location.reload()
+                    }, 1200)
+                },
+                error: err => {
+                    e.target.removeAttribute('data-kt-indicator')
+                    e.target.classList.remove('btn-danger')
+                    e.target.classList.add('btn-danger')
+                    e.target.queryselector('.indicator-label').html('<i class="fa-solid fa-exclamation-triangle text-white me-2 fs-2"></i> Erreur')
+
+                    console.log(err)
+                    toastr.error(`Erreur lors de l'execution de l'appel, consulter les logs ou contacter un administrateur`, `Erreur Système`)
+                }
+            })
         })
     }
 
