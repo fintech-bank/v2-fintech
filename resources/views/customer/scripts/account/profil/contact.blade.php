966809a35bb4b45128af58a9dec490660245daf4
mmockelyn <trainznation@gmail.com>
2022-12-02 13:28:39 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/scripts/account/profil/contact.blade.php
+++ resources/views/customer/scripts/account/profil/contact.blade.php
@@ -22,12 +22,14 @@
         let modal = new bootstrap.Modal(modals.modalEditMail)
         modal.hide()
 
-        @if(Agent::isMobile())
+        modals.modalEditMail.addEventListener('hidden.bs.modal', () => {
+            @if(Agent::isMobile())
             let agent = '{{ Agent::device().'-'.Agent::platform().'-'.Agent::version(Agent::platform()).'-'.gethostname() }}'
             executeVerifiedAjax('secure', {{ $customer->id }}, {'form': form, 'url': url, 'method': method, 'data': data, 'btn': btn}, agent)
-        @else
+            @else
             executeVerifiedAjax('password', {{ $customer->id }}, {'form': form, 'url': url, 'method': method, 'data': data, 'btn': btn})
-        @endif
+            @endif
+        })
 
     })
 </script>
