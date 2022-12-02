b0725731d159d33e0f7cf92cfa7e27897127b76c
mmockelyn <trainznation@gmail.com>
2022-12-02 04:12:00 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/Profil/Identity/IdentityController.php
+++ app/Http/Controllers/Customer/Profil/Identity/IdentityController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Customer\Profil\Identity;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer\Customer;
 
 class IdentityController extends Controller
 {
     public function __invoke()
     {
-
+        return view('customer.account.profil.identity', [
+            'customer' => Customer::find(auth()->user()->id)
+        ]);
     }
 }
