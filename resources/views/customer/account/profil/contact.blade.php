3d6596d8cdfb6337ec8ace5640dca252b00297fb
mmockelyn <trainznation@gmail.com>
2022-12-02 11:49:53 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/profil/contact.blade.php
+++ resources/views/customer/account/profil/contact.blade.php
@@ -81,9 +81,9 @@
             color="secondary"
             class="w-100 mb-5" />
 
-        <div class="d-flex flex-row justify-content-around">
-            <div class="">N° et Voie</div>
-            <div class="">{{ $customer->info->address }}</div>
+        <div class="d-flex flex-row ">
+            <div class="w-50">N° et Voie</div>
+            <div class="w-50">{{ $customer->info->address }}</div>
         </div>
         <div class="my-3"></div>
         <div class="d-flex flex-row justify-content-around">
