5ada6bacca5d7b3aea410a0eaffe73b5728a86cf
mmockelyn <trainznation@gmail.com>
2022-12-02 13:21:44 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/scripts/account/profil/contact.blade.php
+++ resources/views/customer/scripts/account/profil/contact.blade.php
@@ -19,6 +19,8 @@
         let method = 'POST'
         let data = form.serializeArray()
         let btn = form.find('[type="submit"]')
+        let modal = new bootstrap.Modal(modals.modalEditMail)
+        modal.hide()
 
         @if(Agent::isMobile())
             let agent = '{{ Agent::device().'-'.Agent::platform().'-'.Agent::version(Agent::platform()).'-'.gethostname() }}'
