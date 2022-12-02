ae32fe9cda618f40e93da1af5b80773c7bcc625c
mmockelyn <trainznation@gmail.com>
2022-12-02 14:36:23 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/scripts/account/profil/contact.blade.php
+++ resources/views/customer/scripts/account/profil/contact.blade.php
@@ -4,10 +4,12 @@
     let modals = {
         modalEditMail: document.querySelector('#EditMail'),
         modalEditPhone: document.querySelector('#EditPhone'),
+        modalEditAddress: document.querySelector('#EditAddress'),
     }
     let forms = {
         formEditMail: document.querySelector("#formEditMail"),
         formEditPhone: document.querySelector("#formEditPhone"),
+        formEditAddress: document.querySelector("#formEditAddress"),
     }
     let dataTable = {}
     let block = {
@@ -42,6 +44,25 @@
         let data = form.serializeArray()
         let btn = form.find('[type="submit"]')
         let modal = new bootstrap.Modal(modals.modalEditPhone)
+        modal.hide()
+
+        @if(Agent::isMobile())
+        let agent = '{{ Agent::device().'-'.Agent::platform().'-'.Agent::version(Agent::platform()).'-'.gethostname() }}'
+        executeVerifiedAjax('secure', {{ $customer->id }}, {'form': form, 'url': url, 'method': method, 'data': data, 'btn': btn}, e, agent)
+        @else
+        executeVerifiedAjax('password', {{ $customer->id }}, {'form': form, 'url': url, 'method': method, 'data': data, 'btn': btn}, e)
+        @endif
+
+    })
+    $(forms.formEditAddress).on('submit', e => {
+
+        e.preventDefault()
+        let form = $(forms.formEditAddress)
+        let url = form.attr('action')
+        let method = 'PUT'
+        let data = form.serializeArray()
+        let btn = form.find('[type="submit"]')
+        let modal = new bootstrap.Modal(modals.modalEditAddress)
         modal.hide()
 
         @if(Agent::isMobile())
