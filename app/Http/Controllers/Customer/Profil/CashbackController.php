a3276222ee5c9d3cfe08f69828d42945b54daf57
mmockelyn <trainznation@gmail.com>
2022-12-02 23:05:26 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/Profil/CashbackController.php
+++ app/Http/Controllers/Customer/Profil/CashbackController.php
@@ -9,6 +9,16 @@ class CashbackController extends Controller
 {
     public function __invoke()
     {
-        return view('customer.account.profil.cashback', ["customer" => Customer::find(auth()->user()->customers->id)]);
+        $customer = Customer::find(auth()->user()->customers->id);
+
+        if($customer->setting->cashback) {
+            if($customer->setting->tos_cashback) {
+                return redirect()->to("https://cashback.fintech.ovh/");
+            } else {
+                return view('customer.account.profil.cashback', ["customer" => Customer::find(auth()->user()->customers->id)]);
+            }
+        } else {
+            return redirect()->back()->with('error', "Vous n'avez pas accès ou souscrit à ce services");
+        }
     }
 }
