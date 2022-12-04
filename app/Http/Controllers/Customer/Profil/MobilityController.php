d91b8ce26bea5c3f45cdb177dbdf7129e7ffba67
mmockelyn <trainznation@gmail.com>
2022-12-04 19:17:10 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/Profil/MobilityController.php
+++ app/Http/Controllers/Customer/Profil/MobilityController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Customer\Profil;
 
 use App\Http\Controllers\Controller;
+use App\Models\Customer\Customer;
 
 class MobilityController extends Controller
 {
     public function index()
     {
-        return view('customer.account.profil.mobility.index');
+        return view('customer.account.profil.mobility.index', [
+            'customer' => Customer::find(auth()->user()->customers->id)
+        ]);
     }
 }
