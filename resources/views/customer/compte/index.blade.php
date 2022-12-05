86c1cedacf196e3591c7314a3a3214e055490134
mmockelyn <trainznation@gmail.com>
2022-12-06 00:10:29 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/index.blade.php
+++ resources/views/customer/compte/index.blade.php
@@ -9,7 +9,11 @@
 
 @section("content")
     <div id="app" class="rounded">
-
+        <div class="row">
+            <div class="col-md-9 col-sm-12 mb-10">
+                <div class="fs-2tx fw-bold text-primary uppercase">Comptes bancaires</div>
+            </div>
+        </div>
     </div>
 @endsection
 
