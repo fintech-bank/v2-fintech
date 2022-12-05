339ba43a0892da8bc9707300737e6db090a1911b
mmockelyn <trainznation@gmail.com>
2022-12-06 00:15:24 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/index.blade.php
+++ resources/views/customer/compte/index.blade.php
@@ -13,7 +13,7 @@
             <div class="col-md-9 col-sm-12 mb-10">
                 <div class="fs-1 fw-bold text-primary uppercase">Comptes bancaires</div>
                 @foreach($wallets->where('type', 'compte')->get() as $wallet)
-                    <a href="" class="btn btn-flex btn-secondary px-3">
+                    <a href="" class="btn btn-flex btn-secondary px-3 w-100">
                         <span class="d-flex flex-row justify-content-between align-items-center">
                             <span class="d-flex flex-row">
                                 <span class="fw-bolder">Compte Bancaire <span class="text-muted">{{ Str::mask($wallet->number_account, 0, 5) }}</span></span>
