b1700a58947592559ba58570e087a7edb1fe5da5
mmockelyn <trainznation@gmail.com>
2022-12-05 17:12:04 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/profil/mobility/index.blade.php
+++ resources/views/customer/account/profil/mobility/index.blade.php
@@ -61,9 +61,7 @@
                 @endforeach
             </tbody>
         </table>
-        <div class="d-flex flex-end">
-            <x-form.button />
-        </div>
+
     </div>
     <div class="modal fade" tabindex="-1" id="Subscribe">
         <div class="modal-dialog modal-lg">
