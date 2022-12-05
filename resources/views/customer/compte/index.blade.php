968ca8b4915e3e9716e16adf532c9edf01d47882
mmockelyn <trainznation@gmail.com>
2022-12-06 00:53:45 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/index.blade.php
+++ resources/views/customer/compte/index.blade.php
@@ -37,7 +37,7 @@
                         @endforeach
                     </div>
                 @endforeach
-                @if($wallet->where('type', 'epargne')->count() != 0)
+                @if($wallets->where('type', 'epargne')->count() != 0)
                     <div class="separator separator-dashed border-gray-600 my-10"></div>
                     <div class="fs-1 fw-bold text-warning uppercase mb-3"><i class="fa-solid fa-coins fs-1 me-2 text-warning"></i> Comptes Epargne</div>
                     @foreach($wallets->where('type', 'epargne')->get() as $wallet)
