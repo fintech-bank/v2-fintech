9ff3db8c74b07139a89756f945e6ed14fe633877
mmockelyn <trainznation@gmail.com>
2022-12-06 00:02:44 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/Compte/CompteController.php
+++ app/Http/Controllers/Customer/Compte/CompteController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Customer\Compte;
 
 use App\Http\Controllers\Controller;
+use App\Services\GoCardless\Customer;
 
 class CompteController extends Controller
 {
     public function index()
     {
-
+        return view('customer.compte.index', [
+            'wallets' => Customer::find(auth()->user()->customers->id)->wallets
+        ]);
     }
 }
---
+++ resources/views/customer/compte/index.blade.php
@@ -0,0 +1,18 @@
+@extends("customer.layouts.app")
+
+@section("css")
+
+@endsection
+
+@section('toolbar')
+@endsection
+
+@section("content")
+    <div id="app" class="rounded">
+
+    </div>
+@endsection
+
+@section("script")
+    @include("customer.scripts.dashboard")
+@endsection
