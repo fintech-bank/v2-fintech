ba58691c5773769dc3534f89d51e37638e1d9816
mmockelyn <trainznation@gmail.com>
2022-12-05 22:49:15 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Agent/Account/ProfilController.php
+++ app/Http/Controllers/Agent/Account/ProfilController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Agent\Account;
 
 use App\Http\Controllers\Controller;
+use App\Services\GoCardless\Customer;
 use Illuminate\Http\Request;
 
 class ProfilController extends Controller
 {
     public function index()
     {
-        return view('customer.account.profil.index');
+        return view('customer.account.profil.index', [
+            "customer" => Customer::find(auth()->user()->customers->id)
+        ]);
     }
 }
