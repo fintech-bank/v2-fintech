04754f925fed262c1bf65191342d1f15ee799d32
mmockelyn <trainznation@gmail.com>
2022-12-06 00:54:35 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/Compte/CompteController.php
+++ app/Http/Controllers/Customer/Compte/CompteController.php
@@ -9,6 +9,7 @@ class CompteController extends Controller
 {
     public function index()
     {
+        dd(Customer::find(auth()->user()->customers->id)->wallets()->get());
         return view('customer.compte.index', [
             "customer" => Customer::find(auth()->user()->customers->id),
             'wallets' => Customer::find(auth()->user()->customers->id)->wallets()
