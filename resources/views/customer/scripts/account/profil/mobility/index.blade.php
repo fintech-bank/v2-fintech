afff953dd84e03f097ba37acbef4530edc73885f
mmockelyn <trainznation@gmail.com>
2022-12-05 12:20:50 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/scripts/account/profil/mobility/index.blade.php
+++ resources/views/customer/scripts/account/profil/mobility/index.blade.php
@@ -1,25 +1,9 @@
 <script type="text/javascript">
     let tables = {}
-    let elements = {
-        inputMobilityType: document.querySelectorAll('[name="mobility_type_id"]')
-    }
+    let elements = {}
     let modals = {}
     let forms = {}
     let dataTable = {}
     let block = {}
-    $("#cloture_old_account").fadeOut()
 
-    if(elements.inputMobilityType) {
-        elements.inputMobilityType.forEach(input => {
-            input.addEventListener('click', e => {
-                e.preventDefault()
-                console.log(e.target.value)
-                if(e.target.value === 1) {
-                    $("#cloture_old_account").fadeIn()
-                } else {
-                    $("#cloture_old_account").fadeOut()
-                }
-            })
-        })
-    }
 </script>
