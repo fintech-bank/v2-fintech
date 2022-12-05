5d8fc000bf5f1ee5a5bd76e5989f3c44d087007f
mmockelyn <trainznation@gmail.com>
2022-12-06 00:44:35 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/index.blade.php
+++ resources/views/customer/compte/index.blade.php
@@ -37,6 +37,7 @@
                     </div>
                 @endforeach
                 <div class="separator separator-dashed border-gray-600 my-10"></div>
+                <div class="fs-1 fw-bold text-warning uppercase"><i class="fa-solid fa-coins fs-1 me-2 text-warning"></i> Comptes Epargne</div>
             </div>
         </div>
     </div>
