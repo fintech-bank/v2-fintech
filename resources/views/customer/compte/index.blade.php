45b11bcf8802990f6f72ae8f91508b805adc4c13
mmockelyn <trainznation@gmail.com>
2022-12-06 00:48:37 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/index.blade.php
+++ resources/views/customer/compte/index.blade.php
@@ -19,7 +19,7 @@
                             <span class="text-muted">{{ Str::mask($wallet->number_account, '.', 0, 5) }}</span>
                         </span>
                         <span class="d-flex flex-row">
-                            {!! $wallet->status_label !!}
+                            <span class="me-10">{!! $wallet->status_label !!}</span>
                             <span class="fs-2 fw-bolder">{{ eur($wallet->solde_remaining) }}</span>
                         </span>
                     </a>
@@ -47,7 +47,7 @@
                                 <span class="text-muted">{{ Str::mask($wallet->number_account, '.', 0, 5) }}</span>
                             </span>
                             <span class="d-flex flex-row">
-                                <span class="ms-10">{!! $wallet->status_label !!}</span>
+                                <span class="me-10">{!! $wallet->status_label !!}</span>
                                 <span class="fs-2 fw-bolder">{{ eur($wallet->solde_remaining) }}</span>
                             </span>
                         </a>
