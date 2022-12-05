d5e9585f72bf38b56e0ae66fef53aaa65bc03d08
mmockelyn <trainznation@gmail.com>
2022-12-06 00:39:13 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/index.blade.php
+++ resources/views/customer/compte/index.blade.php
@@ -26,7 +26,9 @@
                         @foreach($wallet->cards as $card)
                             <a href="" class="d-flex flex-row justify-content-between align-items-center p-5 rounded border border-2 bg-gray-300 text-black hover-zoom">
                                 <span class="d-flex flex-row fs-2">
-                                    <img src="/storage/card/{{ $card->support->slug }}.png" alt="" class="img-fluid w-40px">
+                                    <span class="symbol symbol-50px symbol-2by3">
+                                        <img src="/storage/card/{{ $card->support->slug }}.png" alt="">
+                                    </span>
                                     <span class="fw-bolder">CB Visa </span>
                                     <span class="text-muted">{{ $card->number_card_oscure }}</span>
                                 </span>
