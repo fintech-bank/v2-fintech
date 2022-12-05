b352fca75b74126b09eaf5be8595ca31ded49592
mmockelyn <trainznation@gmail.com>
2022-12-05 09:49:32 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/dashboard.blade.php
+++ resources/views/customer/dashboard.blade.php
@@ -57,7 +57,7 @@
                                                     {{ $wallet->type_text }}
                                                 </div>
                                             </div>
-                                            <span class="text-black d-flex flex-end">{{ $wallet->solde_remaining }}</span>
+                                            <span class="text-black d-flex flex-end">{{ eur($wallet->solde_remaining) }}</span>
                                         </a>
                                     @elseif($wallet->type == 'epargne')
                                         <a href="{{ route('customer.compte.wallet', $wallet->uuid) }}" class="d-flex flex-row align-items-center text-black">
