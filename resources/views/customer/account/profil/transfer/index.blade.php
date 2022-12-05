d84e784ff3dd738dcda8c9ecfa5d02712ae128fb
mmockelyn <trainznation@gmail.com>
2022-12-05 20:18:01 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/account/profil/transfer/index.blade.php
+++ resources/views/customer/account/profil/transfer/index.blade.php
@@ -35,6 +35,58 @@
             </div>
         @endif
     </div>
+    <div class="modal fade" tabindex="-1" id="AddTransferAgency">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header bg-bank">
+                    <h3 class="modal-title text-white">Transfert Agence simple</h3>
+                    <!--begin::Close-->
+                    <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal" aria-label="Close">
+                        <i class="fa-solid fa-xmark text-white fs-1"></i>
+                    </div>
+                    <!--end::Close-->
+                </div>
+
+                <form id="formAddTransferAgency" action="" method="post" enctype="multipart/form-data">
+                    @csrf
+                    @method('POST')
+                    <div class="modal-body">
+                        <p class="fw-bolder">Dans le cas de prestations en compte-joint, la signature des deux titulaires est obligatoire ainsi que la copie des deux pièces d'identité.</p>
+                        <table class="table table-sm">
+                            <tbody>
+                                <tr>
+                                    <td class="fw-bolder">Civilité</td>
+                                    <td>{{ $customer->info->civility }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="fw-bolder">Nom</td>
+                                    <td>{{ $customer->info->lastname }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="fw-bolder">Prénom</td>
+                                    <td>{{ $customer->info->firstname }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="separator separator-dashed my-2 border-gray-500"></div>
+                    <strong>Mon Agence Actuel:</strong> {{ $customer->agency->name }}<br>
+                    <div class="mb-10">
+                        <label for="agency_id" class="form-label required">Je souhaite transférer les comptes de cette agence vers :</label>
+                        <select class="form-control form-control-solid selectpicker" name="agency_id" data-live-search="true">
+                            <option value="" data-tokens=""></option>
+                            @foreach(\App\Models\Core\Agency::all() as $agency)
+                                <option value="{{ $agency->id }}" data-tokens="{{ $agency->postal }} {{ $agency->name }}">{{ $agency->postal }} - {{ $agency->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="modal-footer text-end">
+                        <x-form.button />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section("script")
