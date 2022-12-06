774b151ba957d253b5fb9e0b32a1ee26165dc2e6
mmockelyn <trainznation@gmail.com>
2022-12-06 23:47:40 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/card/index.blade.php
+++ resources/views/customer/compte/card/index.blade.php
@@ -11,7 +11,7 @@
     <div id="app" class="rounded">
         @foreach($wallets as $wallet)
             @foreach($wallet->cards as $card)
-                <a href="{{ route('customer.compte.card.show', $card->id) }}" class="d-flex flex-row justify-content-between align-items-center p-5 rounded border border-2 bg-gray-300 text-black hover-zoom">
+                <a href="{{ route('customer.card.show', $card->id) }}" class="d-flex flex-row justify-content-between align-items-center p-5 rounded border border-2 bg-gray-300 text-black hover-zoom">
                     <span class="d-flex flex-row align-items-center">
                         <span class="symbol symbol-30px symbol-2by3">
                             <img src="/storage/card/{{ $card->support->slug }}.png" alt="">
