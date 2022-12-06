16843951e65cc14e651f03214277657761652a98
mmockelyn <trainznation@gmail.com>
2022-12-07 00:01:44 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/card/show.blade.php
+++ resources/views/customer/compte/card/show.blade.php
@@ -10,7 +10,7 @@
 @section("content")
     <div id="app" class="rounded">
         <div class="d-flex flex-wrap align-items-center w-75 rounded rounded-2 bg-white p-5">
-            <div class="d-flex flex-row align-items-center">
+            <div class="d-flex flex-row">
                 <div class="w-50 me-5">
                     <img src="/storage/card/{{ $card->support->slug }}.png" alt="" class="img-fluid m-0 p-0">
                 </div>
