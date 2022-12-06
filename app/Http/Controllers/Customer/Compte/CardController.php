23f2c4da1bd31a79b462f0aecf1cb73410daa634
mmockelyn <trainznation@gmail.com>
2022-12-06 23:40:58 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Http/Controllers/Customer/Compte/CardController.php
+++ app/Http/Controllers/Customer/Compte/CardController.php
@@ -4,16 +4,24 @@ namespace App\Http\Controllers\Customer\Compte;
 
 use App\Http\Controllers\Controller;
 use App\Models\Customer\Customer;
+use App\Models\Customer\CustomerCreditCard;
 use App\Models\Customer\CustomerWallet;
 
 class CardController extends Controller
 {
     public function index()
     {
-
         return view('customer.compte.card.index', [
             'customer' => Customer::find(auth()->user()->customers->id),
             "wallets" => Customer::find(auth()->user()->customers->id)->wallets
+        ]);
+    }
+
+    public function show($card_id)
+    {
+        return view('customer.compte.card.index', [
+            'customer' => Customer::find(auth()->user()->customers->id),
+            "card" => CustomerCreditCard::find($card_id)
         ]);
     }
 }
