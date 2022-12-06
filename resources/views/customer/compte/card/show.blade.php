8f1779c0c37db03c2839eee5f6bb9cdd4878fd7d
mmockelyn <trainznation@gmail.com>
2022-12-07 00:03:36 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/card/show.blade.php
+++ resources/views/customer/compte/card/show.blade.php
@@ -9,7 +9,27 @@
 
 @section("content")
     <div id="app" class="rounded">
-
+        <div class="d-flex flex-wrap align-items-center w-75 rounded rounded-2 bg-white p-5">
+            <div class="d-flex flex-row">
+                <div class="w-50 me-5">
+                    <img src="/storage/card/{{ $card->support->slug }}.png" alt="" class="img-fluid m-0 p-0">
+                </div>
+                <div class="w-50 align-items-center">
+                    <div class="d-flex flex-center">
+                        <div class="d-flex flex-column align-items-center">
+                            <div class="fw-bolder fs-2">CB VISA</div>
+                            {{ $card->debit_format }}
+                            <a href="{{ route('customer.compte.wallet', $card->wallet->uuid) }}" class="btn btn-link">{{ $card->wallet->name_account_generic }}</a>
+                            @if($card->status == 'active')
+                                <button class="btn btn-circle btn-outline btn-outline-dark">Verrouiller ma carte</button>
+                            @else
+                                <button class="btn btn-circle btn-outline btn-outline-dark">Dever ma carte</button>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
 
