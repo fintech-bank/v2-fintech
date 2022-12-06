8e80a0f9ac9fd15d71b43a68734d5b39e80c7d5d
mmockelyn <trainznation@gmail.com>
2022-12-06 20:56:46 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/show.blade.php
+++ resources/views/customer/compte/show.blade.php
@@ -12,8 +12,8 @@
         <div class="row">
             <div class="col-md-9 col-sm-12">
                 <div class="d-flex flex-center flex-column">
-                    <div class="uppercase">Solde au {{ $wallet->transactions()->orderBy('updated_at', 'desc')->first() ? $wallet->transactions()->orderBy('updated_at', 'desc')->first()->updated_at->format('d/m/Y') : now() }}</div>
-                    <div class="fw-bolder">{{ $wallet->solde_remaining >= 0 ? "+ ".eur($wallet->solde_remaining) : "- ".eur($wallet->solde_remaining)}}</div>
+                    <div class="uppercase fs-1">Solde au {{ $wallet->transactions()->orderBy('updated_at', 'desc')->first() ? $wallet->transactions()->orderBy('updated_at', 'desc')->first()->updated_at->format('d/m/Y') : now() }}</div>
+                    <div class="fw-bolder fs-2hx">{{ $wallet->solde_remaining >= 0 ? "+ ".eur($wallet->solde_remaining) : "- ".eur($wallet->solde_remaining)}}</div>
                 </div>
             </div>
         </div>
