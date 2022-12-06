6b139d4130017c8b03e856dbced197dc360ca79f
mmockelyn <trainznation@gmail.com>
2022-12-06 21:20:55 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Models/Customer/CustomerTransaction.php
+++ app/Models/Customer/CustomerTransaction.php
@@ -154,7 +154,7 @@ class CustomerTransaction extends Model
     {
         $size_icon = $size / 2;
         return '<div class="symbol symbol-' . $size . 'px symbol-circle me-2" data-bs-toggle="tooltip" title="' . $this->getTypeTextAttribute() . '">
-                    <div class="symbol-label bg-'.$this->type_color.'"><span class="iconify text-light-'.$this->type_color.'" data-icon="' . $this->getTypeIconAttribute() . '"  data-width="' . $size_icon . '" data-height="' . $size_icon . '"></span></div>
+                    <div class="symbol-label bg-'.$this->type_color.'"><span class="iconify text-white" data-icon="' . $this->getTypeIconAttribute() . '"  data-width="' . $size_icon . '" data-height="' . $size_icon . '"></span></div>
                 </div>';
     }
 
