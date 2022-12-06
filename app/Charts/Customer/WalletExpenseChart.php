db3732f2d5468bb215a5656196aeb0be6cc8e968
mmockelyn <trainznation@gmail.com>
2022-12-06 23:02:33 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- app/Charts/Customer/WalletExpenseChart.php
+++ app/Charts/Customer/WalletExpenseChart.php
@@ -17,18 +17,54 @@ class WalletExpenseChart
     public function build($customer_wallet_id): \ArielMejiaDev\LarapexCharts\DonutChart
     {
         $data = collect();
-        $depot = CustomerTransaction::where('customer_wallet_id', $customer_wallet_id)
-            ->where('type', 'depot')
+
+        $retrait = CustomerTransaction::where('customer_wallet_id', $customer_wallet_id)
+            ->where('type', 'retrait')
             ->whereBetween('confirmed_at', [now()->startOfMonth(), now()->endOfMonth()])
             ->sum('amount');
 
-        dd($depot);
+        $payment = CustomerTransaction::where('customer_wallet_id', $customer_wallet_id)
+            ->where('type', 'payment')
+            ->whereBetween('confirmed_at', [now()->startOfMonth(), now()->endOfMonth()])
+            ->sum('amount');
+
+        $virement = CustomerTransaction::where('customer_wallet_id', $customer_wallet_id)
+            ->where('type', 'virement')
+            ->whereBetween('confirmed_at', [now()->startOfMonth(), now()->endOfMonth()])
+            ->sum('amount');
+
+        $sepa = CustomerTransaction::where('customer_wallet_id', $customer_wallet_id)
+            ->where('type', 'sepa')
+            ->whereBetween('confirmed_at', [now()->startOfMonth(), now()->endOfMonth()])
+            ->sum('amount');
+
+        $frais = CustomerTransaction::where('customer_wallet_id', $customer_wallet_id)
+            ->where('type', 'frais')
+            ->whereBetween('confirmed_at', [now()->startOfMonth(), now()->endOfMonth()])
+            ->sum('amount');
+
+        $souscription = CustomerTransaction::where('customer_wallet_id', $customer_wallet_id)
+            ->where('type', 'souscription')
+            ->whereBetween('confirmed_at', [now()->startOfMonth(), now()->endOfMonth()])
+            ->sum('amount');
+
+        $autre = CustomerTransaction::where('customer_wallet_id', $customer_wallet_id)
+            ->where('type', 'autre')
+            ->whereBetween('confirmed_at', [now()->startOfMonth(), now()->endOfMonth()])
+            ->sum('amount');
+
+        $credit = CustomerTransaction::where('customer_wallet_id', $customer_wallet_id)
+            ->where('type', 'facelia')
+            ->whereBetween('confirmed_at', [now()->startOfMonth(), now()->endOfMonth()])
+            ->sum('amount');
+
+
 
         return $this->chart->donutChart()
             ->setTitle('Dépense')
             ->setSubtitle('Depuis le '.now()->startOfMonth()->format('d.m.Y'))
-            ->addData([80.30, 89.63, 96.30])
-            ->setLabels(['Vie quotidienne', 'Emprunts', 'Loisirs'])
-            ->setColors(['#D32F2F', '#03A9F4', '#04D6F9']);
+            ->addData([$retrait, $payment, $virement, $sepa, $frais, $souscription, $autre, $credit])
+            ->setLabels(['Retrait bancaire', 'CB, Paiement, Loisir', 'Virement Bancaire', 'Prélèvement', 'Frais Bancaire', 'Souscription', 'Autre', 'Emprunts, Crédit'])
+            ->setColors(['#E57373', '#F06292', '#BA68C8', '#9575CD', '#7986CB', '#64B5F6', '#4DB6AC', '#FFD54F']);
     }
 }
