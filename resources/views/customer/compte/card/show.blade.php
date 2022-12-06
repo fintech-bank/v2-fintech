b64ea9bce2b6735d6507845e3443cbee2d05d7fe
mmockelyn <trainznation@gmail.com>
2022-12-06 23:52:51 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/card/show.blade.php
+++ resources/views/customer/compte/card/show.blade.php
@@ -10,7 +10,7 @@
 @section("content")
     <div id="app" class="rounded">
         <div class="d-flex flex-center w-75 rounded rounded-2 bg-white p-5">
-            <div class="d-flex flex-row">
+            <div class="d-flex flex-row m-0">
                 <div class="w-50">
                     <img src="/storage/card/{{ $card->support->slug }}.png" alt="" class="img-fluid">
                 </div>
