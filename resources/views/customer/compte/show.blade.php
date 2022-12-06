cfd87b88ae5015441aaa35659cadcd8b1466f303
mmockelyn <trainznation@gmail.com>
2022-12-06 21:08:03 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/show.blade.php
+++ resources/views/customer/compte/show.blade.php
@@ -21,6 +21,24 @@
                     size="4"
                     size-text="fs-1"
                     class="uppercase w-100 my-5" />
+
+                @foreach($wallet->transactions()->where('confirmed', 0)->orderBy('updated_at', 'desc')->get() as $transaction)
+
+                @endforeach
+                <div class="accordion accordion-flush" id="opsAtt">
+                    @foreach($wallet->transactions()->where('confirmed', 0)->orderBy('updated_at', 'desc')->get() as $transaction)
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="flush-headingOne">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-{{ $transaction->uuid }}" aria-expanded="false" aria-controls="flush-{{ $transaction->uuid }}">
+                                    Accordion Item #1
+                                </button>
+                            </h2>
+                            <div id="flush-{{ $transaction->uuid }}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#opsAtt">
+                                <div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the first item's accordion body.</div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
