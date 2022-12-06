141ec82e49fe3cd2d9bb20b4bde9c18125d38fba
mmockelyn <trainznation@gmail.com>
2022-12-06 21:00:28 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/show.blade.php
+++ resources/views/customer/compte/show.blade.php
@@ -12,9 +12,15 @@
         <div class="row">
             <div class="col-md-9 col-sm-12">
                 <div class="d-flex flex-center flex-column">
-                    <div class="uppercase fs-1">Solde au {{ $wallet->transactions()->orderBy('updated_at', 'desc')->first() ? $wallet->transactions()->orderBy('updated_at', 'desc')->first()->updated_at->format('d/m/Y') : now() }}</div>
+                    <div class="uppercase fs-2">Solde au {{ $wallet->transactions()->orderBy('updated_at', 'desc')->first() ? $wallet->transactions()->orderBy('updated_at', 'desc')->first()->updated_at->format('d/m/Y') : now() }}</div>
                     <div class="fw-bolder fs-2hx">{{ $wallet->solde_remaining >= 0 ? "+ ".eur($wallet->solde_remaining) : "- ".eur($wallet->solde_remaining)}}</div>
                 </div>
+
+                <x-base.underline
+                    title="Opération à venir:"
+                    size="4"
+                    size-text="fs-1"
+                    class="uppercase w-100 my-5" />
             </div>
         </div>
     </div>
