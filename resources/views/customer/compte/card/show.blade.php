4dbeb268b48ad0b6e6c3935935fa02a01ab84ed8
mmockelyn <trainznation@gmail.com>
2022-12-06 23:52:34 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/card/show.blade.php
+++ resources/views/customer/compte/card/show.blade.php
@@ -12,7 +12,7 @@
         <div class="d-flex flex-center w-75 rounded rounded-2 bg-white p-5">
             <div class="d-flex flex-row">
                 <div class="w-50">
-                    <img src="/storage/card/{{ $card->support->slug }}.png" alt="" class="img-thumbnail">
+                    <img src="/storage/card/{{ $card->support->slug }}.png" alt="" class="img-fluid">
                 </div>
             </div>
         </div>
