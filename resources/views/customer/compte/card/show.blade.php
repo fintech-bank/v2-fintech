dd1f176c767e9e755097b892952d43537c9353d5
mmockelyn <trainznation@gmail.com>
2022-12-07 00:46:49 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/card/show.blade.php
+++ resources/views/customer/compte/card/show.blade.php
@@ -150,14 +150,12 @@
             </div>
         </div>
         <!--end::Accordion-->
-        <a href="" class="rounded border border-2 w-100 bg-white text-dark text-hover-primary p-5">
-            <div class="d-flex flex-row justify-content-between">
-                <div class="d-flex flex-column">
-                    <span class="fw-bolder fs-2">Consulter mon code secret</span>
-                    <p>Vous avez oublier le code secret de votre carte bancaire ? Consulter le !</p>
-                </div>
-                <i class="fa-solid fa-arrow-right-long fs-1"></i>
+        <a href="" class="d-flex flex-row justify-content-between rounded border border-2 w-100 bg-white text-dark text-hover-primary p-5">
+            <div class="d-flex flex-column">
+                <span class="fw-bolder fs-2">Consulter mon code secret</span>
+                <p>Vous avez oublier le code secret de votre carte bancaire ? Consulter le !</p>
             </div>
+            <i class="fa-solid fa-arrow-right-long fs-1"></i>
         </a>
     </div>
 @endsection
