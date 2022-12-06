6269c5b8c1ad5d7b72148415c595b69864b0d16d
mmockelyn <trainznation@gmail.com>
2022-12-06 20:49:34 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/index.blade.php
+++ resources/views/customer/compte/index.blade.php
@@ -103,7 +103,7 @@
                 </a>
                 <div class="separator separator-dotted border-gray-400 my-10"></div>
                 <a href="{{ route('customer.account.agenda.index') }}" class="d-flex flex-row align-items-center text-dark hover-zoom">
-                    <i class="fa-regular calendar-check fs-2hx me-2"></i>
+                    <i class="fa-regular fa-calendar-check fs-2hx me-2"></i>
                     <span class="fs-1">Mes rendez-vous</span>
                 </a>
                 <div class="separator separator-dotted border-gray-400 my-10"></div>
