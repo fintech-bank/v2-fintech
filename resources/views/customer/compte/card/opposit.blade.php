2d04564cbdbc672786330a5d8bb2620721f10223
mmockelyn <trainznation@gmail.com>
2022-12-07 10:01:05 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/compte/card/opposit.blade.php
+++ resources/views/customer/compte/card/opposit.blade.php
@@ -15,7 +15,7 @@
                     <ol class="cd-multi-steps text-center custom-icons">
                         <li {{ $card->opposition->status == 'submit' ? 'class=current' : 'class=visited' }}><em>Soumission</em></li>
                         <li {{ $card->opposition->status == 'progress' ? 'class=current' : ($card->opposition->status == 'terminate' ? 'class=visited' : '') }}><em>Etude en cours</em></li>
-                        <li><em>Terminer</em></li>
+                        <li {{ $card->opposition->status == 'terminated' ? 'class=current' : '' }}><em>Terminer</em></li>
                     </ol>
                 </nav>
             </div>
