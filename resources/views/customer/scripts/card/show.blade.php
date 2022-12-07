f9ae81815196d98a90bb56cc129a409ac1711e61
mmockelyn <trainznation@gmail.com>
2022-12-07 01:29:49 +0100

    feat(Agent Client Création Client) : Edition de la partie client

--- resources/views/customer/scripts/card/show.blade.php
+++ resources/views/customer/scripts/card/show.blade.php
@@ -68,36 +68,50 @@
         })
     }
 
-    $(forms.formOppositCard).on('submit', e => {
-        e.preventDefault()
-        let form = $(forms.formOppositCard)
-        let url = form.attr('action')
-        let data = form.serializeArray()
-        let btn = form.find('[type="submit"]')
-        let method = form.find('[name="_method"]').val()
+    Swal.fire({
+        title: 'Êtes-vous sur ?',
+        text: "La création d'un dossier d'opposition va bloquer votre carte bancaire ?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Oui!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $(forms.formOppositCard).on('submit', e => {
+                e.preventDefault()
+                let form = $(forms.formOppositCard)
+                let url = form.attr('action')
+                let data = form.serializeArray()
+                let btn = form.find('[type="submit"]')
+                let method = form.find('[name="_method"]').val()
 
-        btn.attr('data-kt-indicator', 'on')
+                btn.attr('data-kt-indicator', 'on')
 
-        $.ajax({
-            url: url,
-            method: method,
-            data: data,
-            success: data => {
-                btn.removeAttr('data-kt-indicator')
-                if(data.state === 'warning') {
-                    toastr.warning(`${data.message}`)
-                } else {
-                    toastr.success(`${data.message}`)
+                $.ajax({
+                    url: url,
+                    method: method,
+                    data: data,
+                    success: data => {
+                        btn.removeAttr('data-kt-indicator')
+                        if(data.state === 'warning') {
+                            toastr.warning(`${data.message}`)
+                        } else {
+                            toastr.success(`${data.message}`)
 
-                    setTimeout(() => {
-                        window.location.reload()
-                    }, 1200)
-                }
-            },
-            error: () => {
-                btn.removeAttr('data-kt-indicator')
-                toastr.error(`Erreur lors de l'execution de l'appel, consulter les logs ou contacter un administrateur`, `Erreur Système`)
-            }
-        })
+                            setTimeout(() => {
+                                window.location.reload()
+                            }, 1200)
+                        }
+                    },
+                    error: () => {
+                        btn.removeAttr('data-kt-indicator')
+                        toastr.error(`Erreur lors de l'execution de l'appel, consulter les logs ou contacter un administrateur`, `Erreur Système`)
+                    }
+                })
+            })
+        }
     })
+
+
 </script>
