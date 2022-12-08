b985152ce9cc2d6ad067d8b121a9a0f8bf8cf9f5
mmockelyn <trainznation@gmail.com>
2022-12-08 12:04:14 +0100

    fix(Client) : Correction

--- resources/views/customer/compte/show.blade.php
+++ resources/views/customer/compte/show.blade.php
@@ -13,7 +13,7 @@
             <div class="col-md-9 col-sm-12">
                 <div class="d-flex flex-center flex-column">
                     <div class="uppercase fs-2">Solde au {{ $wallet->transactions()->orderBy('updated_at', 'desc')->first() ? $wallet->transactions()->orderBy('updated_at', 'desc')->first()->updated_at->format('d/m/Y') : now() }}</div>
-                    <div class="fw-bolder fs-2hx">{{ $wallet->solde_remaining >= 0 ? "+ ".eur($wallet->solde_remaining) : "- ".eur($wallet->solde_remaining)}}</div>
+                    <div class="fw-bolder fs-2hx">{{ $wallet->solde_remaining >= 0 ? "+ ".eur($wallet->solde_remaining) : eur($wallet->solde_remaining)}}</div>
                 </div>
 
                 <x-base.underline
