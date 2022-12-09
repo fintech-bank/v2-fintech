609f47bf7e3aa0c43eed5abc2e7d723533a38ae1
mmockelyn <trainznation@gmail.com>
2022-12-09 20:13:19 +0100

    fix(Client) : Correction

--- resources/views/customer/compte/show.blade.php
+++ resources/views/customer/compte/show.blade.php
@@ -256,12 +256,14 @@
                         <div class="card-body">
                             @foreach($wallet->cards as $card)
                                 <div class="d-flex flex-row justify-content-between">
-                                    <div class="symbol symbol-30px symbol-2by3">
-                                        <img src="/storage/card/{{ $card->support->slug }}.png" alt=""/>
-                                    </div>
-                                    <div class="d-flex flex-column align-items-center">
-                                        <span>CB {{ $card->support->name }}</span>
-                                        Capacité mensuelle de paiement jusqu'au {{ now()->endOfMonth()->format("d/m/Y") }}
+                                    <div class="d-flex flex-row">
+                                        <div class="symbol symbol-30px symbol-2by3">
+                                            <img src="/storage/card/{{ $card->support->slug }}.png" alt=""/>
+                                        </div>
+                                        <div class="d-flex flex-column align-items-center">
+                                            <span>CB {{ $card->support->name }}</span>
+                                            Capacité mensuelle de paiement jusqu'au {{ now()->endOfMonth()->format("d/m/Y") }}
+                                        </div>
                                     </div>
                                     {{ $card->actual_limit_payment }}
                                 </div>
