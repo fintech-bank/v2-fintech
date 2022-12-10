8706cf3b587cf130494e1fe82d376e3d9f48ca3e
mmockelyn <trainznation@gmail.com>
2022-12-10 16:31:08 +0100

    fix(Client) : Correction

--- app/Http/Controllers/Api/Insurance/InsuranceController.php
+++ app/Http/Controllers/Api/Insurance/InsuranceController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Api\Insurance;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class InsuranceController extends Controller
 {
     public function index()
     {
 
+    }
+
+    public function info(Request $request)
+    {
+        dd($request->all());
     }
 }
