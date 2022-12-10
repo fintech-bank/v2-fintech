ce56846875d2faabf613ab5c4e32fef97ad0bc42
mmockelyn <trainznation@gmail.com>
2022-12-10 01:03:44 +0100

    fix(Client) : Correction

--- resources/views/auths/register/suivi.blade.php
+++ resources/views/auths/register/suivi.blade.php
@@ -25,6 +25,19 @@ License: For each use you must have a valid license purchased only from above li
     <link href="/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <link href="/css/app.css" rel="stylesheet" type="text/css" />
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6393cca0b0d6371309d38dcc/1gjslekvm';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
     <!--end::Global Stylesheets Bundle-->
 </head>
 <!--end::Head-->
