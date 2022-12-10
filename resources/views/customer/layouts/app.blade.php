6615db41cc2b8b4bbe6c99c4acf6f0472a871ed4
mmockelyn <trainznation@gmail.com>
2022-12-10 01:08:55 +0100

    fix(Client) : Correction

--- resources/views/customer/layouts/app.blade.php
+++ resources/views/customer/layouts/app.blade.php
@@ -105,19 +105,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/dist/js/bootstrap-select.min.js"></script>
 <script src="/js/app.js"></script>
 <script src="/js/function.js"></script>
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/6393cca0b0d6371309d38dcc/1gjslekvm';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
+<script src="//code.tidio.co/y3ykzy1n0thcqss40z4rsnbllis4zog7.js" async></script>
 @auth
     <script src="{{ asset('js/enable-push.js') }}"></script>
 @endauth
--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -36,18 +36,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.0.279/pdf.min.js"></script>
     <script src="/assets/plugins/custom/datatables/datatables.bundle.js"></script>
     <script src="/js/app.js"></script>
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-            s1.async=true;
-            s1.src='https://embed.tawk.to/6393cca0b0d6371309d38dcc/1gjslekvm';
-            s1.charset='UTF-8';
-            s1.setAttribute('crossorigin','*');
-            s0.parentNode.insertBefore(s1,s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
+    <script src="//code.tidio.co/y3ykzy1n0thcqss40z4rsnbllis4zog7.js" async></script>
     @yield("scripts")
 </html>
