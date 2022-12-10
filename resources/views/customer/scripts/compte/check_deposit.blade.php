9ead64f8ef1285732e351b94153aefdfa0b0b4d7
mmockelyn <trainznation@gmail.com>
2022-12-10 17:23:22 +0100

    fix(Client) : Correction

--- resources/views/customer/scripts/compte/check_deposit.blade.php
+++ resources/views/customer/scripts/compte/check_deposit.blade.php
@@ -1,8 +1,25 @@
 <script type="text/javascript">
     let tables = {}
-    let elements = {}
+    let elements = {
+        repeaterChqRepeat: document.querySelector("#chq_repeat")
+    }
     let modals = {}
     let forms = {}
     let dataTable = {}
     let block = {}
+
+    $(elements.repeaterChqRepeat).repeater({
+        initEmpty: false,
+        defaultValues: {
+            'text-input': 'foo'
+        },
+
+        show: function () {
+            $(this).slideDown();
+        },
+
+        hide: function (deleteElement) {
+            $(this).slideUp(deleteElement);
+        }
+    })
 </script>
